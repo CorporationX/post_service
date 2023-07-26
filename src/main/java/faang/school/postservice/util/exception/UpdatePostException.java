456f8a6011010e7aab5fd64335cae9e8fc320fd8
Andrey <khalyavinandrey@gmail.com>
2023-07-26 17:10:41 +0500

    добавил новый exception и обновил exception handler

---
+++ src/main/java/faang/school/postservice/util/exception/UpdatePostException.java
@@ -0,0 +1,7 @@
+package faang.school.postservice.util.exception;
+
+public class UpdatePostException extends RuntimeException{
+    public UpdatePostException(String message) {
+        super(message);
+    }
+}
