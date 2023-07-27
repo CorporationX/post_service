669d53f0e8c2c5fc44f42466a7dc0cd095f4affd
Andrey <khalyavinandrey@gmail.com>
2023-07-27 17:07:43 +0500

    добавил новый exception и обновил exception handler

---
+++ src/main/java/faang/school/postservice/util/exception/DeletePostException.java
@@ -0,0 +1,7 @@
+package faang.school.postservice.util.exception;
+
+public class DeletePostException extends RuntimeException{
+    public DeletePostException(String message) {
+        super(message);
+    }
+}
--- src/main/java/faang/school/postservice/util/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/util/exceptionhandler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package faang.school.postservice.util.exceptionhandler;
 import faang.school.postservice.dto.response.ErrorResponse;
 import faang.school.postservice.util.exception.CreatePostException;
 import faang.school.postservice.util.exception.DataValidationException;
+import faang.school.postservice.util.exception.DeletePostException;
 import faang.school.postservice.util.exception.PublishPostException;
 import faang.school.postservice.util.exception.UpdatePostException;
 import feign.FeignException;
@@ -37,6 +38,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UpdatePostException.class)
     public ResponseEntity<ErrorResponse> handleException(UpdatePostException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(DeletePostException.class)
+    public ResponseEntity<ErrorResponse> handleException(DeletePostException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
     }
 
