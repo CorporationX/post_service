6ea8a10dd86311cec5bb1b25528aa1229d3a105a
Egor Selivanov <egorselivanov0@gmail.com>
2023-07-28 16:40:13 +0300

    Добавил create метод в controller и первичную валидацию dto

--- src/main/java/faang/school/postservice/dto/comment/CommentDto.java
+++ src/main/java/faang/school/postservice/dto/comment/CommentDto.java
@@ -1,6 +1,10 @@
 package faang.school.postservice.dto.comment;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +19,19 @@ import java.util.List;
 @AllArgsConstructor
 public class CommentDto {
     private Long id;
+
+    @NotBlank(message = "Content cannot be empty!")
+    @Size(min = 1, max = 4096, message = "Contest size should be between 1 and 4096!")
     private String content;
+
+    @NotNull
+    @Min(value = 1L, message = "Author id cannot be lower than 1!")
     private Long authorId;
+
+    @NotNull
+    @Min(value = 1L, message = "Post id cannot be lower than 1!")
     private Long postId;
+
     private List<Long> likesIds;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
