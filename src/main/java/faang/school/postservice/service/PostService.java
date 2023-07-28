4409931ae4a60f5c7585e5755d4a3ee0113f6389
An Dima <an.dima@slexn.com>
2023-07-28 17:56:15 +0900

    not finished

--- src/main/java/faang/school/postservice/service/PostService.java
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -1,24 +1,52 @@
 package faang.school.postservice.service;
 
+import faang.school.postservice.client.ProjectServiceClient;
+import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.post.CreatePostDto;
 import faang.school.postservice.dto.post.ResponsePostDto;
+import faang.school.postservice.dto.project.ProjectDto;
+import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.mapper.post.ResponsePostMapper;
+import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
     private final ResponsePostMapper responsePostMapper;
+    private final UserServiceClient userServiceClient;
+    private final ProjectServiceClient projectServiceClient;
 
     public ResponsePostDto createDraft(CreatePostDto dto) {
-        validateCreate(dto);
-        return null;
+        Post post = new Post();
+
+        validateCreate(dto, post);
+
+        post.setContent(dto.getContent());
+        post.setCreatedAt(LocalDateTime.now());
+        post.setPublished(false);
+        post.setDeleted(false);
+
+        return responsePostMapper.toDto(postRepository.save(post));
     }
 
-    private void validateCreate(CreatePostDto dto) {
-
+    private void validateCreate(CreatePostDto dto, Post post) {
+        if (dto.getAuthorId() != null && dto.getProjectId() != null) {
+            throw new IllegalArgumentException("Both AuthorId and ProjectId can't be not null");
+        }
+        if (dto.getAuthorId() != null) {
+            UserDto userDto = Objects.requireNonNull(userServiceClient.getUser(dto.getAuthorId()));
+            post.setAuthorId(userDto.getId());
+        }
+        if (dto.getProjectId() != null) {
+            ProjectDto projectDto = Objects.requireNonNull(projectServiceClient.getProject(dto.getProjectId()));
+            post.setAuthorId(projectDto.getId());
+        }
     }
 }
--- src/test/java/faang/school/postservice/service/PostServiceTest.java
+++ src/test/java/faang/school/postservice/service/PostServiceTest.java
@@ -1,12 +1,18 @@
 package faang.school.postservice.service;
 
+import faang.school.postservice.client.ProjectServiceClient;
+import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.dto.post.CreatePostDto;
 import faang.school.postservice.mapper.post.ResponsePostMapper;
 import faang.school.postservice.repository.PostRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -14,6 +20,29 @@ class PostServiceTest {
     private PostRepository postRepository;
     @Spy
     private ResponsePostMapper responsePostMapper = ResponsePostMapper.INSTANCE;
+    @Mock
+    private UserServiceClient userServiceClient;
+    @Mock
+    private ProjectServiceClient projectServiceClient;
     @InjectMocks
     private PostService postService;
+
+//    @Test
+//    void createTest() {
+//        CreatePostDto blankContent = CreatePostDto.builder().authorId(1L).content("").build();
+//        CreatePostDto correct = CreatePostDto.builder().authorId(1L).content("Content").build();
+//
+//        List<CreatePostDto> createPostDtoList = List.of(bothNotNull, blankContent, correct);
+//
+//        List<>
+//        createPostDtoList.forEach(postDto -> );
+//    }
+
+    @Test
+    void createBothNotNull(){}
+    CreatePostDto bothNotNull = CreatePostDto.builder().authorId(1L).projectId(1L).build();
+
+    IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> postService.createDraft(bothNotNull));
+
+    assertEquals()
 }
