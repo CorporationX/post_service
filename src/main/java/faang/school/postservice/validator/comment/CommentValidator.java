dda56ebbda2268008f4496d99513bf817efb2852
alexandr <matcnbev2001@mail.ru>
2023-07-28 16:05:08 +0300

    First step (without tests)

--- src/main/java/faang/school/postservice/validator/comment/CommentValidator.java
+++ src/main/java/faang/school/postservice/validator/comment/CommentValidator.java
@@ -1,23 +1,40 @@
 package faang.school.postservice.validator.comment;
 
+import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.exeption.DataValidationException;
+import faang.school.postservice.repository.PostRepository;
 
 public class CommentValidator {
-    public void idValidator(long id){
-        if (id<1){
+    private PostRepository postRepository;
+    private UserServiceClient userServiceClient;
+
+    public void idValidator(long id) {
+        if (id < 1) {
             throw new DataValidationException("Id mast be more 1, your isn't");
         }
     }
 
-    public void commentDtoValidator(CommentDto commentDto){
+    public void commentDtoValidator(CommentDto commentDto) {
         idValidator(commentDto.getId());
-        idValidator(commentDto.getAuthorId());
-        idValidator(commentDto.getPostId());
+        postExistValidator(commentDto.getPostId());
+        authorExistValidator(commentDto.getAuthorId());
+
 
         int lenComment = commentDto.getContent().length();
-        if(lenComment>=4096 || lenComment==0){
+        if (lenComment >= 4096 || lenComment == 0) {
             throw new DataValidationException("Length of comment is not correct");
         }
     }
+
+    public void postExistValidator(long postId) {
+        postRepository.findById(postId)
+                .orElseThrow(() -> new DataValidationException("Post is not exist"));
+    }
+    public void authorExistValidator(long authorId){
+        if (userServiceClient.getUser(authorId) == null) {
+            throw new DataValidationException("User is not exist");
+        }
+    }
+
 }
--- src/test/java/faang/school/postservice/service/comment/CommentServiceTest.java
+++ src/test/java/faang/school/postservice/service/comment/CommentServiceTest.java
@@ -1,13 +1,25 @@
 package faang.school.postservice.service.comment;
 
+import faang.school.postservice.mapper.comment.CommentMapper;
+import faang.school.postservice.repository.CommentRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommentServiceTest {
+    @InjectMocks
+    private CommentService commentService;
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private CommentMapper commentMapper;
 
     @Test
     void createComment() {
+
     }
 
     @Test
