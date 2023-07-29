d4c8b4340dfd8d295d53c7d945b8387ec39e1307
alexandr <matcnbev2001@mail.ru>
2023-07-29 17:09:13 +0300

    update validator and mapper

--- src/main/java/faang/school/postservice/validator/comment/CommentValidator.java
+++ src/main/java/faang/school/postservice/validator/comment/CommentValidator.java
@@ -1,23 +1,38 @@
 package faang.school.postservice.validator.comment;
 
+import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.exeption.DataValidationException;
+import faang.school.postservice.service.post.PostService;
 
 public class CommentValidator {
-    public void idValidator(long id){
-        if (id<1){
+    private PostService postService;
+    private UserServiceClient userServiceClient;
+
+    public void idValidator(long id) {
+        if (id < 1) {
             throw new DataValidationException("Id mast be more 1, your isn't");
         }
     }
 
-    public void commentDtoValidator(CommentDto commentDto){
+    public void commentDtoValidator(CommentDto commentDto) {
         idValidator(commentDto.getId());
-        idValidator(commentDto.getAuthorId());
-        idValidator(commentDto.getPostId());
+        postExistValidator(commentDto.getPostId());
+        authorExistValidator(commentDto.getAuthorId());
 
         int lenComment = commentDto.getContent().length();
-        if(lenComment>=4096 || lenComment==0){
+        if (lenComment >= 4096 || lenComment == 0) {
             throw new DataValidationException("Length of comment is not correct");
+        }
+    }
+
+    public void postExistValidator(long postId) {
+        postService.getPostById(postId);
+    }
+
+    public void authorExistValidator(long authorId) {
+        if (userServiceClient.getUser(authorId) == null) {
+            throw new DataValidationException("User is not exist");
         }
     }
 }
