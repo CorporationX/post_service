2a4f33b4ab597f32bf03ac96a18d55e94710a47d
kxnvg <nikita.armeec.88@mail.ru>
2023-07-29 13:04:37 +0300

    Изменены endpoints в PostController

--- src/main/java/faang/school/postservice/controller/PostController.java
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/posts")
+@RestController
+@RequestMapping("posts")
 @RequiredArgsConstructor
 public class PostController {
 
