894d98ee97f35115c9a24153f1acbe5aa52dff03
alexandr <matcnbev2001@mail.ru>
2023-07-29 16:57:32 +0300

    first step without tests

--- src/main/java/faang/school/postservice/service/comment/CommentService.java
+++ src/main/java/faang/school/postservice/service/comment/CommentService.java
@@ -1,7 +1,12 @@
 package faang.school.postservice.service.comment;
 
 import faang.school.postservice.dto.comment.CommentDto;
+import faang.school.postservice.exeption.DataValidationException;
+import faang.school.postservice.model.Comment;
+import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.CommentRepository;
+import faang.school.postservice.service.post.PostService;
+import faang.school.postservice.validator.comment.CommentValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +16,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final CommentValidator commentValidator;
+    private final PostService postService;
 
     public void createComment(CommentDto commentDto){
 
     }
 
     public void updateComment(CommentDto commentDto){
+        Comment comment = getCommentById(commentDto.getId());
+        Post post = postService.getPostById(commentDto.getPostId());
 
+        commentValidator.updateCommentValidator(post,comment);
+
+        comment.setContent(commentDto.getContent());
     }
 
     public List<CommentDto> getAllComments(long postId){
@@ -26,5 +38,10 @@ public class CommentService {
 
     public void deleteComment(long commentId, long authorId){
 
+    }
+
+    public Comment getCommentById(long commentId){
+        return commentRepository.findById(commentId)
+                .orElseThrow(()->new DataValidationException("Comment was not found"));
     }
 }
