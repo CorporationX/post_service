17a70effa2b7a35b2fcfca51c8bf0bf11f1a6294
alexandr <matcnbev2001@mail.ru>
2023-07-30 06:54:25 +0300

    update after rew

--- src/main/java/faang/school/postservice/mapper/comment/CommentMapper.java
+++ src/main/java/faang/school/postservice/mapper/comment/CommentMapper.java
@@ -2,12 +2,15 @@ package faang.school.postservice.mapper.comment;
 
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.model.Comment;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.FIELD)
 public interface CommentMapper {
     @Mapping(source = "post.id", target = "postId")
     CommentDto toDto(Comment comment);
