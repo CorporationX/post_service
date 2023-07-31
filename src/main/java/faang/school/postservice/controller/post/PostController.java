8a27274948f6526bda81487ffbfc00debd823836
An Dima <an.dima@slexn.com>
2023-07-31 12:48:37 +0900

    soft delete and getById

--- src/main/java/faang/school/postservice/controller/post/PostController.java
+++ src/main/java/faang/school/postservice/controller/post/PostController.java
@@ -1,7 +1,11 @@
 package faang.school.postservice.controller.post;
 
+import faang.school.postservice.dto.post.ResponsePostDto;
 import faang.school.postservice.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/posts/")
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("{id}")
+    public ResponsePostDto getById(@PathVariable Long id){
+        return postService.getById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponsePostDto softDelete(@PathVariable Long id){
+        return postService.softDelete(id);
+    }
 }
--- src/main/java/faang/school/postservice/service/PostService.java
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -1,13 +1,34 @@
 package faang.school.postservice.service;
 
+import faang.school.postservice.dto.post.ResponsePostDto;
 import faang.school.postservice.mapper.post.ResponsePostMapper;
+import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
     private final ResponsePostMapper responsePostMapper;
+
+    @Transactional(readOnly = true)
+    public ResponsePostDto getById(Long id) {
+        return responsePostMapper.toDto(
+                postRepository.findById(id)
+                        .orElseThrow(() -> new IllegalArgumentException("Post is not found"))
+        );
+    }
+
+    @Transactional
+    public ResponsePostDto softDelete(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Post is not found"));
+
+        post.setDeleted(true);
+
+        return responsePostMapper.toDto(post);
+    }
 }
