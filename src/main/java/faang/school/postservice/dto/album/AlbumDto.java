d6aab3d7693ffcbebde6a6f880b734afa7711052
Viktor Ivanov <avacode78@gmail.com>
2023-08-01 18:57:05 +0700

    small fixes

--- src/main/java/faang/school/postservice/dto/album/AlbumDto.java
+++ src/main/java/faang/school/postservice/dto/album/AlbumDto.java
@@ -1,12 +1,16 @@
 package faang.school.postservice.dto.album;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlbumDto {
     private long id;
     private String title;
