1441cd6a607526646c0102236352ba10a05f6db7
Devilserk <demonserker@gmail.com>
2023-08-02 13:41:04 +0600

    без тестов

--- src/main/java/faang/school/postservice/service/like/LikeService.java
+++ src/main/java/faang/school/postservice/service/like/LikeService.java
@@ -18,20 +18,33 @@ public class LikeService {
     private static final int BATCH_SIZE = 100;
 
     public List<UserDto> getUsersByPostId(long postId) {
-        List<UserDto> allUsers = new ArrayList<>();
         List<Long> userIds = getLikedUserIdsByPost(postId);
+        return getAllUsersDto(userIds);
+    }
 
-        for (int i = 0; i < userIds.size(); i += BATCH_SIZE) {
-            int endIndex = Math.min(i + BATCH_SIZE, userIds.size());
-            allUsers.add(userServiceClient.getUsersByIds(userIds.subList(i, endIndex)));
-        }
-
-        return allUsers;
+    public List<UserDto> getUsersByCommentId(long commentId) {
+        List<Long> userIds = getLikedUserIdsByComment(commentId);
+        return getAllUsersDto(userIds);
     }
 
     private List<Long> getLikedUserIdsByPost(long postId) {
         return likeRepository.findByPostId(postId).stream()
                 .map(Like::getUserId)
                 .toList();
+    }
+
+    private List<Long> getLikedUserIdsByComment(long commentId) {
+        return likeRepository.findByCommentId(commentId).stream()
+                .map(Like::getUserId)
+                .toList();
+    }
+
+    private List<UserDto> getAllUsersDto(List<Long> userIds) {
+        List<UserDto> allUsers = new ArrayList<>();
+        for (int i = 0; i < userIds.size(); i += BATCH_SIZE) {
+            int endIndex = Math.min(i + BATCH_SIZE, userIds.size());
+            allUsers.add(userServiceClient.getUsersByIds(userIds.subList(i, endIndex)));
+        }
+        return allUsers;
     }
 }
