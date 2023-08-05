7fe033ff48c261f2bea6e616f62e3fe8bb9fd28c
kxnvg <nikita.armeec.88@mail.ru>
2023-08-05 10:34:18 +0300

    Созданы методы обработки исключений в GlobalExceptionHandler

--- src/main/java/faang/school/postservice/exception/NoPostInDataBaseException.java
+++
@@ -1,8 +0,0 @@
-package faang.school.postservice.exception;
-
-public class NoPostInDataBaseException extends RuntimeException {
-
-    public NoPostInDataBaseException(String message) {
-        super(message);
-    }
-}
