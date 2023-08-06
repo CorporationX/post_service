15f7ce59579b5187f3f42098081941e7966e848e
Andrey <khalyavinandrey@gmail.com>
2023-08-06 20:20:48 +0500

    убрал лишний валидатор, изменил структуру методов в контроллере, добавил логирование в хэндлере, изменил валидацию полей дто

--- src/main/java/faang/school/postservice/util/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/util/exceptionhandler/GlobalExceptionHandler.java
@@ -5,37 +5,50 @@ import faang.school.postservice.util.exception.CreatePostException;
 import faang.school.postservice.util.exception.DataValidationException;
 import faang.school.postservice.util.exception.PublishPostException;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CreatePostException.class)
     public ResponseEntity<ErrorResponse> handleException(CreatePostException e) {
+        log.error("Error has been occurred: ", e);
+
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
     }
 
     @ExceptionHandler(FeignException.class)
-    public ResponseEntity<ErrorResponse> handleException() {
-        return ResponseEntity.badRequest().body(new ErrorResponse("Some error with Feign has been occured", LocalDateTime.now()));
+    public ResponseEntity<ErrorResponse> handleException(FeignException e) {
+        log.error("Error with Feign has been occurred: ", e);
+
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException e) {
+        log.error("Error has been occurred: ", e);
+
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
     }
 
     @ExceptionHandler(DataValidationException.class)
     public ResponseEntity<ErrorResponse> handleException(DataValidationException e) {
+        log.error("Error has been occurred: ", e);
+
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
     }
 
     @ExceptionHandler(PublishPostException.class)
     public ResponseEntity<ErrorResponse> handleException(PublishPostException e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
-    }
+        log.error("Error has been occurred: ", e);
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage(), LocalDateTime.now()));
     }
 }
