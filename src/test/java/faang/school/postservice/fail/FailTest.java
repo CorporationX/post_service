79f9934e04da9179f2584e4b71d5ef951141ba89
Sergey Ivahnenko <ivahnenkose.2003@gmail.com>
2023-08-07 11:38:52 +0700

    ci pipeline fail test

---
+++ src/test/java/faang/school/postservice/fail/FailTest.java
@@ -0,0 +1,12 @@
+package faang.school.postservice.fail;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FailTest {
+    @Test
+    public void testFail() {
+        fail();
+    }
+}
