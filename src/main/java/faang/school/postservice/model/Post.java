3e64181c8a66c45077ffddba429e2bbd0a99bdd2
Matvey <matveynikishaev@gmail.com>
2023-08-07 20:48:58 +0300

    Added verification column, edited dto and hibernate entity

--- src/main/java/faang/school/postservice/model/Post.java
+++ src/main/java/faang/school/postservice/model/Post.java
@@ -68,4 +68,11 @@ public class Post {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Column(name = "verified", nullable = false)
+    private boolean verified = false;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "verified_date")
+    private LocalDateTime verifiedDate;
 }
