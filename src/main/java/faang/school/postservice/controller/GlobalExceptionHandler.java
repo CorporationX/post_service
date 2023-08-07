ab183574fd4b58ed16454358a0d152d81f7d6085
zazozhik <ya.940@mail.ru>
2023-08-07 12:54:04 +0300

    Не Все

---
+++ src/main/java/faang/school/postservice/controller/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package faang.school.postservice.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+
+}
