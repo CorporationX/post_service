0161947f171b74f6429ae0258937d50f1edd6cbd
Andrey <khalyavinandrey@gmail.com>
2023-08-08 12:32:08 +0500

    добавил миграции для публикации постов по расписанию

---
+++ src/main/resources/db/changelog/changeset/post_V003__schedule.sql
@@ -0,0 +1,12 @@
+CREATE TABLE scheduled_tasks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    entity_type varchar(255),
+    task_type varchar(255),
+    entity_id bigint UNIQUE,
+    retry_count int DEFAULT 0,
+    status varchar(255) DEFAULT 'NEW',
+    scheduled_at timestamptz
+)
+
+CREATE INDEX scheduled_tasks_scheduled_at_idx
+    ON scheduled_tasks (entity_id) WHERE status = 'NEW' OR status = 'ERROR';
