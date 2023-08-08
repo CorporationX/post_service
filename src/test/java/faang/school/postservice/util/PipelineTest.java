946214310f8ca7d645dbb337e6d0f1adce0f5525
Alex Tarasov <tarasof.av@gmail.com>
2023-08-08 20:52:50 +0300

    task BC-4200 PipelineTest with fail test

---
+++ src/test/java/faang/school/postservice/util/PipelineTest.java
@@ -0,0 +1,12 @@
+package faang.school.postservice.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PipelineTest {
+    @Test
+    public void test() {
+        fail();
+    }
+}
