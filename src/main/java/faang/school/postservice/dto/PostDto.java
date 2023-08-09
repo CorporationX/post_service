e50733af1c5a97cbdcebaf16b825ac7d31a76f1d
zhugalcf <zhugalcf@gmail.com>
2023-08-09 16:13:16 +0300

    Add likes in getPost methods

--- src/main/java/faang/school/postservice/dto/PostDto.java
+++ src/main/java/faang/school/postservice/dto/PostDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -21,4 +22,5 @@ public class PostDto {
     private LocalDateTime publishedAt;
     private boolean published;
     private boolean deleted;
+    private List<LikeDto> likes;
 }
