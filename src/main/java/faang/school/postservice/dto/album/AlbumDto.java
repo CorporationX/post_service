38410385c6fc74962e6021d22ea09690411fe84d
Viktor Ivanov <avacode78@gmail.com>
2023-08-09 09:53:15 +0700

    small fixes

--- src/main/java/faang/school/postservice/dto/album/AlbumDto.java
+++ src/main/java/faang/school/postservice/dto/album/AlbumDto.java
@@ -1,19 +1,18 @@
 package faang.school.postservice.dto.album;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Builder
 public class AlbumDto {
-    private long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String description;
     private long authorId;
     private List<Long> postsIds;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 }
--- src/main/java/faang/school/postservice/service/album/AlbumService.java
+++ src/main/java/faang/school/postservice/service/album/AlbumService.java
@@ -4,14 +4,18 @@ import faang.school.postservice.config.context.UserContext;
 import faang.school.postservice.model.Album;
 import faang.school.postservice.repository.AlbumRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AlbumService {
     private final AlbumRepository albumRepository;
     private final UserContext userContext;
 
+    @Transactional
     public DeleteResult deleteAlbumOfCertainUser(Long albumId) {
         long userId = userContext.getUserId();
         Album album = albumRepository.findById(albumId).orElse(null);
@@ -24,6 +28,7 @@ public class AlbumService {
             return DeleteResult.NOT_AUTHORIZED;
         }
 
+        log.info("Deleting album with id: {}", albumId);
         albumRepository.deleteById(albumId);
         return DeleteResult.DELETED;
     }
