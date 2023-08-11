f4fd92c7cc79a074b745a08bc1986acd533296d5
kxnvg <nikita.armeec.88@mail.ru>
2023-08-11 19:43:46 +0300

    Созданы методы для обработки исключений, и отправке сообщения об этом клиенту. Обработчик протестирован через Postman( все методы работают корректно)

--- src/main/java/faang/school/postservice/exceptions/SameTimeActionException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.postservice.exceptions;
-
-public class SameTimeActionException extends RuntimeException{
-    public SameTimeActionException(String message) {
-        super(message);
-    }
-}
