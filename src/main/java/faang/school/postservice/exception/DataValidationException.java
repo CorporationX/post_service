f70dff1b6d919ba42ced1375c51a164d468400c8
zazozhik <ya.940@mail.ru>
2023-08-12 19:04:51 +0300

    Все сделал, но нет аннотаций

--- src/main/java/faang/school/postservice/exception/DataValidationException.java
+++ src/main/java/faang/school/postservice/exception/DataValidationException.java
@@ -2,4 +2,6 @@ package faang.school.postservice.exception;
 
 public class DataValidationException extends RuntimeException {
     public DataValidationException(String message){
+        super(message);
+    }
 }
