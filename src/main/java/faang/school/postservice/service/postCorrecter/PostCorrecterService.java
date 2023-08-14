55dc509f44f37b67fc34a9756eb1ffdd387f414b
zhugalcf <zhugalcf@gmail.com>
2023-08-15 00:17:22 +0300

    Pr changes by Kolya

--- src/main/java/faang/school/postservice/service/postCorrecter/PostCorrecterService.java
+++ src/main/java/faang/school/postservice/service/postCorrecter/PostCorrecterService.java
@@ -6,7 +6,6 @@ import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
@@ -22,28 +21,26 @@ public class PostCorrecterService {
     private final PostRepository postRepository;
     private final TextCorrecter textCorrecter;
     private final BingSpellCheckingConfig bingSpellCheckingConfig;
-    private final ApplicationContext applicationContext;
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     public void correctUnpublishedPosts() throws JsonProcessingException {
         List<Post> readyToPublish = postRepository.findNotPublished();
-        ThreadPoolTaskExecutor bingSpellAsyncExecutor = applicationContext
-                .getBean("bingSpellAsyncExecutor", ThreadPoolTaskExecutor.class);
 
         int rateLimit = bingSpellCheckingConfig.getRateLimitPerSecond();
         AtomicInteger requestCount = new AtomicInteger();
-        bingSpellAsyncExecutor.submit(() -> {
+        threadPoolTaskExecutor.submit(() -> {
             for (Post toPublish : readyToPublish) {
                 String content = toPublish.getContent();
                 Post post = toPublish;
                 try {
-                    if (requestCount.get() >= rateLimit){
+                    if (requestCount.get() >= rateLimit) {
                         Thread.sleep(1000);
                         requestCount.set(0);
                     }
                     post.setContent(textCorrecter.getCorrectText(content).get());
                     requestCount.getAndIncrement();
                     post.setCorrected(true);
-                    log.info("Draft posts was corrected successfully postId ={}",toPublish.getId());
+                    log.info("Draft posts was corrected successfully postId ={}", toPublish.getId());
                 } catch (JsonProcessingException | ExecutionException | InterruptedException e) {
                     throw new RuntimeException(e);
                 }
