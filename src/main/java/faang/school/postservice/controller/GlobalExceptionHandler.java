3786b7325b6f1baf39a7fb182596f95f0940b941
zazozhik <ya.940@mail.ru>
2023-08-16 22:07:41 +0300

    Exception

--- src/main/java/faang/school/postservice/controller/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/controller/GlobalExceptionHandler.java
@@ -1,11 +1,35 @@
 package faang.school.postservice.controller;
 
+import faang.school.postservice.exception.DataValidationException;
+import faang.school.postservice.exception.EntityNotFoundException;
+import faang.school.postservice.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(DataValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataValidationException(DataValidationException e) {
+        log.error("Data validation error", e);
+        return new ErrorResponse("Internal Server error",e.getMessage());
+    }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleEntityNotFoundException(EntityNotFoundException e) {
+        log.error("Entity not found error", e);
+        return new ErrorResponse("Entity not found",e.getMessage());
+    }
 
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleRuntimeException(RuntimeException e) {
+        log.error("Runtime error", e);
+        return new ErrorResponse("Internal Server error",e.getMessage());
+    }
 }
