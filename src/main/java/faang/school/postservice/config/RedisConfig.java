6a1b0dd9ee0774bc875b6f581696bfe83fdcfd49
Dobrovolsky Artem <artdobrov@mail.ru>
2023-08-18 18:59:34 +0300

    BC-5534: Добавлены класс конфигурации Redis и CommentEventPublisher

---
+++ src/main/java/faang/school/postservice/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package faang.school.postservice.config;
+
+import faang.school.postservice.publisher.CommentEventPublisher;
+import faang.school.postservice.publisher.MessagePublisher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.channels.comment_event_channel.name}")
+    private String commentTopicName;
+
+    @Bean
+    public JedisConnectionFactory commentConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    MessagePublisher commentEventPublisher() {
+        return new CommentEventPublisher(CommentRedisTemplate(commentConnectionFactory()), commentTopic());
+    }
+
+    @Bean
+    ChannelTopic commentTopic() {
+        return new ChannelTopic(commentTopicName);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> CommentRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
