15c2c2374475ee3cabf75f28cb2a96e4904afb0f
queen <youlka25@yandex.ru>
2023-08-21 08:48:15 +0300

    BC-5545 Remove test implementation for jackson datatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
-    testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
 
     /**
      * Tests
