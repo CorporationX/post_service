4073a11b1fae72849fd1be4a7f971f59cc634766
Irina Vereshchagina <irankamoya@gmail.com>
2023-08-22 10:13:14 +0300

    Проверка комментариев на оскорбительный контент - исправления

--- src/main/java/faang/school/postservice/moderation/CommentModerator.java
+++ src/main/java/faang/school/postservice/moderation/CommentModerator.java
@@ -5,11 +5,10 @@ import faang.school.postservice.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Component
 @RequiredArgsConstructor
@@ -17,11 +16,11 @@ public class CommentModerator {
     private final CommentService commentService;
     @Value("${comment.moderation.scheduler.batchSize}")
     private final int batchSize;
+    private final ThreadPoolTaskExecutor executor;
 
     @Scheduled(cron = "${comment.moderation.scheduler.cron}")
     public void moderateComments() {
         List<Comment> unverifiedComments = commentService.getUnverifiedComments();
-        ExecutorService executor = Executors.newFixedThreadPool(unverifiedComments.size() / batchSize + 1);
 
         for (int i = 0; i < unverifiedComments.size(); i += batchSize) {
             int endIndex = Math.min(i + batchSize, unverifiedComments.size());
