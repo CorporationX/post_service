ae759134628d984848b9d0259ff00d7c31ba1dd5
Anton <Agressive.cat@yandex.ru>
2023-08-29 00:05:22 +0300

    fixed some issues

--- src/main/java/faang/school/postservice/messaging/CommentEventPublisher/RedisCommentEventPublisher.java
+++ src/main/java/faang/school/postservice/messaging/CommentEventPublisher/RedisCommentEventPublisher.java
@@ -1,38 +1,24 @@
 package faang.school.postservice.messaging.CommentEventPublisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.dto.comment.CommentEventDto;
+import faang.school.postservice.util.JsonMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class RedisCommentEventPublisher implements CommentEventPublisher {
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-    @Autowired
-    private ChannelTopic topic;
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    public RedisCommentEventPublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic topic) {
-        this.redisTemplate = redisTemplate;
-        this.topic = topic;
-    }
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ChannelTopic topic;
+    private final JsonMapper jsonMapper;
 
     @Override
     public void publish(CommentEventDto commentEventDto) {
-        String json = null;
-        try {
-            json = objectMapper.writeValueAsString(commentEventDto);
-        } catch (JsonProcessingException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-        redisTemplate.convertAndSend(topic.getTopic(), json);
+        jsonMapper.toObject(commentEventDto)
+                .ifPresent(s -> redisTemplate.convertAndSend(topic.getTopic(), s));
     }
 }
