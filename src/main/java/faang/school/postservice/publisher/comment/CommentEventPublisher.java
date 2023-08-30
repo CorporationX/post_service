fe0708c9243de5c0564977c84cf1cd38ae1ca3c0
ipavelbel <ilpavel94@gmail.com>
2023-08-30 16:55:51 +0200

    Task 5615: Creation and customization CommentEventPublisher no tests

---
+++ src/main/java/faang/school/postservice/publisher/comment/CommentEventPublisher.java
@@ -0,0 +1,16 @@
+package faang.school.postservice.publisher.comment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.postservice.dto.redis.CommentEventDto;
+import faang.school.postservice.publisher.AbstractPublisher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentEventPublisher extends AbstractPublisher<CommentEventDto> {
+    public CommentEventPublisher(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper,
+                                 @Value("${spring.data.redis.channels.comment_channels.name}") String topic) {
+        super(redisTemplate, objectMapper, topic);
+    }
+}
