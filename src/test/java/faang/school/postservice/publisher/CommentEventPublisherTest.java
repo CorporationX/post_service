1242e5af3f165743019f117057fbbed4fb9426cc
RasulMurtuzaliev <ras8705@mail.ru>
2023-09-09 23:16:01 +0500

    created tests

---
+++ src/test/java/faang/school/postservice/publisher/CommentEventPublisherTest.java
@@ -0,0 +1,44 @@
+package faang.school.postservice.publisher;
+
+import faang.school.postservice.model.Comment;
+import faang.school.postservice.model.Post;
+import faang.school.postservice.publisher.events.CommentEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class CommentEventPublisherTest {
+    @InjectMocks
+    private CommentEventPublisher commentEventPublisher;
+    @Mock
+    private RedisMessagePublisher redisMessagePublisher;
+
+    @Test
+    void testPublisherCommentEvent() {
+        Comment comment = Comment.builder()
+                .authorId(1L)
+                .post(Post.builder()
+                        .id(2L)
+                        .build())
+                .id(3L)
+                .content("test")
+                .build();
+        CommentEvent commentEvent = CommentEvent.builder()
+                .authorId(1L)
+                .postId(2L)
+                .commentId(3L)
+                .commentText("test")
+                .build();
+        commentEventPublisher.setCommentEventsChannel("channel");
+
+        commentEventPublisher.publishCommentEvent(comment);
+
+        verify(redisMessagePublisher, times(1)).publish("channel", commentEvent);
+    }
+}
--- src/test/java/faang/school/postservice/publisher/RedisMessagePublisherTest.java
+++ src/test/java/faang/school/postservice/publisher/RedisMessagePublisherTest.java
@@ -1,12 +1,37 @@
 package faang.school.postservice.publisher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RedisMessagePublisherTest {
+    @InjectMocks
+    private RedisMessagePublisher redisMessagePublisher;
+    @Mock
+    private ObjectMapper objectMapper;
+    @Mock
+    private RedisTemplate<String, Object> redisTemplate;
 
+    @Test
+    void testPublish() throws JsonProcessingException {
+        String channel = "test-channel";
+        String json = "test-json";
 
+        when(objectMapper.writeValueAsString(json)).thenReturn(json);
+
+        redisMessagePublisher.publish(channel, json);
+
+        verify(objectMapper, times(1)).writeValueAsString(json);
+        verify(redisTemplate, times(1)).convertAndSend(channel, json);
+    }
 }
