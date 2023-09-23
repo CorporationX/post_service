1c75deeb1fb023745bf23c5a8e7c56bc6ff409db
RasulMurtuzaliev <ras8705@mail.ru>
2023-09-23 20:27:58 +0500

    added Serializable for LikeEvent

--- src/main/java/faang/school/postservice/publisher/events/LikeEvent.java
+++ src/main/java/faang/school/postservice/publisher/events/LikeEvent.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class LikeEvent {
+public class LikeEvent implements Serializable {
     private Long postId;
     private Long postAuthorId;
     private Long likeAuthorId;
