f6717b1967bfdcec7324cf336bcc1cbefef8f3e5
Pavel Voedilov <voedilov.p@gmail.com>
2023-10-14 07:03:03 +0200

    6555 - Publish post event
    Implemented Kafka & related classes + config
    Implemented Async distribute post to followers functionality
    
    Tests are pending..

---
+++ src/main/java/faang/school/postservice/messaging/publishing/PostProducer.java
@@ -0,0 +1,23 @@
+package faang.school.postservice.messaging.publishing;
+
+import faang.school.postservice.messaging.events.PostPublishedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PostProducer implements Publishable<PostPublishedEvent> {
+
+    @Value("${spring.kafka.channels.post_event_channel.name}")
+    private final String topic;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Override
+    public void publish(PostPublishedEvent event) {
+        kafkaTemplate.send(topic, event);
+    }
+}
