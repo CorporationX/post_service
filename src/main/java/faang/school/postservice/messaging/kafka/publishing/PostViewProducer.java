24fe0d027348fccdc622371068a222129de94d67
b.mukhtarkhanov <b.mukhtarkhanov>
2023-10-22 20:50:44 +0600

    post view event kafka publisher

---
+++ src/main/java/faang/school/postservice/messaging/kafka/publishing/PostViewProducer.java
@@ -0,0 +1,33 @@
+package faang.school.postservice.messaging.kafka.publishing;
+
+import faang.school.postservice.messaging.kafka.events.PostViewEvent;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PostViewProducer extends AbstractProducer<PostViewEvent> {
+
+    @Setter
+    @Value("${spring.kafka.channels.post_view_event_channel.name}")
+    private String topic;
+
+    @Autowired
+    public PostViewProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+        super(kafkaTemplate);
+    }
+
+    @Override
+    public void publish(PostViewEvent event) {
+        kafkaTemplate.send(topic, event)
+                .thenAccept(ack -> log.info("Post view event has published to kafka {}, {}", topic, event))
+                .exceptionally(e -> {
+                    log.error("Failed to publish on topic {}, event {}", topic, event);
+                    return null;
+                });
+    }
+}
