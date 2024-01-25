926ce2d1f56082c53d5a35970a95e4722900b087
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-01-26 02:58:59 +0300

    add fail test

---
+++ src/test/java/faang/school/postservice/ClassTest.java
@@ -0,0 +1,14 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Target;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ClassTest {
+    @Test
+    void test() {
+        fail();
+    }
+}
