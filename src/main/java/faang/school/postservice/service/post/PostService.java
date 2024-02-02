36cfbde3fbee65acd625bad51f6fef8efd5779da
fd_shell <dariuskpp@gmail.com>
2024-02-02 10:10:33 +0300

    Fix after merge

--- src/main/java/faang/school/postservice/service/post/PostService.java
+++ src/main/java/faang/school/postservice/service/post/PostService.java
@@ -6,11 +6,11 @@ import faang.school.postservice.dto.post.PostDto;
 import faang.school.postservice.dto.project.ProjectDto;
 import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.exception.DataValidationException;
-import faang.school.postservice.exception.EntityNotFoundException;
 import faang.school.postservice.mapper.post.PostMapper;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
 import faang.school.postservice.validator.PostValidator;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
