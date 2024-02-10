cc2725e44565bf4f28f50ce84d513dccf6157f5d
Ruslan Latyshev <latyshevruslan@ya.ru>
2024-02-10 20:47:08 +0300

    fix

--- src/main/java/faang/school/postservice/client/ProjectServiceClient.java
+++ src/main/java/faang/school/postservice/client/ProjectServiceClient.java
@@ -12,12 +12,12 @@ import java.util.List;
 @FeignClient(name = "project-service", url = "${project-service.host}:${project-service.port}")
 public interface ProjectServiceClient {
 
-    @GetMapping("/project/{projectId}")
+    @GetMapping("/api/v1/project/{projectId}")
     ProjectDto getProject(@PathVariable("projectId") long projectId);
 
-    @PostMapping("/projects")
+    @PostMapping("/api/v1/projects")
     List<ProjectDto> getProjectsByIds(@RequestBody List<Long> ids);
 
-    @PostMapping("/projects/exists/{projectId}")
+    @PostMapping("/api/v1/projects/exists/{projectId}")
     void existsProjectById(@PathVariable("projectId") long projectId);
 }
