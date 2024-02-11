b3f1429c5a71d5931321b7c4bd7b357bd800aa38
Emperor_Samedi <mexanik13970@gmail.com>
2024-02-11 15:22:08 +0300

    add TestPipeline

---
+++ src/test/java/faang/school/postservice/service/TestPipeline.java
@@ -0,0 +1,14 @@
+package faang.school.postservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestPipeline {
+
+    @Test
+    void testPipeline(){
+        fail();
+    }
+
+}
