c33ce608a3a869a84e97019e5c92f7d66e1233c3
Marina Razina <razinamv3@gmail.com>
2024-02-12 12:11:45 +0300

    edit thread pool

--- src/main/java/faang/school/postservice/config/ThreadPoolConfig.java
+++ src/main/java/faang/school/postservice/config/ThreadPoolConfig.java
@@ -2,20 +2,14 @@ package faang.school.postservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
 public class ThreadPoolConfig {
     @Bean
-    public Executor publishedPostThreadPool() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        return executor;
-//        return Executors.newScheduledThreadPool(10);
-//        return Executors.newFixedThreadPool(10);
+    public ExecutorService publishedPostThreadPool() {
+        return Executors.newFixedThreadPool(10);
     }
 }
