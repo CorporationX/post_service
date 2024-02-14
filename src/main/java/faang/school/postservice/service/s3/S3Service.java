61766338a27779dd5689536096fa9ee7e8e8fd74
phoen1x <106927036+vladmeniailov@users.noreply.github.com>
2024-02-14 21:29:39 +0300

    Install s3(minio)

---
+++ src/main/java/faang/school/postservice/service/s3/S3Service.java
@@ -0,0 +1,68 @@
+package faang.school.postservice.service.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import faang.school.postservice.model.Resource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@ConditionalOnProperty(value = "services.s3.isMocked", havingValue = "false")
+public class S3Service {
+    private final AmazonS3 s3Client;
+
+    @Value("${services.s3.bucketName}")
+    private String bucketName;
+
+    public Resource uploadFile(MultipartFile file, String folder) {
+        long fileSize = file.getSize();
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(fileSize);
+        objectMetadata.setContentType(file.getContentType());
+        String key = String.format("%s/%d%s", folder, System.currentTimeMillis(), file.getOriginalFilename());
+        try {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    bucketName, key, file.getInputStream(), objectMetadata);
+            s3Client.putObject(putObjectRequest);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        Resource resource = new Resource();
+        resource.setKey(key);
+        resource.setSize(fileSize);
+        resource.setCreatedAt(LocalDateTime.now());
+        resource.setType(file.getContentType());
+        resource.setName(file.getOriginalFilename());
+
+        return resource;
+    }
+
+    public void deleteFile(String key) {
+        s3Client.deleteObject(bucketName,key);
+    }
+
+    public InputStream downloadFile(String key) {
+        try {
+            S3Object s3Object = s3Client.getObject(bucketName,key);
+            return s3Object.getObjectContent();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
