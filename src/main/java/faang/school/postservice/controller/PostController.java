8ab1b5169b244db15da2529817482646d7c9ee97
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-02-16 21:57:00 +0300

    fix

--- src/main/java/faang/school/postservice/controller/PostController.java
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/posts")
+@RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
 
