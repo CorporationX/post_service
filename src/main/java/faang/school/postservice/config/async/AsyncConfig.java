6ec543b7cb7defb6cf7bfe6722dd73d980081f58
Marina-Zu <2311.mv@gmail.com>
2024-02-19 23:51:40 +0300

    Fixed

--- src/main/java/faang/school/postservice/config/async/AsyncConfig.java
+++ src/main/java/faang/school/postservice/config/async/AsyncConfig.java
@@ -3,7 +3,10 @@ package faang.school.postservice.config.async;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AsyncConfig {
