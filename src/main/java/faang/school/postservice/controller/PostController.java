b6e64ab972e162ad8019578e9c382a8f9c826301
Nikita <soul2009.89@gmail.com>
2024-02-21 21:41:43 +0500

    fix

---
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -0,0 +1,50 @@
+package faang.school.postservice.controller;
+
+import faang.school.postservice.dto.PostDto;
+import faang.school.postservice.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "PostController", description = "Посылаем запросы в PostController")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @Operation(
+            summary = "Создаём draft post",
+            description = "Получает PostDto и создаёт draft post"
+    )
+    @PostMapping
+    public PostDto crateDraftPost(@RequestBody @Validated PostDto postDto) {
+        return postService.createDraftPost(postDto);
+    }
+
+    @Operation(
+            summary = "Публикуем draft post",
+            description = "Получает id и публикуем post"
+    )
+    @PutMapping("/{id}")
+    public PostDto publishPost(@PathVariable Long id) {
+        return postService.publishPost(id);
+    }
+
+    @Operation(
+            summary = "Обновляем draft post",
+            description = "Получает PostDto и обновляет post"
+    )
+    @PutMapping
+    public PostDto updatePost(@RequestBody @Validated PostDto postDto) {
+        return postService.updatePost(postDto);
+    }
+}
