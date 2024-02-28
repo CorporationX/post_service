6255c057e931ff51d2587a1615a23924b465beca
Marina-Zu <2311.mv@gmail.com>
2024-02-28 20:34:41 +0300

    В процессе

---
+++ src/main/java/faang/school/postservice/publisher/AlbumCreatedEvent.java
@@ -0,0 +1,28 @@
+package faang.school.postservice.publisher;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AlbumCreatedEvent {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private final ChannelTopic topic;
+
+    public RedisMessagePublisher() {
+    }
+
+    public RedisMessagePublisher(
+            RedisTemplate<String, Object> redisTemplate, ChannelTopic topic) {
+        this.redisTemplate = redisTemplate;
+        this.topic = topic;
+    }
+
+    public void publish(String message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
