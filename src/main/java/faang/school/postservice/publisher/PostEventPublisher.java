2bf57035f055d9a8ac0c4f695e1db97ffbdaaf99
Ruslan Latyshev <latyshevruslan@ya.ru>
2024-02-29 12:45:14 +0300

    fix (add @Component annotation, ObjectMapper)

--- src/main/java/faang/school/postservice/publisher/PostEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/PostEventPublisher.java
@@ -3,16 +3,16 @@ package faang.school.postservice.publisher;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.dto.event.PostEventDto;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-
+@Component
 public class PostEventPublisher extends AbstractEventPublisher<PostEventDto> {
 
     public PostEventPublisher(ObjectMapper objectMapper, RedisTemplate redisTemplate) {
         super(objectMapper, redisTemplate);
     }
 
-    private String channelTopicName = "post_channel";
+    private String channelTopicName = "post_achievement_channel";
 
     public void publish(PostEventDto postEventDto) {
         publishInTopic(postEventDto, channelTopicName);
