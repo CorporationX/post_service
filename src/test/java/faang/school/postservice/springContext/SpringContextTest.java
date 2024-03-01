57924a6f7b976bc443f7f6f7be4476aba6afd682
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-03-01 18:20:45 +0300

    добавлен тест поднятия сринг контекста

---
+++ src/test/java/faang/school/postservice/springContext/SpringContextTest.java
@@ -0,0 +1,16 @@
+package faang.school.postservice.springContext;
+
+import faang.school.postservice.service.s3.S3Service;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class SpringContextTest {
+    @MockBean
+    private S3Service s3Service;
+
+    @Test
+    void contextLoads(){
+    }
+}
