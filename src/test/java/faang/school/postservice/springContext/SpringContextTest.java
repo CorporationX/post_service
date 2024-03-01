e53d26639a49ef90af5805baa2c806ae9069a263
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-03-01 18:45:24 +0300

    добавлен тест поднятия сринг контекста

--- src/test/java/faang/school/postservice/springContext/SpringContextTest.java
+++ src/test/java/faang/school/postservice/springContext/SpringContextTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class SpringContextTest {
     @MockBean
-    private S3Service s3Service;
+    private S3Service clientAmazonS3;
 
     @Test
     void contextLoads(){
