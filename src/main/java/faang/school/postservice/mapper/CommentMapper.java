7b4e76025c3e427851ad069cfe195d6e66edb877
InnMish <inn.mishustina@gmail.com>
2024-03-07 18:44:18 +0300

    refactor

--- src/main/java/faang/school/postservice/mapper/CommentMapper.java
+++ src/main/java/faang/school/postservice/mapper/CommentMapper.java
@@ -1,6 +1,6 @@
 package faang.school.postservice.mapper;
 
-import faang.school.postservice.dto.CommentDto;
+import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.dto.event.CommentEventDto;
 import faang.school.postservice.model.Comment;
 import org.mapstruct.Mapper;
--- src/test/java/faang/school/postservice/mapper/CommentMapperTest.java
+++ src/test/java/faang/school/postservice/mapper/CommentMapperTest.java
@@ -2,8 +2,6 @@ package faang.school.postservice.mapper;
 
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.dto.comment.CommentEditDto;
-import faang.school.postservice.dto.CommentDto;
-import faang.school.postservice.dto.CommentEditDto;
 import faang.school.postservice.dto.event.CommentEventDto;
 import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Post;
