8f28baf54cc094ca9cfd6a957777ffb017913fd3
Marina-Zu <2311.mv@gmail.com>
2024-03-11 15:57:36 +0300

    Fixed

--- src/main/java/faang/school/postservice/service/PostService.java
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -25,7 +25,7 @@ public class PostService {
 
     @Transactional
     public PostDto createDraft(PostDto postDto) {
-        validatedAuthor(postDto);
+        validateAuthor(postDto);
         Post post = postMapper.toEntity(postDto);
         postRepository.save(post);
         return postMapper.toDto(post);
@@ -89,7 +89,7 @@ public class PostService {
         return filterPosts(posts, true);
     }
 
-    private void validatedAuthor(PostDto postDto) {
+    private void validateAuthor(PostDto postDto) {
         if (postDto.getAuthorId() == null && postDto.getProjectId() == null) {
             throw new DataValidationException("The author of the post is not specified");
         }
