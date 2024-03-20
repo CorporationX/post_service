d190cbd9dd88758e8c46ccd8c5e16becf4ef81ed
Bukevsky | Kirill <bykaplay@gmail.com>
2024-03-20 18:23:29 +0300

    completed

---
+++ src/main/java/faang/school/postservice/controller/like/LikeController.java
@@ -0,0 +1,28 @@
+package faang.school.postservice.controller.like;
+
+import faang.school.postservice.config.context.UserContext;
+import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.service.like.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.control.MappingControl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class LikeController {
+    private final LikeService likeService;
+
+    @GetMapping("/post/{postId}")
+    public List<UserDto> getUsersLikedPost(@PathVariable Long postId){
+        return likeService.getUsersLikedPost(postId);
+    }
+
+    @GetMapping("/comment/{commentId}")
+    public List<UserDto> getUsersLikedComment(@PathVariable Long commentId){
+        return likeService.getUsersLikedComment(commentId);
+    }
+}
