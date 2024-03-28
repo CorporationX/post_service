bc99de218a9a9077aa4511517a7547f6bcf4c0e2
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-03-28 17:18:44 +0600

    Spring context test

---
+++ src/test/java/faang/school/postservice/SpringContextTest.java
@@ -0,0 +1,23 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = PostServiceApp.class)
+@Testcontainers
+public class SpringContextTest {
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    @DisplayName("Spring context test check")
+    public void contextLoads() {
+        assertNotNull( applicationContext );
+    }
+}
