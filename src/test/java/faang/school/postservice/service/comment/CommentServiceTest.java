322cdd58c49977e006cb5a861c5b36483d5e73d1
Viktor Dyukov <snailrake@mail.ru>
2024-04-02 20:44:27 +0400

    test: add commenter banner test

--- src/test/java/faang/school/postservice/service/comment/CommentServiceTest.java
+++ src/test/java/faang/school/postservice/service/comment/CommentServiceTest.java
@@ -1,4 +1,4 @@
-package faang.school.postservice.service;
+package faang.school.postservice.service.comment;
 
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.mapper.CommentMapperImpl;
@@ -6,7 +6,6 @@ import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.CommentRepository;
 import faang.school.postservice.repository.PostRepository;
-import faang.school.postservice.service.comment.CommentService;
 import faang.school.postservice.validation.user.UserValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -84,6 +83,13 @@ class CommentServiceTest {
         commentService.deleteComment(getId());
 
         verify(commentRepository, times(1)).deleteById(anyLong());
+    }
+
+    @Test
+    void findCommentsByVerified_ValidArgs() {
+        commentService.findCommentsByVerified(true);
+
+        verify(commentRepository, times(1)).findByVerified(true);
     }
 
     private List<Comment> getComments() {
