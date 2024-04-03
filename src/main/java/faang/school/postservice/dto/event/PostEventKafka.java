6feed65e80db9c867df58e92c57188383c78303f
Ackerman995 <rauanture@gmail.com>
2024-04-03 18:44:35 +0500

    add KafkaPostProducer

---
+++ src/main/java/faang/school/postservice/dto/event/PostEventKafka.java
@@ -0,0 +1,35 @@
+package faang.school.postservice.dto.event;
+
+import faang.school.postservice.model.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostEventKafka {
+    private String content;
+    private Long authorId;
+    private Long postId;
+    private Long projectId;
+    private List<Long> followerIds;
+    private LocalDateTime publishedAt;
+    private LocalDateTime updatedAt;
+    private Long likeCount;
+
+    public PostEventKafka(Post post, List<Long> followerIds) {
+        this.content = post.getContent();
+        this.authorId = post.getAuthorId();
+        this.postId = post.getId();
+        this.projectId = post.getProjectId();
+        this.updatedAt = post.getUpdatedAt();
+        this.likeCount = (long) post.getLikes().size();
+        this.followerIds = followerIds;
+    }
+}
