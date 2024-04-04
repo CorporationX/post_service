ba508488dddef82c1cbf445346883836b58e57ca
Edisolt Khatsiev <Edisolt Khatsiev>
2024-04-04 19:00:12 +0300

    done. need refactor and tests

---
+++ src/main/java/faang/school/postservice/dto/redis/UserRedisDto.java
@@ -0,0 +1,28 @@
+package faang.school.postservice.dto.redis;
+
+import faang.school.postservice.config.RedisConfig;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@RedisHash("User")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRedisDto implements Serializable {
+    @TimeToLive
+    @Value("${spring.data.redis.cache.ttl.user}")
+    private int ttl;
+
+    @Id
+    private Long id;
+    private String name;
+}
