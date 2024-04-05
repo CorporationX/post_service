2c54cd606133a60d6ee7521e782adac5c0fa1291
Ackerman995 <rauanture@gmail.com>
2024-04-05 22:46:42 +0500

    Save author of comment

--- src/main/java/faang/school/postservice/consumer/KafkaCommentConsumer.java
+++ src/main/java/faang/school/postservice/consumer/KafkaCommentConsumer.java
@@ -1,8 +1,8 @@
 package faang.school.postservice.consumer;
 
 import faang.school.postservice.dto.event.CommentEventKafka;
-import faang.school.postservice.dto.event.PostEventKafka;
 import faang.school.postservice.service.hash.PostHashService;
+import faang.school.postservice.service.hash.UserHashService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -12,10 +12,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KafkaCommentConsumer {
     private final PostHashService postHashService;
+    private final UserHashService userHashService;
 
     @KafkaListener(topics = "${spring.kafka.topics.comment.name}")
     public void listen(CommentEventKafka commentEventKafka, Acknowledgment acknowledgment) {
         postHashService.addComment(commentEventKafka);
+        userHashService.saveAuthor(commentEventKafka.getUserDto());
         acknowledgment.acknowledge();
     }
 }
