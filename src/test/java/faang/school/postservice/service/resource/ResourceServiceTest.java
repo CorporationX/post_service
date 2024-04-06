e3de84c69041c6d16cd814e31b018b8a679bca0c
Viktor Dyukov <snailrake@mail.ru>
2024-04-06 10:30:08 +0400

    test: add resource tests

--- src/test/java/faang/school/postservice/service/resource/ResourceServiceTest.java
+++ src/test/java/faang/school/postservice/service/resource/ResourceServiceTest.java
@@ -2,16 +2,20 @@ package faang.school.postservice.service.resource;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import faang.school.postservice.dto.resource.ResourceDto;
+import faang.school.postservice.image.ImageResizer;
+import faang.school.postservice.mapper.resource.ResourceMapperImpl;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.model.resource.Resource;
 import faang.school.postservice.repository.ResourceRepository;
-import faang.school.postservice.image.ImageResizer;
 import faang.school.postservice.validation.resource.ResourceValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,6 +28,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyLong;
@@ -43,6 +48,8 @@ class ResourceServiceTest {
     private ImageResizer imageResizer;
     @Mock
     private ResourceValidator resourceValidator;
+    @Spy
+    private ResourceMapperImpl resourceMapper;
     @InjectMocks
     private ResourceService resourceService;
 
@@ -80,5 +87,26 @@ class ResourceServiceTest {
         resourceService.deleteResource(1L);
 
         verify(amazonS3Client, times(1)).deleteObject(anyString(), anyString());
+    }
+
+    @Test
+    void attachMediaToPost_ValidArgs() {
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "test.mpeg", "video/mpeg", "test.mpeg".getBytes());
+        Post post = getPost();
+
+        ResourceDto resource = resourceService.attachMediaToPost(mockMultipartFile, post);
+
+        assertEquals(post.getId(), resource.getPostId());
+        verify(resourceValidator, times(1)).validateAudioOrVideoFileSize(any(MultipartFile.class));
+        verify(resourceValidator, times(1)).validateTypeAudioOrVideo(any(MultipartFile.class));
+        verify(amazonS3Client, times(1)).putObject(any(PutObjectRequest.class));
+        verify(resourceRepository, times(1)).save(any(Resource.class));
+        verify(resourceMapper, times(1)).toDto(any(Resource.class));
+    }
+
+    private Post getPost() {
+        return Post.builder()
+                .id(1L)
+                .build();
     }
 }
