e9a9cd2657b9a71253df64184998e07f6317833d
Edisolt Khatsiev <Edisolt Khatsiev>
2024-04-07 16:17:01 +0300

    PostViewedProducer fixed
    tests to fix

--- src/main/java/faang/school/postservice/publisher/kafka/KafkaCommentProducer.java
+++ src/main/java/faang/school/postservice/publisher/kafka/KafkaCommentProducer.java
@@ -1,18 +1,21 @@
 package faang.school.postservice.publisher.kafka;
 
 import faang.school.postservice.dto.kafka.CommentPublishedEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
 public class KafkaCommentProducer extends AbstractKafkaEventProducer<CommentPublishedEvent>{
+    @Value("${kafka.topics.comment.name}")
+    private String commentTopic;
     public KafkaCommentProducer(KafkaTemplate<String, Object> kafkaTemplate) {
         super(kafkaTemplate);
     }
 
     public void publish (long postId, long commentId, long commentOwnerId) {
-        CommentPublishedEvent.builder()
+        send(commentTopic, CommentPublishedEvent.builder()
                 .postId(postId)
                 .commentId(commentId)
                 .commentOwnerId(commentOwnerId)
-                .build();
+                .build());
     }
 }
