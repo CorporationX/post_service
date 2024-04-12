12d12bcd41a259618890c9fb319022e6a30db897
Viktor Dyukov <snailrake@mail.ru>
2024-04-12 12:42:51 +0400

    refactor: minor changes

--- src/main/java/faang/school/postservice/config/kafka/KafkaTopicConfig.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaTopicConfig.java
@@ -1,33 +1,15 @@
 package faang.school.postservice.config.kafka;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
 
-    @Value("${spring.data.kafka.host}")
-    private String host;
-
-    @Value("${spring.data.kafka.port}")
-    private int port;
-
     @Value("${spring.data.kafka.channels.comment-channel.name}")
     private String commentEventChannel;
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, String.format("%s:%s", host, port));
-        return new KafkaAdmin(configs);
-    }
 
     @Bean
     public NewTopic commentEventTopic() {
