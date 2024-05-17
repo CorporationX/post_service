5cc49e9f07985ba07454db749e56da8279303354
nadir <den4ik.strelok@gmail.com>
2024-05-18 01:24:52 +0300

    Реализовал требуемые действия без тестов

---
+++ src/main/java/faang/school/postservice/exception/post/PostOperationException.java
@@ -0,0 +1,7 @@
+package faang.school.postservice.exception.post;
+
+public class PostOperationException extends RuntimeException{
+    public PostOperationException(String message) {
+        super(message);
+    }
+}
