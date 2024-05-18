4988153f1b126a4ef7a47d6e79504a17ca30e043
KuvarzinAlex <fargus.wow@mail.ru>
2024-05-18 13:33:57 +0500

    feature-BJS2-6556 Swagger для post_service. Подключил зависимости, проверил работу метода с помощью swagger(2).

--- src/main/java/faang/school/postservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/postservice/dto/project/ProjectDto.java
@@ -1,8 +1,10 @@
 package faang.school.postservice.dto.project;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ProjectDto {
     private long id;
     private String title;
