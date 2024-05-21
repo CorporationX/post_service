48a41cc5000eb38538f2b4aa9cc00b77546276b9
Ikhsanov Nail <ikhsanov.nail@outlook.com>
2024-05-21 07:48:31 +0500

    Add requests to PostController

--- src/main/java/faang/school/postservice/controller/PostController.java
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -3,11 +3,15 @@ package faang.school.postservice.controller;
 import faang.school.postservice.dto.PostDto;
 import faang.school.postservice.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
@@ -20,10 +24,20 @@ public class PostController {
     @PostMapping("/create")
     @Operation(
             summary = "Creating a draft post",
-            description = "Exactly one author user or project creates a draft of the post"
+            description = "Exactly one author user or project creates a draft of the post",
+            parameters = {@Parameter(in = ParameterIn.HEADER, name = "x-user-id", description = "User ID", required = true)}
     )
-    public PostDto createPost() {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The post has been created"),
+            @ApiResponse(responseCode = "500", description = "There is no access to the database of users or projects")
+    })
+    public PostDto createPost(@Valid @RequestBody PostDto postDto) {
+        return postService.createPost(postDto);
+    }
 
+
+    public PostDto publishPost(@PathVariable @Min(1) long postId) {
+        return postService.publishPost(postId);
     }
 
 }
--- src/main/java/faang/school/postservice/service/PostService.java
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -1,9 +1,11 @@
 package faang.school.postservice.service;
 
+import faang.school.postservice.dto.PostDto;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
@@ -12,6 +14,19 @@ import java.util.NoSuchElementException;
 public class PostService {
 
     private final PostRepository postRepository;
+
+    @Transactional
+    public PostDto createPost(PostDto postDto) {
+        return
+    }
+
+    @Transactional
+    public PostDto publishPost(long id) {
+        return
+    }
+
+
+
 
     public Post getPost(Long postId){
         return  postRepository.findById(postId).orElseThrow(NoSuchElementException::new);
