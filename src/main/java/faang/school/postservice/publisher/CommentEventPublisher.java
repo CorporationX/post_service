80b161b6bf1fe6f96f0efb8271628dd0f9f3c413
Sergey Ryabov <mre@mail.ru>
2024-05-26 21:57:29 +0800

    conflicts resolving

--- src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
@@ -1,13 +1,13 @@
 package faang.school.postservice.publisher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.postservice.dto.comment.CommentEventDto;
+import faang.school.postservice.dto.comment.CommentEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CommentEventPublisher extends MessagePublisher<CommentEventDto> {
+public class CommentEventPublisher extends MessagePublisher<CommentEvent> {
 
     public CommentEventPublisher(RedisTemplate<String, Object> redisTemplate,
                                  ObjectMapper jsonMapper,
