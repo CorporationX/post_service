f6b961b32af555151c743a4afd0a1247ec9c9452
Sergey Ryabov <mre@mail.ru>
2024-05-26 22:38:46 +0800

    conflicts resolving

--- src/test/java/faang/school/postservice/service/CommentServiceTest.java
+++ src/test/java/faang/school/postservice/service/CommentServiceTest.java
@@ -4,7 +4,7 @@ import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.mapper.CommentMapper;
 import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Post;
-import faang.school.postservice.publishers.CommentEventPublisher;
+import faang.school.postservice.publisher.CommentEventPublisher;
 import faang.school.postservice.repository.CommentRepository;
 import faang.school.postservice.validator.CommentValidation;
 import org.junit.Assert;
