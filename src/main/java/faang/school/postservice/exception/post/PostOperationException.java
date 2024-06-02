508fdfb9aa81c2cf030ae0e98bca83b32046aef8
nadir <den4ik.strelok@gmail.com>
2024-06-02 09:04:59 +0300

    Изменил названия исключений на более общие и перенес енамы с сообщениями об ошибках в отдельный подпакет

--- src/main/java/faang/school/postservice/exception/post/PostOperationException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.postservice.exception.post;
-
-public class PostOperationException extends RuntimeException{
-    public PostOperationException(String message) {
-        super(message);
-    }
-}
