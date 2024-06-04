88375063b71e4cbabcdf034ecb3b967c8ff765f8
Rick <RickHammerson@ya.ru>
2024-06-04 12:17:12 +0300

    Задача BJS2-6748 Обработчик исключений в post_service

--- src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
@@ -16,8 +16,8 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(LikeOperatingException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleLikeOperatingException(LikeOperatingException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
@@ -42,5 +42,11 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleConstraintViolationException(ConstraintViolationException constraintViolationException) {
         return new ErrorResponse(constraintViolationException.getMessage());
+    }
+    
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity handleRuntimeExceptions(RuntimeException ex) {
+        return ResponseEntity.internalServerError().body(String.format("Ошибка на стороне сервера: %s", ex.getMessage()));
     }
 }
