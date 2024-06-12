f9c73ad2579488c5d2940a3f863535c8f3d15c13
Alex <153853667+Fargus74@users.noreply.github.com>
2024-06-12 15:14:20 +0500

    feature-7236 Анализ лайков. Поправил PostService перед реализацией Redis

--- src/main/java/faang/school/postservice/client/UserServiceClient.java
+++ src/main/java/faang/school/postservice/client/UserServiceClient.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
+@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}/api/v1")
 public interface UserServiceClient {
 
     @GetMapping("/users/{userId}")
