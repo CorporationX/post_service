4c41c02706280f99c81e413ea0fbab4eb4a1c44d
nadir <den4ik.strelok@gmail.com>
2024-06-17 14:16:30 +0300

    поправил эндпоинт existsById

--- src/main/java/faang/school/postservice/client/ProjectServiceClient.java
+++ src/main/java/faang/school/postservice/client/ProjectServiceClient.java
@@ -12,12 +12,12 @@ import java.util.List;
 @FeignClient(name = "project-service", url = "${project-service.host}:${project-service.port}")
 public interface ProjectServiceClient {
 
-    @GetMapping("/project/{projectId}")
+    @GetMapping("/projects/{projectId}")
     ProjectDto getProject(@PathVariable long projectId);
 
     @PostMapping("/projects")
     List<ProjectDto> getProjectsByIds(@RequestBody List<Long> ids);
 
-    @GetMapping("/project/{projectId}/exists")
+    @GetMapping("/projects/{projectId}/exists")
     boolean existsById(@PathVariable long projectId);
 }
