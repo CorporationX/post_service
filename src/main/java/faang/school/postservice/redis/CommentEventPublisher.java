47d2f4ed6387379418f69543f31b88cdb8459477
nadir <den4ik.strelok@gmail.com>
2024-06-19 09:01:55 +0300

    Исправил ошибку в конструкторе паблишера

--- src/main/java/faang/school/postservice/redis/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/redis/CommentEventPublisher.java
@@ -1,5 +1,6 @@
 package faang.school.postservice.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CommentEventPublisher extends AbstractEventPublisher {
-    public CommentEventPublisher(RedisTemplate<String, Object> redisTemplate, @Qualifier("commentTopic") ChannelTopic topic) {
-        super(redisTemplate, topic);
+    public CommentEventPublisher(ObjectMapper objectMapper,
+                                 RedisTemplate<String, Object> redisTemplate,
+                                 @Qualifier("commentTopic") ChannelTopic topic) {
+        super(objectMapper, redisTemplate, topic);
     }
 }
--- src/main/java/faang/school/postservice/redis/LikeEventPublisher.java
+++ src/main/java/faang/school/postservice/redis/LikeEventPublisher.java
@@ -1,5 +1,6 @@
 package faang.school.postservice.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LikeEventPublisher extends AbstractEventPublisher {
-    public LikeEventPublisher(RedisTemplate<String, Object> redisTemplate, @Qualifier("likeTopic") ChannelTopic topic) {
-        super(redisTemplate, topic);
+    public LikeEventPublisher(ObjectMapper objectMapper,
+                              RedisTemplate<String, Object> redisTemplate,
+                              @Qualifier("likeTopic") ChannelTopic topic) {
+        super(objectMapper, redisTemplate, topic);
     }
 }
