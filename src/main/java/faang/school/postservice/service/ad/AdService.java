50490b0b929189511d4111f39c3a8947c30392db
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-06-28 00:20:44 +0800

    feature-BJS2-7000

---
+++ src/main/java/faang/school/postservice/service/ad/AdService.java
@@ -0,0 +1,48 @@
+package faang.school.postservice.service.ad;
+
+import faang.school.postservice.model.ad.Ad;
+import faang.school.postservice.repository.ad.AdRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.collections4.ListUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdService {
+    private final AdRepository adRepository;
+
+    @Value("${post.ad-remover.batch-size}")
+    @Setter
+    private int batchSize;
+
+    public List<Ad> findAll() {
+        return adRepository.findAll();
+    }
+
+    public void deleteByIds(List<Long> ids) {
+        adRepository.deleteByIds(ids);
+    }
+
+    @Async
+    public void deleteExpiredAds() {
+        List<Long> idsOfExpiredAds = filteringExpiredAds(findAll());
+        if (!idsOfExpiredAds.isEmpty()) {
+            List<List<Long>> expiresAdsByBatches = ListUtils.partition(idsOfExpiredAds, batchSize);
+            expiresAdsByBatches.parallelStream().forEach(this::deleteByIds);
+        }
+    }
+
+    private List<Long> filteringExpiredAds(List<Ad> ads) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        return ads.stream()
+                .filter(ad -> ad.getAppearancesLeft() == 0 || ad.getEndDate().isBefore(currentDateTime))
+                .map(Ad::getId)
+                .toList();
+    }
+}
---
+++ src/main/java/faang/school/postservice/service/ad/ScheduledExpiredAdRemover.java
@@ -0,0 +1,16 @@
+package faang.school.postservice.service.ad;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduledExpiredAdRemover {
+    private final AdService adService;
+
+    @Scheduled(cron = "${post.ad-remover.cron}")
+    public void deleteExpiredAds() {
+        adService.deleteExpiredAds();
+    }
+}
