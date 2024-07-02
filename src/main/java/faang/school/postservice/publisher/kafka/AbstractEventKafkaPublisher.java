8ddf4225478fe0bbb251e9359ddbec148bc99688
DadyaVolodya <pl4stmass@yandex.ru>
2024-07-02 14:26:48 +0300

    add abstract kafka publisher , event , logic in service , kafka config and properties. Refactoring comment event

--- src/main/java/faang/school/postservice/publisher/kafka/AbstractEventKafkaPublisher.java
+++ src/main/java/faang/school/postservice/publisher/kafka/AbstractEventKafkaPublisher.java
@@ -2,11 +2,19 @@ package faang.school.postservice.publisher.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.postservice.client.UserServiceClient;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ListUtils;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.errors.SerializationException;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.annotation.Retryable;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,5 +31,28 @@ public abstract class AbstractEventKafkaPublisher<T> implements MessagePublisher
         } catch (JsonProcessingException e) {
             throw new SerializationException(e.getMessage());
         }
+    }
+    private final UserServiceClient userServiceClient;
+
+    public PostKafkaPublisher(KafkaTemplate<String, Object> kafkaTemplate, UserServiceClient userServiceClient) {
+        super(kafkaTemplate);
+        this.userServiceClient = userServiceClient;
+    }
+
+    @Retryable(retryFor = FeignException.class, maxAttempts = 5)
+    public void publish(long postId, long ownerId, LocalDateTime publishedAt, KafkaKey kafkaKey) {
+        List<Long> followers = userServiceClient.getFollowerIdsById(ownerId);
+
+        ListUtils.partition(followers, batchSize).forEach(followersPartition -> {
+                    PostEventDto postEventDto = PostEventDto.builder()
+                            .postId(postId)
+                            .authorId(ownerId)
+                            .publishedAt(publishedAt)
+                            .authorSubscriberIds(new HashSet<>(followersPartition))
+                            .build();
+
+                    send(postTopic, kafkaKey, postEventDto);
+                }
+        );
     }
 }
--- src/main/java/faang/school/postservice/publisher/kafka/post/PostEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/kafka/post/PostEventPublisher.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.publisher.kafka.post;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.event.post.PostEvent;
 import faang.school.postservice.publisher.kafka.AbstractEventKafkaPublisher;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PostEventPublisher extends AbstractEventKafkaPublisher<PostEvent> {
-    public PostEventPublisher(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper, NewTopic posts) {
-        super(kafkaTemplate, objectMapper, posts);
+
+    public PostEventPublisher(KafkaTemplate<String, String> kafkaTemplate, UserServiceClient userServiceClient) {
+        super(kafkaTemplate,userServiceClient);
+
     }
 }
