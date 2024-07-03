58a932fa81aed68eb28d205b810f95e2d6f269d0
DadyaVolodya <pl4stmass@yandex.ru>
2024-07-03 14:34:37 +0300

    add abstract kafka publisher , event , logic in service , kafka config and properties. Refactoring comment event

--- src/main/java/faang/school/postservice/event/post/PostEventDto.java
+++ src/main/java/faang/school/postservice/event/post/PostEventDto.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostEventDto {
-    List<Long> followers;
+    private Set<Long> authorSubscriberIds = new HashSet<>();
 }
--- src/main/java/faang/school/postservice/service/kafka/KafkaPostService.java
+++ src/main/java/faang/school/postservice/service/kafka/KafkaPostService.java
@@ -1,11 +1,25 @@
 package faang.school.postservice.service.kafka;
 
+import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.event.post.PostEventDto;
+import faang.school.postservice.publisher.kafka.post.PostEventPublisher;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
+@RequiredArgsConstructor
 public class KafkaPostService {
 
-    public void sendPostToKafka(){
+    private final UserServiceClient userServiceClient;
+    private final PostEventPublisher postEventPublisher;
 
+    public void sendPostToKafka(){
+        Set<Long> followers = userServiceClient.getFollowersById();
+        PostEventDto postEventDto = PostEventDto.builder()
+                .authorSubscriberIds()
+                .build();
+        postEventPublisher.publish(postEventDto);
     }
 }
