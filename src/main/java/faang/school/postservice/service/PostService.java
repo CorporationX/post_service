68b9fba75d6c1ac6e8cfac876814afddd010b336
Nikita Volzhin <vlzh.nik@mail.ru>
2024-07-16 14:01:49 +0500

    BJS2-15594

--- src/main/java/faang/school/postservice/service/PostService.java
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -9,7 +9,6 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 
