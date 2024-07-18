6196a1d7105d78b4e7b0abac453a20436e22d9e4
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-07-18 16:33:14 +0500

    Внес корректировки

--- src/main/java/faang/school/postservice/controller/comment/CommentController.java
+++ src/main/java/faang/school/postservice/controller/comment/CommentController.java
@@ -5,7 +5,6 @@ import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.service.comment.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,24 +15,24 @@ import java.util.List;
 @RequestMapping(ApiPath.COMMENT)
 public class CommentController {
     private final CommentService commentService;
-    // postman проверено
+
     @PostMapping("/post/{id}")
     public ResponseEntity<CommentDto> addCommentController(@PathVariable Long id,
                                                            @RequestBody @Valid CommentDto commentDto) {
         return ResponseEntity.ok(commentService.addCommentService(id, commentDto));
     }
-    // postman проверено
+
     @PutMapping("/update/{id}")
     public ResponseEntity<CommentDto> updateCommentController(@PathVariable Long id,
                                                               @RequestBody @Valid CommentDto commentDto) {
         return ResponseEntity.ok(commentService.updateCommentService(id, commentDto));
     }
-    // postman проверено
+
     @GetMapping("/get/{id}")
     public ResponseEntity<List<CommentDto>> getCommentsController(@PathVariable Long id) {
         return ResponseEntity.ok(commentService.getCommentsService(id));
     }
-    // postman проверено
+
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<CommentDto> deleteCommentController(@PathVariable Long id,
                                                               @RequestBody @Valid CommentDto commentDto) {
