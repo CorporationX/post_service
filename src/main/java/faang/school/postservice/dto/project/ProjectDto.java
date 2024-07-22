211b2721b9253c099a0abc4a6f00251a24dabfce
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-07-23 00:59:29 +0500

    BJS2-15608 - Валидация в сервисе Постов
    + Провалидировал поля Dtoшек

--- src/main/java/faang/school/postservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/postservice/dto/project/ProjectDto.java
@@ -1,9 +1,16 @@
 package faang.school.postservice.dto.project;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ProjectDto {
     private long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 128)
     private String title;
 }
--- src/main/java/faang/school/postservice/dto/user/UserDto.java
+++ src/main/java/faang/school/postservice/dto/user/UserDto.java
@@ -1,5 +1,9 @@
 package faang.school.postservice.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,6 +11,15 @@ import lombok.Data;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 64)
     private String username;
+
+    @NotNull
+    @NotBlank
+    @Email
+    @Size(max = 64)
     private String email;
 }
