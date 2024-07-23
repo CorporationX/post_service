5285104ec5a65ba2ebe98ab565f23c86fcc01a8b
sta1er <vlad.sokogubov@gmail.com>
2024-07-23 19:24:52 +0300

    Add failing test

---
+++ src/test/java/faang/school/postservice/FailingTest.java
@@ -0,0 +1,13 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Fail.fail;
+
+public class FailingTest {
+
+    @Test
+    public void testThatFails() {
+        fail("This test is designed to fail");
+    }
+}
