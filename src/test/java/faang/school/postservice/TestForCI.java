25c2ca013487eb8f9ce9b217e0aedf1fd4256eeb
l0zain <nanoowood@gmail.com>
2024-07-24 13:54:07 +0300

    test methos

---
+++ src/test/java/faang/school/postservice/TestForCI.java
@@ -0,0 +1,12 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestForCI {
+    @Test
+    public void failTest() {
+        fail("This is fail test");
+    }
+}
