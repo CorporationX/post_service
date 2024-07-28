ac0a1b4ea68714527d7b76324e24fdac5730df47
sta1er <vlad.sokogubov@gmail.com>
2024-07-28 15:09:35 +0300

    ContextTest

---
+++ src/test/java/faang/school/postservice/ContextTest.java
@@ -0,0 +1,12 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContextTest {
+    @Test
+    public void contextTest() {
+
+    }
+}
