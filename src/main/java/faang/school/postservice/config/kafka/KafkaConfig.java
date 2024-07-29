d58d8ca8d6a7a6f976fb54248447e3afda790a77
Roman Antonov <romzesvxo@mail.ru>
2024-07-29 23:06:34 +0300

    News_Feed_Roma_17379

--- src/main/java/faang/school/postservice/config/kafka/KafkaConfig.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaConfig.java
@@ -30,6 +30,9 @@ public class KafkaConfig {
     @Value("${spring.data.kafka.topics.likes.name}")
     private String likesTopic;
 
+    @Value("${spring.data.kafka.topics.post_views.name}")
+    private String postViewsTopic;
+
     @Bean
     public NewTopic posts(){
         return TopicBuilder.name(postTopic).build();
@@ -43,6 +46,11 @@ public class KafkaConfig {
     @Bean
     public NewTopic likes(){
         return TopicBuilder.name(likesTopic).build();
+    }
+
+    @Bean
+    public NewTopic postViews(){
+        return TopicBuilder.name(postViewsTopic).build();
     }
 
     @Bean
