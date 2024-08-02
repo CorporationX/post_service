fd7d06119040735d174fdcd057783920b95efce2
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-08-02 18:51:43 +0300

    feature-BJS2-16748 - GlobalExceptionHandler для PostService

---
+++ src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package faang.school.postservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRunTimeException(
+            RuntimeException exception) {
+        System.out.println("RunTimeException occurred: " + exception.getMessage());
+        return new ResponseEntity<>(
+                "RunTime error occurred: " + exception.getMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
+}
