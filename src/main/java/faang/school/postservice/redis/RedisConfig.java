36d4cde1feec28c7973ef8dd5b2ed1544a88142b
Artem <xotabggg@gmail.com>
2024-08-02 19:13:06 +0700

    BJS2-15642

--- src/main/java/faang/school/postservice/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/redis/RedisConfig.java
@@ -2,7 +2,7 @@ package faang.school.postservice.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -11,7 +11,12 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
@@ -27,29 +32,4 @@ public class RedisConfig {
     public MessagePublisher redisPublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic topic) {
         return new RedisMessagePublisher(redisTemplate, topic);
     }
-
-/** v2
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory() {
-//        return new JedisConnectionFactory();
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
-//        return template;
-//    }
-//
-//    @Bean
-//    public ChannelTopic topic() {
-//        return new ChannelTopic("user_ban");
-//    }
-//
-//    @Bean
-//    public MessagePublisher redisPublisher() {
-//        return new RedisMessagePublisher(redisTemplate(), topic());
-//    }
- */
 }
--- src/main/java/faang/school/postservice/redis/RedisMessagePublisher.java
+++ src/main/java/faang/school/postservice/redis/RedisMessagePublisher.java
@@ -16,5 +16,3 @@ public class RedisMessagePublisher implements MessagePublisher {
         redisTemplate.convertAndSend(topic.getTopic(), message);
     }
 }
-
-
