1c176c5509d59cb22a3b159bc84cd4ae01462566
Caveri95 <ura22051995@gmail.com>
2024-08-04 23:35:32 +0300

    BJS2-15601
    - Написана документация API

--- src/main/java/faang/school/postservice/controller/CommentController.java
+++ src/main/java/faang/school/postservice/controller/CommentController.java
@@ -2,6 +2,13 @@ package faang.school.postservice.controller;
 
 import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -13,29 +20,55 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/comments")
+@Tag(name = "Комментарии", description = "Контроллер для работы над комментариями")
 public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Создать комментарий", description = "Введите данные комментария")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Комментарий создан", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CommentDto.class)))}),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
+
+    })
     public CommentDto createComment(@Valid @RequestBody CommentDto commentDto){
         return commentService.createComment(commentDto);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Обновить комментарий", description = "Введите данные комментария, чтобы обновить его содержание")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Комментарий обновлен", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CommentDto.class)))}),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
+
+    })
     public CommentDto updateComment(@Valid @RequestBody CommentDto commentDto){
         return commentService.updateComment(commentDto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Удалить комментарий", description = "Введите номер комментария, который нужно удалить")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Комментарий удален"),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
+
+    })
     public void deleteComment(@Positive @PathVariable long id){
         commentService.deleteComment(id);
     }
 
     @GetMapping("/posts/{id}")
+    @Operation(summary = "Найти все комментарии", description = "Введите идентификатор поста, чтобы найти все его комментарии")
     @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Комментарии получены"),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
+    })
     public void findAllByPostId(@Positive @PathVariable long id){
         commentService.findAllByPostId(id);
     }
--- src/main/java/faang/school/postservice/controller/like/LikeController.java
+++ src/main/java/faang/school/postservice/controller/like/LikeController.java
@@ -2,6 +2,13 @@ package faang.school.postservice.controller.like;
 
 import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.service.LikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,16 +20,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/like")
 @RequiredArgsConstructor
+@Tag(name = "Система лайков", description = "Контроллер для работы с лайками")
 public class LikeController {
 
     private final LikeService likeService;
 
     @GetMapping("/post/{postId}")
+    @Operation(summary = "Получить лайки поста", description = "Введите идентификатор поста, чтобы получить лайки поста")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Пользователи получены", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UserDto.class)))}),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
+    })
     public List<UserDto> getLikesUsersByPostId(@PathVariable long postId) {
         return likeService.getLikesUsersByPostId(postId);
     }
 
     @GetMapping("/comment/{commentId}")
+    @Operation(summary = "Получить список пользователей, лайкнувших коммент",
+            description = "Введите идентификатор коммента, чтобы получить список пользователей, лайкнувших коммент")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Пользователи получены", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UserDto.class)))}),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка, не зависящая от вызывающей стороны")
+    })
     public List<UserDto> getLikesUsersByCommentId(@PathVariable long commentId) {
         return likeService.getLikesUsersByCommentId(commentId);
     }
