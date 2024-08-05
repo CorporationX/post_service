0cfa54d747000df6aceee0950ac64e60e34bcb01
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-05 05:43:08 +0300

    Add validation descriptions

--- src/main/java/faang/school/postservice/dto/post/DraftPostDto.java
+++ src/main/java/faang/school/postservice/dto/post/DraftPostDto.java
@@ -1,7 +1,11 @@
 package faang.school.postservice.dto.post;
 
+import faang.school.postservice.exception.exceptionmessages.ValidationExceptionMessage;
+import faang.school.postservice.exception.validation.DataValidationException;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +21,20 @@ import java.util.List;
 public class DraftPostDto {
     private Long authorId;
     private Long projectId;
-    @NotBlank
-    @NotNull
+    @NotBlank(
+            message = "The content of the post cannot consist of white separators.",
+            groups = DataValidationException.class
+    )
+    @NotEmpty(
+            message = "The content of the post cannot be empty.",
+            groups = DataValidationException.class
+    )
     private String content;
+    @Size(
+            max = 10,
+            message = "The maximum number of files in a draft has been exceeded.",
+            groups = DataValidationException.class
+    )
     private List<MultipartFile> media;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime scheduledAt;
