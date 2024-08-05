1853e68abcc2bf0a848ce1eed86842bbc33fef72
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-05 03:36:06 +0300

    add PostDto and PostResourceDto

---
+++ src/main/java/faang/school/postservice/dto/post/PostDto.java
@@ -0,0 +1,39 @@
+package faang.school.postservice.dto.post;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import faang.school.postservice.dto.resource.PostResourceDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+public class PostDto {
+    private Long id;
+    private Long authorId;
+    private Long projectId;
+    @NotBlank
+    @NotNull
+    private String content;
+    private List<PostResourceDto> resources;
+    @PositiveOrZero
+    private long likesCount;
+    @PositiveOrZero
+    private long commentsCount;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime scheduledAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime publishedAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime createdAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime updatedAt;
+}
