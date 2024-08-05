563068378562bc00bddb51a2ef988764daec118c
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-05 04:07:38 +0300

    add new method for deleting records with returning deletable records

--- src/main/java/faang/school/postservice/repository/ResourceRepository.java
+++ src/main/java/faang/school/postservice/repository/ResourceRepository.java
@@ -2,8 +2,23 @@ package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM resource WHERE id = :id RETURNING *", nativeQuery = true)
+    Resource popById(@Param("id") Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM resource WHERE id IN :ids RETURNING *", nativeQuery = true)
+    List<Resource> popAllByIds(@Param("ids") List<Long> ids);
 }
