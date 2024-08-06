e2e539363f7b65138702d28fa25c1cf4ae344f64
Grisha Tkach <grisatkac@mail.ru>
2024-08-07 00:00:10 +0300

    update service

--- src/main/java/faang/school/postservice/service/post/PostService.java
+++ src/main/java/faang/school/postservice/service/post/PostService.java
@@ -10,9 +10,9 @@ import faang.school.postservice.mapper.PostMapper;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.post.PostFilterRepository;
 import faang.school.postservice.repository.post.PostRepository;
-import faang.school.postservice.validation.post.PostValidation;
-import faang.school.postservice.validation.project.ProjectValidation;
-import faang.school.postservice.validation.user.UserValidation;
+import faang.school.postservice.validator.post.PostValidator;
+import jakarta.annotation.Nonnull;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -23,7 +23,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,28 +33,12 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final List<PostFilterRepository> postFilterRepository;
-    private final PostValidation postValidation;
-    private final UserValidation userValidation;
-    private final ProjectValidation projectValidation;
+    private final PostValidator postValidator;
     private final PostMapper postMapper;
 
     @Transactional
-    public PostDto create(PostCreateDto postCreateDto) {
-        if (postValidation.isNullable(postCreateDto)) {
-            throw new DataValidationException("Post can't be empty");
-        }
-
-        if (!postValidation.oneOfTheAuthorsIsNoNullable(postCreateDto.getAuthorId(), postCreateDto.getProjectId())) {
-            throw new DataValidationException("Only one of projectId or authorId must be provided");
-        }
-
-        if (!postValidation.isNullable(postCreateDto.getAuthorId())) {
-            userValidation.doesUserExist(postCreateDto.getAuthorId());
-        }
-
-        if (!postValidation.isNullable(postCreateDto.getProjectId())) {
-            projectValidation.doesProjectExist(postCreateDto.getProjectId());
-        }
+    public PostDto create(@Valid @Nonnull PostCreateDto postCreateDto) {
+        postValidator.checkIfPostHasAuthor(postCreateDto.getAuthorId(), postCreateDto.getProjectId());
 
         Post createdPost = postRepository.save(postMapper.toPost(postCreateDto));
         return postMapper.toDto(createdPost);
@@ -63,35 +46,18 @@ public class PostService {
 
     @Transactional
     public PostDto publish(Long id) {
-        PostDto post = postMapper.toDto(postRepository.findById(id)
-                .orElseThrow(() -> new DataValidationException(String.format("Post with id %s don't exists", id))));
-
-        if (post.isPublished()) {
-            throw new DataValidationException(String.format("Post %s has already published", post.getId()));
-        }
-
-        int updatedCount = postRepository.updatePublishedStatus(post.getId(), LocalDateTime.now(), true);
-
-        if (updatedCount == 0) {
-            throw new DataValidationException("Post wasn't save");
-        }
-
+        Post post = getEntityById(id);
+        postValidator.checkPostPublished(post.getId(), post.isPublished());
         post.setPublished(true);
 
-        return post;
+        return postMapper.toDto(postRepository.save(post));
     }
 
     @Transactional
-    public PostDto update(PostUpdateDto postDto) {
-        if (postValidation.isNullable(postDto)) {
-            throw new DataValidationException("Post can't be empty");
-        }
+    public PostDto update(@Valid @Nonnull PostUpdateDto postDto) {
+        postValidator.validateForUpdating(postDto);
 
-        postValidation.validateForUpdating(postDto);
-
-        Post postToUpdate = postRepository.findById(postDto.getId())
-                .orElseThrow(() -> new DataValidationException(String.format("Post %s doesn't exist", postDto.getId())));
-
+        Post postToUpdate = getEntityById(postDto.getId());
         postToUpdate.setContent(postDto.getContent());
 
         return postMapper.toDto(postRepository.save(postToUpdate));
@@ -99,49 +65,33 @@ public class PostService {
 
     @Transactional
     public void delete(Long id) {
-        Post postToDelete = postRepository.findById(id)
-                .orElseThrow(() -> new DataValidationException(String.format("Post %s doesn't exist", id)));
-        postToDelete.setDeleted(true);
+        Post postToDelete = getEntityById(id);
 
         postRepository.save(postToDelete);
     }
 
     @Transactional
     public PostDto getById(Long id) {
-        Post postToDelete = postRepository.findById(id)
+        Post post = postRepository.findById(id)
                 .orElseThrow(() -> new DataValidationException(String.format("Post %s doesn't exist", id)));
 
-        return postMapper.toDto(postToDelete);
+        return postMapper.toDto(post);
+    }
+
+    @Transactional
+    public Post getEntityById(Long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new DataValidationException(String.format("Post %s doesn't exist", id)));
     }
 
     @Transactional
     public Page<PostDto> getPostsByPublishedStatus(PostFilterDto postFilter) {
-        if (postFilter == null) {
-            throw new DataValidationException("Post filter can't be empty");
-        }
-
-        if (postFilter.getPublished() == null) {
-            throw new DataValidationException("Published field can't be empty");
-        }
-
-        if ((postFilter.getAuthorId() == null && postFilter.getProjectId() == null) || (postFilter.getAuthorId() != null && postFilter.getProjectId() != null)) {
-            throw new DataValidationException("Only one of projectId or authorId must be provided");
-        }
-
-        if (postFilter.getAuthorId() != null) {
-            userValidation.doesUserExist(postFilter.getAuthorId());
-        } else {
-            projectValidation.doesProjectExist(postFilter.getProjectId());
-        }
-
-        postFilter.setDeleted(false);
-
         Optional<Specification<Post>> postSpecification = postFilterRepository.stream()
                 .filter(filter -> filter.isApplicable(postFilter))
                 .map(filter -> filter.apply(postFilter))
                 .reduce(Specification::and);
 
-        postSpecification.orElseThrow(() -> new DataValidationException("Required fields are empty"));
+        postSpecification.orElseThrow(() -> new DataValidationException("Required fields are incorrect"));
 
         Pageable pageRequest = postFilter.getPublished()
                 ? PageRequest.of(postFilter.getPage(), postFilter.getSize(), Sort.by(SortField.PUBLISHED_AT.getValue()).descending())
