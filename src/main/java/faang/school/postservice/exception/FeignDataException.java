342c51325e966e6fd984ce7ff201968534ff0914
Grisha Tkach <grisatkac@mail.ru>
2024-08-06 23:59:08 +0300

    remove custom exception

--- src/main/java/faang/school/postservice/exception/FeignDataException.java
+++
@@ -1,8 +0,0 @@
-package faang.school.postservice.exception;
-
-public class FeignDataException extends RuntimeException{
-
-    public FeignDataException(String message) {
-        super(message);
-    }
-}
