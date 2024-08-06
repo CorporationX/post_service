a957047887f77a5462ec0c645ed3522c264b71cc
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-07 01:47:54 +0300

    Optimize imports

--- src/main/java/faang/school/postservice/dto/post/DraftPostDto.java
+++ src/main/java/faang/school/postservice/dto/post/DraftPostDto.java
@@ -1,10 +1,7 @@
 package faang.school.postservice.dto.post;
 
-import faang.school.postservice.exception.exceptionmessages.ValidationExceptionMessage;
-import faang.school.postservice.exception.validation.DataValidationException;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
