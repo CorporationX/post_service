7ca6f1f6c71fd19ed593f7b652ba22b53a6499d6
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-08 19:04:56 +0300

    Ignore mandatory user-id-x

--- src/main/java/faang/school/postservice/config/context/UserHeaderFilter.java
+++ src/main/java/faang/school/postservice/config/context/UserHeaderFilter.java
@@ -16,22 +16,23 @@ public class UserHeaderFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        String userIdHeader = req.getHeader("x-user-id");
-        if (userIdHeader != null) {
-            try {
-                long userId = Long.parseLong(userIdHeader);
-                userContext.setUserId(userId);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Invalid 'x-user-id' header value. Must be a valid long integer.", e);
-            }
-        } else {
-            throw new IllegalArgumentException("Missing required header 'x-user-id'. Please include 'x-user-id' header with a valid user ID in your request.");
-        }
-        try {
-            chain.doFilter(request, response);
-        } finally {
-            userContext.clear();
-        }
+//        HttpServletRequest req = (HttpServletRequest) request;
+
+//        String userIdHeader = req.getHeader("x-user-id");
+//        if (userIdHeader != null) {
+//            try {
+//                long userId = Long.parseLong(userIdHeader);
+//                userContext.setUserId(userId);
+//            } catch (NumberFormatException e) {
+//                throw new IllegalArgumentException("Invalid 'x-user-id' header value. Must be a valid long integer.", e);
+//            }
+//        } else {
+//            throw new IllegalArgumentException("Missing required header 'x-user-id'. Please include 'x-user-id' header with a valid user ID in your request.");
+//        }
+//        try {
+//            chain.doFilter(request, response);
+//        } finally {
+//            userContext.clear();
+//        }
     }
 }
