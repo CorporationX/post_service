4f23a63cc3b6bb5bd0ec9cb5dddbc1f75f3370f0
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-08 19:07:05 +0300

    Warning added

--- src/main/java/faang/school/postservice/api/MultipartFileMediaApi.java
+++ src/main/java/faang/school/postservice/api/MultipartFileMediaApi.java
@@ -4,4 +4,5 @@ import faang.school.postservice.dto.media.MediaDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface MultipartFileMediaApi extends MediaApi<String, MediaDto, MultipartFile> {
+    // TODO in future this interface was implemented for interact with S3
 }
