45df8188eef886c0a76b901dc43c7a2a2d95a606
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-08 19:04:32 +0300

    Add swagger

---
+++ src/main/java/faang/school/postservice/config/swagger/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package faang.school.postservice.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Post Service API")
+                        .version("1.0")
+                        .description("API for managing posts system")
+                );
+    }
+}
