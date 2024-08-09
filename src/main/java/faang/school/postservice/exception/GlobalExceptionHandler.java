720ee6137a62857a9f1fc3c67466d3b8f5cf7eee
Grisha Tkach <grisatkac@mail.ru>
2024-08-09 05:12:27 +0300

    add exception and handler

---
+++ src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package faang.school.postservice.exception;
+
+import faang.school.postservice.exception.DeletionFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DeletionFailedException.class)
+    public ResponseEntity<Object> handleDataValidationException(DeletionFailedException ex) {
+        return new ResponseEntity<>(response(ex), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, String> response(Exception ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        response.put("time", LocalDateTime.now().toString());
+        return response;
+    }
+}
