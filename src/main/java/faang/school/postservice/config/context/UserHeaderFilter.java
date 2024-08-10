a8e5ce3d23b77ae6cec015354fdb86c7450fb1cc
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-10 15:27:01 +0300

    return prev version

--- src/main/java/faang/school/postservice/config/context/UserHeaderFilter.java
+++ src/main/java/faang/school/postservice/config/context/UserHeaderFilter.java
@@ -16,23 +16,15 @@ public class UserHeaderFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-//        HttpServletRequest req = (HttpServletRequest) request;
-
-//        String userIdHeader = req.getHeader("x-user-id");
-//        if (userIdHeader != null) {
-//            try {
-//                long userId = Long.parseLong(userIdHeader);
-//                userContext.setUserId(userId);
-//            } catch (NumberFormatException e) {
-//                throw new IllegalArgumentException("Invalid 'x-user-id' header value. Must be a valid long integer.", e);
-//            }
-//        } else {
-//            throw new IllegalArgumentException("Missing required header 'x-user-id'. Please include 'x-user-id' header with a valid user ID in your request.");
-//        }
-//        try {
-//            chain.doFilter(request, response);
-//        } finally {
-//            userContext.clear();
-//        }
+        HttpServletRequest req = (HttpServletRequest) request;
+        String userId = req.getHeader("x-user-id");
+        if (userId != null) {
+            userContext.setUserId(Long.parseLong(userId));
+        }
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            userContext.clear();
+        }
     }
 }
