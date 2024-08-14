e8a1ef2f8ea32a92b0b3c007c9c8e87988c43b55
Alexandra <s7125@mail.ru>
2024-08-14 22:24:41 +0500

    method

--- src/main/java/faang/school/postservice/mapper/comment/PostViewMapper.java
+++ src/main/java/faang/school/postservice/mapper/comment/PostViewMapper.java
@@ -1,8 +1,6 @@
 package faang.school.postservice.mapper.comment;
 
-import faang.school.postservice.dto.comment.CommentDto;
 import faang.school.postservice.dto.post.PostViewDto;
-import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
