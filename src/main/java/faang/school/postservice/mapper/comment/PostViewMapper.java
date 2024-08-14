17826e7fb6a86ada7739f5a78666055220c68d9d
Alexandra <s7125@mail.ru>
2024-08-14 22:23:43 +0500

    method

--- src/main/java/faang/school/postservice/mapper/comment/PostViewMapper.java
+++ src/main/java/faang/school/postservice/mapper/comment/PostViewMapper.java
@@ -1,7 +1,9 @@
 package faang.school.postservice.mapper.comment;
 
 import faang.school.postservice.dto.comment.CommentDto;
+import faang.school.postservice.dto.post.PostViewDto;
 import faang.school.postservice.model.Comment;
+import faang.school.postservice.model.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -9,6 +11,5 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PostViewMapper {
     @Mapping(source = "post.id", target = "postId")
-    @Mapping(source = "likes", target = "likesId", qualifiedByName = "listLikesToLong")
-    CommentDto toDto(Comment comment);
+    PostViewDto toDto(Post post);
 }
--- src/main/java/faang/school/postservice/service/post/PostService.java
+++ src/main/java/faang/school/postservice/service/post/PostService.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.service.post;
 
 import faang.school.postservice.exception.ExceptionMessages;
+import faang.school.postservice.mapper.comment.PostViewMapper;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.publisher.postevent.PostViewEventPublisher;
 import faang.school.postservice.repository.PostRepository;
@@ -15,11 +16,12 @@ import org.springframework.stereotype.Service;
 public class PostService {
     private final PostRepository postRepository;
     private final PostViewEventPublisher postViewEventPublisher;
+    private final PostViewMapper postViewMapper;
 
     public Post getPost(long id) {
         return postRepository.findById(id).orElseThrow(() -> {
             log.error(ExceptionMessages.POST_NOT_FOUND);
-            postViewEventPublisher.toEventAndPublish(postRepository.findById(id));
+            postViewEventPublisher.toEventAndPublish(postViewMapper.toDto(postRepository.findById(id).get()));
             return new EntityNotFoundException(ExceptionMessages.POST_NOT_FOUND);
         });
     }
