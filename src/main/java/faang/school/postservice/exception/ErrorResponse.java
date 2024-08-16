41be9a8f6500f01ad68d8807894026b96956435b
Max Prigonyuk <kael.smr@gmail.com>
2024-08-16 18:16:09 +0400

    fix

--- src/main/java/faang/school/postservice/exception/ErrorResponse.java
+++ src/main/java/faang/school/postservice/exception/ErrorResponse.java
@@ -2,9 +2,11 @@ package faang.school.postservice.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private String timestamp;
     private String message;
