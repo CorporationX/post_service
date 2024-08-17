e8919b5d71a4efa143a94af132a6a510b1cbb013
Max Prigonyuk <kael.smr@gmail.com>
2024-08-18 01:35:02 +0400

    fix after cross-review

--- src/main/java/faang/school/postservice/publisher/LikeMessagePublisher.java
+++ src/main/java/faang/school/postservice/publisher/LikeMessagePublisher.java
@@ -1,34 +1,19 @@
 package faang.school.postservice.publisher;
 
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.event.LikeEvent;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
-public class LikeMessagePublisher implements MessagePublisher<LikeEvent>{
-    private final RedisTemplate<String, Object> redisTemplate;
-    private final ChannelTopic topic;
-    private final ObjectMapper objectMapper;
+public class LikeMessagePublisher extends GenericMessagePublisher<LikeEvent> {
 
-
-    @Override
-    public void publish(LikeEvent likeEvent) {
-        String message;
-        try {
-            message = objectMapper.writeValueAsString(likeEvent);
-        } catch (JsonProcessingException e) {
-            log.info("Method: publish {}", e.getMessage());
-            throw new RuntimeException(e);
-        }
-        redisTemplate.convertAndSend(topic.getTopic(), message);
-        System.out.println("Published message: " + message + " to channel: " + topic.getTopic());
+    public LikeMessagePublisher(RedisTemplate<String, Object> redisTemplate,
+                                ChannelTopic topic,
+                                ObjectMapper objectMapper) {
+        super(redisTemplate, topic, objectMapper);
     }
 }
