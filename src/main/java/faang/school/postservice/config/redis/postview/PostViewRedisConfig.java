0f67f93a860e259a8889385368bfadfe469c976a
Alexandra <s7125@mail.ru>
2024-08-19 17:05:16 +0500

    review

--- src/main/java/faang/school/postservice/config/redis/postview/PostViewRedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/postview/PostViewRedisConfig.java
@@ -1,18 +1,16 @@
-package faang.school.postservice.config.publishers;
+package faang.school.postservice.config.redis.postview;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.listener.ChannelTopic;
 
-@Configuration
-public class PostViewConfig {
+public class PostViewRedisConfig {
 
     @Value("${spring.data.redis.channels.post_view_channel.name}")
-    private String postViewChannel;
+    private String postViewChannelName;
 
     @Bean
     public ChannelTopic postViewTopic() {
-        return new ChannelTopic(postViewChannel);
+        return new ChannelTopic(postViewChannelName);
     }
 }
