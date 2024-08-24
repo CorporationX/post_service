d303e2db871495bb090f4a1a447bc6a898d9ccd6
Kirill Maltsev <opp.2211@gmail.com>
2024-08-24 08:15:19 +0500

    fix: common fields

--- src/main/java/faang/school/postservice/event/Event.java
+++ src/main/java/faang/school/postservice/event/Event.java
@@ -1,4 +1,20 @@
 package faang.school.postservice.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
 public abstract class Event {
+
+    int actorId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    LocalDateTime createdAt;
 }
--- src/main/java/faang/school/postservice/event/PostLikeEvent.java
+++ src/main/java/faang/school/postservice/event/PostLikeEvent.java
@@ -1,24 +1,16 @@
 package faang.school.postservice.event;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldDefaults;
 
-import java.time.LocalDateTime;
-
 @Data
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PostLikeEvent extends Event {
 
     int postId;
 
     int postAuthorId;
-
-    int actorId;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
-    LocalDateTime createdAt;
 }
