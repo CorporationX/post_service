405c8fa8b7f189f7e2e2fd996e67f8682c05bafd
Alexandra <s7125@mail.ru>
2024-08-26 11:22:08 +0500

    review

--- src/main/java/faang/school/postservice/filter/album/AlbumDescriptionDto.java
+++ src/main/java/faang/school/postservice/filter/album/AlbumDescriptionDto.java
@@ -2,9 +2,11 @@ package faang.school.postservice.filter.album;
 
 import faang.school.postservice.dto.album.AlbumFilterDto;
 import faang.school.postservice.model.Album;
+import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
 
+@Component
 public class AlbumDescriptionDto implements AlbumFilter {
     @Override
     public boolean isApplicable(AlbumFilterDto albumFilterDto) {
