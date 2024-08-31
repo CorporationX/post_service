4f8b30aa1a4895a15a30e5ed20e11a795332f74f
Gevorg <a0964593@gmail.com>
2024-08-31 18:02:58 +0300

    done task

--- src/main/java/faang/school/postservice/config/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/RedisConfig.java
@@ -1,51 +1,5 @@
 package faang.school.postservice.config;
 
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.listener.ChannelTopic;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class RedisConfig {
-//
-//    private final RedisCredentials credentials;
-//
-//    @Bean("postChannelTopic")
-//    public ChannelTopic postChannelTopic() {
-//        return new ChannelTopic(credentials.getChannels().getPost());
-//    }
-//
-//    @Bean("postLikeChannelTopic")
-//    public ChannelTopic postLikeTopic() {
-//        return new ChannelTopic(credentials.getChannels().getPostLike());
-//    }
-//
-//    @Bean("likePostChannelTopicAnalytics")
-//    public ChannelTopic likePostTopic() {
-//        return new ChannelTopic(credentials.getChannels().getLikePostAnalytics());
-//    }
-//
-//    @Bean
-//    public JedisConnectionFactory jedisConnectionFactory() {
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(credentials.getHost(), credentials.getPort());
-//        return new JedisConnectionFactory(redisStandaloneConfiguration);
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new StringRedisSerializer());
-//        return redisTemplate;
-//    }
-//}
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
