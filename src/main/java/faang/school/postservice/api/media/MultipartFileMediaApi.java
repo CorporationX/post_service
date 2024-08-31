3d1372b12a00d55af4bc085bb563a10a1c804249
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-01 02:55:40 +0300

    fix: Refactoring `MediaApi`

--- src/main/java/faang/school/postservice/api/media/MultipartFileMediaApi.java
+++ src/main/java/faang/school/postservice/api/media/MultipartFileMediaApi.java
@@ -1,8 +1,7 @@
-package faang.school.postservice.api;
+package faang.school.postservice.api.media;
 
 import faang.school.postservice.dto.media.MediaDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface MultipartFileMediaApi extends MediaApi<String, MediaDto, MultipartFile> {
-    // TODO in future this interface was implemented for interact with S3
 }
---
+++ src/main/java/faang/school/postservice/api/processor/MultipartFileProcessor.java
@@ -0,0 +1,6 @@
+package faang.school.postservice.api.processor;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface MultipartFileProcessor extends ResourceProcessor<MultipartFile> {
+}
