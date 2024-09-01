bf48b80d2b72a4e5477d62b0a89fda8ff9a32b31
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-01 03:06:43 +0300

    fix: change invoke methods

--- src/main/java/faang/school/postservice/service/post/command/UpdatePostResourceCommand.java
+++ src/main/java/faang/school/postservice/service/post/command/UpdatePostResourceCommand.java
@@ -1,6 +1,6 @@
 package faang.school.postservice.service.post.command;
 
-import faang.school.postservice.api.MultipartFileMediaApi;
+import faang.school.postservice.api.media.MultipartFileMediaApi;
 import faang.school.postservice.dto.media.MediaDto;
 import faang.school.postservice.dto.post.UpdatablePostDto;
 import faang.school.postservice.dto.resource.ResourceDto;
@@ -9,17 +9,16 @@ import faang.school.postservice.mapper.post.MediaMapper;
 import faang.school.postservice.mapper.post.ResourceMapper;
 import faang.school.postservice.model.Resource;
 import faang.school.postservice.repository.ResourceRepository;
+import faang.school.postservice.service.resource.ResourceService;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.util.Pair;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -30,17 +29,12 @@ import java.util.stream.Stream;
 @RequiredArgsConstructor
 public class UpdatePostResourceCommand {
 
-    private final ResourceRepository resourceRepository;
-
-    private final MultipartFileMediaApi mediaApi;
-
-    private final ResourceMapper resourceMapper;
-    private final MediaMapper mediaMapper;
+    private final ResourceService resourceService;
 
     private final ExecutorService executor;
     private final int taskTimeout;
 
-    public List<Resource> execute(Long postId, List<UpdatableResourceDto> updatableResource) {
+    public List<ResourceDto> execute(Long postId, List<UpdatableResourceDto> updatableResource) {
 
         List<UpdatableResourceDto>
                 newResources = new LinkedList<>(),
@@ -54,17 +48,17 @@ public class UpdatePostResourceCommand {
                 deletableResources
         );
 
-        CompletableFuture<List<Resource>> createResFuture = !newResources.isEmpty() ?
+        CompletableFuture<List<ResourceDto>> createResFuture = !newResources.isEmpty() ?
                 processCreatableResources(postId, newResources)
                 :
                 CompletableFuture.completedFuture(Collections.emptyList());
 
-        CompletableFuture<List<Resource>> updateResFuture = !refreshableResources.isEmpty() ?
+        CompletableFuture<List<ResourceDto>> updateResFuture = !refreshableResources.isEmpty() ?
                 processUpdatableResources(refreshableResources)
                 :
                 CompletableFuture.completedFuture(Collections.emptyList());
 
-        CompletableFuture<List<Resource>> deleteResFuture = !deletableResources.isEmpty() ?
+        CompletableFuture<List<ResourceDto>> deleteResFuture = !deletableResources.isEmpty() ?
                 processDeletableResources(deletableResources)
                 :
                 CompletableFuture.completedFuture(Collections.emptyList());
@@ -81,7 +75,7 @@ public class UpdatePostResourceCommand {
             throw new RuntimeException(e);
         }
 
-        List<Resource> updatedResources = Stream.concat(
+        List<ResourceDto> updatedResources = Stream.concat(
                 createResFuture.join().stream(),
                 updateResFuture.join().stream()
         ).toList();
@@ -118,103 +112,49 @@ public class UpdatePostResourceCommand {
         });
     }
 
-    private CompletableFuture<List<Resource>> processCreatableResources(Long postId, List<UpdatableResourceDto> creatable) {
+    private CompletableFuture<List<ResourceDto>> processCreatableResources(Long postId, List<UpdatableResourceDto> creatable) {
         return provideCompletableFuture(() -> {
-            log.info("Start creating resources for post {}.\nResources: {}", postId, creatable);
-
-            List<MultipartFile> media = creatable.stream()
-                    .map(UpdatableResourceDto::getResource)
+            List<MultipartFile> files = creatable.stream()
+                    .map(
+                            UpdatableResourceDto::getResource
+                    )
                     .toList();
 
-            log.info("Start saving post media in media storage");
-            List<MediaDto> savedMedia = mediaApi.saveAll(media);
-            log.info("Post media was saved. Saved: {}", savedMedia);
+            var created = resourceService.createResources(postId, files);
 
-            List<Resource> resources = savedMedia.stream()
-                    .map(m -> {
-                        ResourceDto resDto = mediaMapper.toResourceDto(m);
-                        return resourceMapper.toEntity(postId, resDto);
-                    }).toList();
-
-            log.info("Start saving media info in resource storage");
-            List<Resource> savedResources = resourceRepository.saveAll(resources);
-            log.info("Post media info was saved. Saved: {}", savedResources);
-
-            return savedResources;
+            return created;
         });
     }
 
-    private CompletableFuture<List<Resource>> processUpdatableResources(List<UpdatableResourceDto> updatable) {
+    private CompletableFuture<List<ResourceDto>> processUpdatableResources(List<UpdatableResourceDto> updatable) {
         return provideCompletableFuture(() -> {
-            log.info("Start updating post resources.\nResources: {}", updatable);
+            Map<Long, MultipartFile> res = updatable.stream().collect(Collectors.toMap(
+                    UpdatableResourceDto::getResourceId,
+                    UpdatableResourceDto::getResource
+            ));
 
-            Map<Long, MultipartFile> mediaMap = updatable.stream()
-                    .collect(Collectors.toMap(
-                            UpdatableResourceDto::getResourceId, UpdatableResourceDto::getResource)
-                    );
+            var updated = resourceService.updateResources(res);
 
-            Map<String, Resource> resourceMap = resourceRepository.findAllById(mediaMap.keySet()).stream()
-                    .collect(Collectors.toMap(Resource::getKey, r -> r));
-
-            List<Pair<String, MultipartFile>> updatableMedia = resourceMap.entrySet().stream()
-                    .map(e -> {
-                        String key = e.getKey();
-                        MultipartFile media = mediaMap.get(e.getValue().getId());
-
-                        return Pair.of(key, media);
-                    })
-                    .toList();
-
-            log.info("Start updating post media. Updatable media: {}", updatableMedia);
-            List<MediaDto> savedMedia = mediaApi.updateAll(updatableMedia).orElseThrow();
-            log.info("Post media was updated. Updated: {}", savedMedia);
-
-            List<Resource> updatableResource = savedMedia.stream()
-                    .map(m -> {
-                        Resource res = resourceMap.get(m.getKey());
-
-                        res.setSize(m.getSize());
-                        res.setName(m.getName());
-                        res.setType(m.getType());
-
-                        return res;
-                    })
-                    .toList();
-
-            log.info("Start updating media info");
-            List<Resource> savedResources = resourceRepository.saveAll(updatableResource);
-            log.info("Media info was updated.");
-
-            return savedResources;
+            return updated;
         });
     }
 
-    private CompletableFuture<List<Resource>> processDeletableResources(List<UpdatableResourceDto> deletable) {
+    private CompletableFuture<List<ResourceDto>> processDeletableResources(List<UpdatableResourceDto> deletable) {
         return provideCompletableFuture(() -> {
-            List<Long> resIds = deletable.stream()
+            Set<Long> deletableIds = deletable.stream()
                     .map(UpdatableResourceDto::getResourceId)
-                    .toList();
+                    .collect(Collectors.toSet());
 
-            log.info("Start deleting post resources.\nResources ids: {}", resIds);
-            List<Resource> poppedResource = resourceRepository.popAllByIds(resIds);
+            resourceService.deleteResources(deletableIds);
 
-            List<String> mediaKeys = poppedResource.stream()
-                    .map(Resource::getKey)
-                    .toList();
-
-            log.info("Start deleting post medias");
-            mediaApi.deleteAll(mediaKeys);
-            log.info("Post medias was deleted.");
-
-            return poppedResource;
+            return Collections.emptyList();
         });
     }
 
     private <T> CompletableFuture<T> provideCompletableFuture(Supplier<T> supplier) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                var result = supplier.get();
-                return result;
+                return supplier.get();
             } catch (Exception e) {
                 log.error(
                         "Error occurred during CompletableFuture execution in thread {}: {}",
