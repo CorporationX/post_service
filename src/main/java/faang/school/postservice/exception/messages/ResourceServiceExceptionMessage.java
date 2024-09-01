cce120915eef22d2d75aca4a32b3476f9eea9892
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-01 03:02:53 +0300

    feat: add new error message

--- src/main/java/faang/school/postservice/exception/messages/ResourceServiceExceptionMessage.java
+++ src/main/java/faang/school/postservice/exception/messages/ResourceServiceExceptionMessage.java
@@ -6,6 +6,15 @@ import lombok.Getter;
 public enum ResourceServiceExceptionMessage {
     RESOURCE_DOESNT_EXIST(
             "Resource with ID %s doesnt exist."
+    ),
+    RESOURCE_PROCESSING_FAILED(
+            "An error occurred while processing the resource %s."
+    ),
+    UNSUPPORTED_RESOURCE(
+            "Unsupported resource %s."
+    ),
+    GROUP_RESOURCE_PROCESSING_FAILED(
+            "An error occurred while processing the group of resource"
     );
 
     private final String message;
