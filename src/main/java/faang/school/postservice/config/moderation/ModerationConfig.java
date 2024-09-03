3a0d6a7d69c4ff2f092529b09e7f8ef7ff02accf
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-04 02:59:01 +0300

    fix: moved into `moderation` packages

--- src/main/java/faang/school/postservice/config/moderation/ModerationConfig.java
+++ src/main/java/faang/school/postservice/config/moderation/ModerationConfig.java
@@ -1,4 +1,4 @@
-package faang.school.postservice.config;
+package faang.school.postservice.config.moderation;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
