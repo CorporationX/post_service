002245b6a8025acfd0285e9ab3f0127b8da8746a
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-04 02:30:37 +0300

    fix: update jupiter version for jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
     /**
      * Jacoco
      */
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
 jacoco {
