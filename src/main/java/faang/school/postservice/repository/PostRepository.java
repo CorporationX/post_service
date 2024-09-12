f9a21523d9c48a266240018d6740f68afd51965e
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-12 12:33:58 +0300

    Изменил PostRepository

--- src/main/java/faang/school/postservice/repository/PostRepository.java
+++ src/main/java/faang/school/postservice/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Post;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -22,4 +23,40 @@ public interface PostRepository extends CrudRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p WHERE p.published = false AND p.deleted = false AND p.scheduledAt <= CURRENT_TIMESTAMP")
     List<Post> findReadyToPublish();
+
+    @Modifying
+    @Query("UPDATE Post SET content = :content, updatedAt = CURRENT_TIMESTAMP WHERE id = :id")
+    void updateContentByPostId(long id, String content);
+
+    @Modifying
+    @Query("UPDATE Post SET deleted = true WHERE id = :id")
+    void softDeletePostById(long id);
+
+    @Query(nativeQuery = true, value = """
+            SELECT * FROM post
+            WHERE author_id = :author_id AND published = false AND deleted = false
+            ORDER BY created_at DESC
+            """)
+    List<Post> findByAuthorIdAndUnpublished(long author_id);
+
+    @Query(nativeQuery = true, value = """
+            SELECT * FROM post
+            WHERE project_id = :projectId AND published = false AND deleted = false
+            ORDER BY created_at DESC
+            """)
+    List<Post> findByProjectIdAndUnpublished(long projectId);
+
+    @Query(nativeQuery = true, value = """
+            SELECT * FROM post
+            WHERE author_id = :authorId AND published = true AND deleted = false
+            ORDER BY published_at DESC
+            """)
+    List<Post> findByAuthorIdAndPublished(long authorId);
+
+    @Query(nativeQuery = true, value = """
+            SELECT * FROM post
+            WHERE project_id = :projectId AND published = true AND deleted = false
+            ORDER BY published_at DESC
+            """)
+    List<Post> findByProjectIdAndPublished(long projectId);
 }
