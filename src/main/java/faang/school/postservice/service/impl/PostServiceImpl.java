a7ff3f9caa24ec71ed60bc8fbb3ba54491e2e4d9
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-12 12:35:52 +0300

    Написал PostService и тесты к нему

---
+++ src/main/java/faang/school/postservice/service/impl/PostServiceImpl.java
@@ -0,0 +1,106 @@
+package faang.school.postservice.service.impl;
+
+import faang.school.postservice.client.ProjectServiceClient;
+import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.dto.post.PostDto;
+import faang.school.postservice.mapper.post.PostMapper;
+import faang.school.postservice.model.Post;
+import faang.school.postservice.repository.PostRepository;
+import faang.school.postservice.service.PostService;
+import faang.school.postservice.validator.PostValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+    private final ProjectServiceClient projectServiceClient;
+    private final UserServiceClient userServiceClient;
+    private final PostValidator validator;
+    private final PostMapper postMapper;
+
+
+    @Override
+    @Transactional
+    public void createDraftPost(PostDto postDto) {
+        validator.validatePost(postDto);
+        validator.validateCreator(existsCreator(postDto));
+
+        postDto.setDeleted(false);
+        postDto.setPublished(false);
+
+        Post newPost = postMapper.toEntity(postDto);
+        postRepository.save(newPost);
+    }
+
+    private boolean existsCreator(PostDto postDto) {
+        if (postDto.getAuthorId() == null) {
+            return projectServiceClient.existsProjectById(postDto.getProjectId());
+        } else {
+            return userServiceClient.existsUserById(postDto.getAuthorId());
+        }
+    }
+
+    @Override
+    @Transactional
+    public void publishPost(long id) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        Post post = validator.validatePost(optionalPost, id);
+
+        if (!post.isPublished()) {
+            post.setPublishedAt(LocalDateTime.now());
+            post.setPublished(true);
+            postRepository.save(post);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void updateContentPost(String newContent, long id) {
+        postRepository.updateContentByPostId(id, newContent);
+    }
+
+    @Override
+    @Transactional
+    public void softDeletePost(long id) {
+        postRepository.softDeletePostById(id);
+    }
+
+    @Override
+    public PostDto getPost(long id) {
+        Optional<Post> optionalPost = postRepository.findById(id);
+        Post post = validator.validatePost(optionalPost, id);
+        return postMapper.toDto(post);
+    }
+
+    @Override
+    public List<PostDto> getDraftPostsByUserId(long id) {
+        List<Post> posts = postRepository.findByAuthorIdAndUnpublished(id);
+        return postMapper.toDto(posts);
+    }
+
+    @Override
+    public List<PostDto> getDraftPostsByProjectId(long id) {
+        List<Post> posts = postRepository.findByProjectIdAndUnpublished(id);
+        return postMapper.toDto(posts);
+    }
+
+    @Override
+    public List<PostDto> getPublishedPostsByUserId(long id) {
+        List<Post> posts = postRepository.findByAuthorIdAndPublished(id);
+        return postMapper.toDto(posts);
+    }
+
+    @Override
+    public List<PostDto> getPublishedPostsByProjectId(long id) {
+        List<Post> posts = postRepository.findByProjectIdAndPublished(id);
+        return postMapper.toDto(posts);
+    }
+}
---
+++ src/test/java/faang/school/postservice/service/impl/PostServiceImplTest.java
@@ -0,0 +1,214 @@
+package faang.school.postservice.service.impl;
+
+import faang.school.postservice.client.ProjectServiceClient;
+import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.dto.post.PostDto;
+import faang.school.postservice.mapper.post.PostMapperImpl;
+import faang.school.postservice.model.Post;
+import faang.school.postservice.repository.PostRepository;
+import faang.school.postservice.validator.PostValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PostServiceImplTest {
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private ProjectServiceClient projectServiceClient;
+
+    @Mock
+    private UserServiceClient userServiceClient;
+
+    @Mock
+    private PostValidator validator;
+
+    @Spy
+    private PostMapperImpl postMapper;
+
+    @InjectMocks
+    private PostServiceImpl postService;
+
+    @Captor
+    private ArgumentCaptor<Post> postCaptor;
+
+    private Long postId;
+    private Post post;
+    private PostDto postDto;
+    private List<Post> posts;
+    private List<PostDto> postDtos;
+
+    @BeforeEach
+    void setUp() {
+        PostMapperImpl postMapper = new PostMapperImpl();
+        postDto = PostDto.builder()
+                .id(1L)
+                .content("Hello world!")
+                .authorId(123L)
+                .projectId(456L)
+                .build();
+        post = postMapper.toEntity(postDto);
+        postId = post.getId();
+        posts = List.of(post);
+        postDtos = List.of(postDto);
+    }
+
+    @Test
+    void createDraftPost_ExistsCreator_AuthorIdIsNull() {
+        Long projectId = postDto.getProjectId();
+        postDto.setAuthorId(null);
+
+        postService.createDraftPost(postDto);
+
+        verify(projectServiceClient).existsProjectById(projectId);
+        verify(userServiceClient, never()).existsUserById(anyLong());
+        verifyCreateDraftPost();
+    }
+
+    @Test
+    void createDraftPost_ExistsCreator_ProjectIdIsNull() {
+        Long authorId = postDto.getAuthorId();
+        postDto.setProjectId(null);
+
+        postService.createDraftPost(postDto);
+
+        verify(userServiceClient).existsUserById(authorId);
+        verify(projectServiceClient, never()).existsProjectById(anyLong());
+        verifyCreateDraftPost();
+    }
+
+    private void verifyCreateDraftPost() {
+        verify(postMapper).toEntity(postDto);
+        verify(postRepository).save(any(Post.class));
+    }
+
+    @Test
+    void publishPost_AlreadyPublished() {
+        Optional<Post> optionalPost = Optional.of(post);
+        configureSearchPost(optionalPost);
+        post.setPublished(true);
+
+        postService.publishPost(postId);
+
+        verify(validator).validatePost(optionalPost, postId);
+        verify(postRepository, never()).save(post);
+    }
+
+    @Test
+    void publishPost_Published() {
+        Optional<Post> optionalPost = Optional.of(post);
+        configureSearchPost(optionalPost);
+        post.setPublished(false);
+
+        postService.publishPost(postId);
+
+        verify(validator).validatePost(optionalPost, postId);
+        verify(postRepository).save(postCaptor.capture());
+
+        Post post = postCaptor.getValue();
+        assertTrue(post.isPublished());
+        assertNotNull(post.getPublishedAt());
+    }
+
+    @Test
+    void updateContentPost() {
+        String content = "Hello world!";
+
+        postService.updateContentPost(content, postId);
+
+        verify(postRepository).updateContentByPostId(postId, content);
+    }
+
+    @Test
+    void softDeletePost() {
+        postService.softDeletePost(postId);
+
+        verify(postRepository).softDeletePostById(postId);
+    }
+
+    @Test
+    void getPost() {
+        Optional<Post> optionalPost = Optional.of(post);
+        configureSearchPost(optionalPost);
+
+        PostDto postDto = postService.getPost(postId);
+
+        verify(postRepository).findById(postId);
+        verify(validator).validatePost(optionalPost, postId);
+        assertEquals(this.postDto, postDto);
+    }
+
+    private void configureSearchPost(Optional<Post> optionalPost) {
+        when(postRepository.findById(postId)).thenReturn(optionalPost);
+        when(validator.validatePost(optionalPost, postId)).thenReturn(post);
+    }
+
+    @Test
+    void getDraftPostsByUserId() {
+        Long authorId = postDto.getAuthorId();
+        when(postRepository.findByAuthorIdAndUnpublished(authorId)).thenReturn(posts);
+
+        var result = postService.getDraftPostsByUserId(authorId);
+
+        verify(postRepository).findByAuthorIdAndUnpublished(authorId);
+        verify(postMapper).toDto(posts);
+        assertEquals(postDtos, result);
+    }
+
+    @Test
+    void getDraftPostsByProjectId() {
+        Long projectId = postDto.getProjectId();
+        when(postRepository.findByProjectIdAndUnpublished(projectId)).thenReturn(posts);
+
+        var result = postService.getDraftPostsByProjectId(projectId);
+
+        verify(postRepository).findByProjectIdAndUnpublished(projectId);
+        verify(postMapper).toDto(posts);
+        assertEquals(postDtos, result);
+    }
+
+    @Test
+    void getPublishedPostsByUserId() {
+        Long authorId = postDto.getAuthorId();
+        when(postRepository.findByAuthorIdAndPublished(authorId)).thenReturn(posts);
+
+        var result = postService.getPublishedPostsByUserId(authorId);
+
+        verify(postRepository).findByAuthorIdAndPublished(authorId);
+        verify(postMapper).toDto(posts);
+        assertEquals(postDtos, result);
+    }
+
+    @Test
+    void getPublishedPostsByProjectId() {
+        Long projectId = postDto.getProjectId();
+        when(postRepository.findByProjectIdAndPublished(projectId)).thenReturn(posts);
+
+        var result = postService.getPublishedPostsByProjectId(projectId);
+
+        verify(postRepository).findByProjectIdAndPublished(projectId);
+        verify(postMapper).toDto(posts);
+        assertEquals(postDtos, result);
+    }
+}
