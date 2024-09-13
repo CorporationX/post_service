7fa1e323f02266a1d1f5d2a31b17787fcd74e7a6
Robert Oguzov <robertoguzov2@gmail.com>
2024-09-13 09:50:41 +0300

    Написал интерфейс LikeService

---
+++ src/main/java/faang/school/postservice/service/LikeService.java
@@ -0,0 +1,12 @@
+package faang.school.postservice.service;
+
+import faang.school.postservice.dto.user.UserDto;
+
+import java.util.List;
+
+public interface LikeService {
+
+    List<UserDto> getUsersLikedPost(long postId);
+
+    List<UserDto> getUsersLikedComment(long commentId);
+}
