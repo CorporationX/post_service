9142d10f00aa615bd5667aa7a3bb220ddbdd1928
Denys Dovgan <ytcsgoatlas@gmail.com>
2024-09-16 16:54:11 +0200

    Minor commit
    Test started

--- src/test/java/faang/school/postservice/controller/post/PostControllerTest.java
+++ src/test/java/faang/school/postservice/controller/post/PostControllerTest.java
@@ -1,4 +1,30 @@
 package faang.school.postservice.controller.post;
 
+import faang.school.postservice.dto.post.PostDto;
+import faang.school.postservice.service.post.PostService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class PostControllerTest {
+    @InjectMocks
+    private PostController postController;
+
+    @Mock
+    private PostService postService;
+
+    private PostDto postDto;
+
+    @BeforeEach
+    void setUp() {
+        postDto = PostDto.builder()
+                .id(1L)
+                .authorId(1L)
+                .content("Content")
+                .title("Title")
+                .build();
+    }
 }
--- src/test/java/faang/school/postservice/service/post/PostServiceTest.java
+++ src/test/java/faang/school/postservice/service/post/PostServiceTest.java
@@ -1,4 +1,67 @@
 package faang.school.postservice.service.post;
 
+import faang.school.postservice.dto.post.PostDto;
+import faang.school.postservice.entity.Post;
+import faang.school.postservice.mapper.post.PostMapper;
+import faang.school.postservice.repository.PostRepository;
+import faang.school.postservice.validator.post.PostValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
+
+    @InjectMocks
+    private PostService postService;
+
+    @Mock
+    private PostValidator postValidator;
+
+    @Spy
+    private PostMapper postMapper = Mappers.getMapper(PostMapper.class);
+
+    @Mock
+    private PostRepository postRepository;
+
+    private PostDto examplePostDto;
+    private Post examplePost;
+
+    @BeforeEach
+    void setUp() {
+        examplePostDto = PostDto.builder()
+                .id(1L)
+                .authorId(1L)
+                .content("Content")
+                .title("Title")
+                .build();
+
+        examplePost = Post.builder()
+                .id(1L)
+                .authorId(1L)
+                .content("Content")
+                .title("Title")
+                .build();
+    }
+
+    @Test
+    void createDraftPost_shouldReturnPostDto() {
+        // Arrange
+        when(postRepository.save(any(Post.class))).thenReturn(examplePost);
+
+        // Act
+        PostDto result = postService.createDraftPost(examplePostDto);
+
+        // Assert
+        assertEquals(examplePostDto, result);
+    }
 }
