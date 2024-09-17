4368402f3e0aaaf34358f4b6f82cdc1886aaec48
Andrew Romanov <r0m4n0v4v@gmail.com>
2024-09-17 21:08:27 +0300

    testing

--- src/test/java/faang/school/postservice/PostServiceAppTests.java
+++ src/test/java/faang/school/postservice/PostServiceAppTests.java
@@ -7,6 +7,6 @@ import org.junit.jupiter.api.Test;
 class PostServiceAppTests {
     @Test
     void contextLoads() {
-        Assertions.assertThat(40 + 2).isEqualTo(42);
+        Assertions.assertThat(40 + 2).isEqualTo(41);
     }
 }
