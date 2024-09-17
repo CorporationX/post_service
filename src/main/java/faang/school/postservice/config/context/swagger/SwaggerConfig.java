7d26d0e41108b106f6361a1e502a99751ef6fa34
Denis Artamonov <dns.artamonov@gmail.com>
2024-09-17 21:41:56 +0300

    BJS2-27230 complete

--- src/main/java/faang/school/postservice/config/context/swagger/SwaggerConfig.java
+++ src/main/java/faang/school/postservice/config/context/swagger/SwaggerConfig.java
@@ -1,22 +1,31 @@
 package faang.school.postservice.config.context.swagger;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Post Service API",
+                description = "Post service api specification",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Faang School",
+                        url = "https://faang-school.com/"
+                ),
+                license = @License(
+                        name = "License name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = @Server(
+                url = "http://localhost:8081/",
+                description = "Local Environment"))
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.controller")) // Replace with your controller package
-                .paths(PathSelectors.any())
-                .build();
-    }
+
 }
--- src/main/java/faang/school/postservice/swagger_example/ExampleDTO.java
+++
@@ -1,9 +0,0 @@
-package faang.school.postservice.swagger_example;
-
-import lombok.Data;
-
-@Data
-public class ExampleDTO {
-    private Long id;
-    private String name;
-}
