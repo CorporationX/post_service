c6d1f61ad243b47eaf818fcdefffc78fe2835a3c
Denis Alginin <AlgininDK@gmail.com>
2024-09-17 14:31:12 +0500

    Реализован методы 1. добавления поста 2. удаление поста

--- src/main/java/faang/school/postservice/controller/AlbumController.java
+++ src/main/java/faang/school/postservice/controller/AlbumController.java
@@ -7,9 +7,13 @@ import faang.school.postservice.service.AlbumService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +32,16 @@ public class AlbumController {
         return albumMapper.toDto(saveAlbum);
     }
 
+    @PutMapping("/{albumId}")
+    public AlbumDto addPostToAlbum(@RequestParam long postId, @PathVariable long albumId, @RequestParam long userId) {
+        Album saveAlbum = service.addPostToAlbum(postId, albumId, userId);
+        return albumMapper.toDto(saveAlbum);
+    }
+
+    @DeleteMapping("/{albumId}")
+    public AlbumDto removePostFromAlbum(@RequestParam long postId, @PathVariable long albumId, @RequestParam long userId) {
+        Album saveAlbum = service.removePostFromAlbum(postId, albumId, userId);
+        return albumMapper.toDto(saveAlbum);
+    }
 
 }
--- src/main/java/faang/school/postservice/service/AlbumService.java
+++ src/main/java/faang/school/postservice/service/AlbumService.java
@@ -3,12 +3,15 @@ package faang.school.postservice.service;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.model.Album;
+import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.AlbumRepository;
 
+import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -17,12 +20,39 @@ public class AlbumService {
 
     private final AlbumRepository albumRepository;
     private final UserServiceClient userServiceClient;
-
+    private final PostRepository postRepository;
+    
     @Transactional(readOnly = true)
     public Album createAlbum(Album album) {
         validUserExist(album.getAuthorId());
         validUniqueAlbumTitleByAuthor(album);
         return albumRepository.save(album);
+    }
+
+    public Album addPostToAlbum(long postId, long albumId, long userId) {
+        validUserExist(userId);
+        validAlbumBelongsToUser(albumId, userId);
+        Post post = postRepository.findById(postId).orElseThrow();
+        Album album = albumRepository.findById(albumId).orElseThrow();
+        album.addPost(post);
+        return albumRepository.save(album);
+    }
+
+    public Album removePostFromAlbum(long postId, long albumId, long userId) {
+        validUserExist(userId);
+        validAlbumBelongsToUser(albumId, userId);
+        Album album = albumRepository.findById(albumId).orElseThrow();
+        album.removePost(postId);
+        return albumRepository.save(album);
+    }
+
+    private void validAlbumBelongsToUser(long albumId, long userId) {
+        List<Long> userAlbumIds = albumRepository.findByAuthorId(userId)
+                .map(album -> album.getId())
+                .toList();
+        if (!userAlbumIds.contains(albumId)) {
+            throw new IllegalArgumentException("error");
+        }
     }
 
     private void validUserExist(Long authorId) {
