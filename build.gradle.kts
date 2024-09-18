2cee36d67229ac0cde055c2cc124bfdb68b1ac8a
Lexus-74 <lexus__74@mail.ru>
2024-09-18 21:46:54 +0500

    Прописаны зависимости для Swagger. Добавлен файл конфигурации

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,6 +63,11 @@ dependencies {
      */
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
+    /**
+     * Swagger
+     */
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.3")
 }
 
 jacoco {
