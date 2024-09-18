69a72f4e0a0e8e0f616dc63af4d70831108bbfc6
zeus <denis@fatum.ru>
2024-09-18 16:25:29 +0300

    BJS2-27221

--- src/main/java/faang/school/postservice/PostServiceApp.java
+++ src/main/java/faang/school/postservice/PostServiceApp.java
@@ -1,8 +1,5 @@
 package faang.school.postservice;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,11 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients(basePackages = "faang.school.postservice.client")
-@OpenAPIDefinition(info = @Info(
-        title = "Post Service API",
-        description = "Service for working with posts,comments...",
-        version = "1.0.0"),
-        servers = @Server(url = "http://localhost:8081/", description = "@dev server"))
 public class PostServiceApp {
     public static void main(String[] args) {
         new SpringApplicationBuilder(PostServiceApp.class)
