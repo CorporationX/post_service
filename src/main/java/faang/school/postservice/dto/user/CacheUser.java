ae7c9356336dc15fdd4b7678f48cf3e0819fd12b
Nikita Volzhin <vlzh.nik@mail.ru>
2024-09-19 14:48:35 +0500

    BJS2-28433

---
+++ src/main/java/faang/school/postservice/dto/user/CacheUser.java
@@ -0,0 +1,25 @@
+package faang.school.postservice.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.util.LinkedHashSet;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@RedisHash("cacheUser")
+public class CacheUser {
+    @Id
+    private long id;
+    private LinkedHashSet<Long> postIdsForFeed;
+
+    @TimeToLive
+    private long ttl;
+}
