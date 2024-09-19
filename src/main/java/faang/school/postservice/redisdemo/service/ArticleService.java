1abaaca0b79bf067763b2d716845ce05cede2a4a
Sultan <sultan.inked@gmail.com>
2024-09-19 20:19:21 +0600

    add host to redis, failed if inject from yaml file =(

--- src/main/java/faang/school/postservice/redisdemo/service/ArticleService.java
+++ src/main/java/faang/school/postservice/redisdemo/service/ArticleService.java
@@ -6,6 +6,7 @@ import faang.school.postservice.redisdemo.dto.ArticleDto;
 import faang.school.postservice.redisdemo.mapper.ArticleMapper;
 import faang.school.postservice.redisdemo.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -17,10 +18,16 @@ import java.util.Random;
 public class ArticleService {
     private static final int TTL = 10;
 
+//    @Value("${spring.data.redis.host}")
+    private String redisHost = "localhost";
+
+//    @Value("${spring.data.redis.port}")
+    private int redisPort = 6379;
+
     private final ArticleRepository articleRepository;
     private final ArticleMapper articleMapper;
     private final ObjectMapper jacksonObjectMapper;
-    private final JedisPool jedisPool = new JedisPool();
+    private final JedisPool jedisPool = new JedisPool(redisHost, redisPort);
     private final Random random = new Random();
 
     public ArticleDto getArticle(Long id) {
@@ -28,9 +35,11 @@ public class ArticleService {
     }
 
     public ArticleDto getCachedArticle(Long id) {
+        System.out.println("HOST: " + redisHost + ", PORT: " + redisPort);
         try (Jedis jedis = jedisPool.getResource()) {
             String key = "article:%d".formatted(id);
             String raw = jedis.get(key);
+            System.out.println("RAW: " + raw);
             if (raw != null) {
                 return jacksonObjectMapper.readValue(raw, ArticleDto.class);
             }
