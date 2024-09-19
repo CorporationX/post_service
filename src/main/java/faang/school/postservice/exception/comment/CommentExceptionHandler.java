6f1e0bddcb4c7655f2f50ddad28e24b00596814f
sta1er <vlad.sokogubov@gmail.com>
2024-09-19 14:02:41 +0300

    ренейминг класса

--- src/main/java/faang/school/postservice/exception/comment/CommentExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package faang.school.postservice.exception.comment;
-
-// why the fuck is this called a handler...
-public class CommentExceptionHandler extends RuntimeException{
-    public CommentExceptionHandler(String message) {
-        super(message);
-    }
-}
