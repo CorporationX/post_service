4a61bb25682d3ea7241f390c21de1c5765c7989b
Garik-create <pfdfkbybujhm@yandex.ru>
2024-09-21 22:47:27 +0300

    - change PostInRedisRepository extending JpaRepository on extending CrudRepository

--- src/main/java/faang/school/postservice/repository/redis/PostInRedisRepository.java
+++ src/main/java/faang/school/postservice/repository/redis/PostInRedisRepository.java
@@ -1,9 +1,9 @@
 package faang.school.postservice.repository.redis;
 
 import faang.school.postservice.model.redis.PostInRedis;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostInRedisRepository extends JpaRepository<PostInRedis, String> {
+public interface PostInRedisRepository extends CrudRepository<PostInRedis, String> {
 }
