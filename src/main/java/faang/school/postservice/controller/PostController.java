3a505b2a8cbf0c55a4b7bc987f22bddd592d9e52
mamadjonovKO <mamadjonov_k@auca.kg>
2024-09-22 16:17:49 +0600

    Removed unused import

--- src/main/java/faang/school/postservice/controller/PostController.java
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -5,7 +5,6 @@ import faang.school.postservice.mapper.PostMapper;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
