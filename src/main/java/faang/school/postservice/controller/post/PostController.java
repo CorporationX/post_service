248ae11edf848d1de3c90dc61ff4cbb935b66cdb
DenysDovgan <ytcsgoatlas@gmail.com>
2024-09-22 19:02:47 +0200

    Mapping removed from config to controllers

--- src/main/java/faang/school/postservice/controller/post/PostController.java
+++ src/main/java/faang/school/postservice/controller/post/PostController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("api/v1/posts")
 @RequiredArgsConstructor
 public class PostController {
 
