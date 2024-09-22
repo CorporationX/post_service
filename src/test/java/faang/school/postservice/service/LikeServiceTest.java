669374fd78f06d22122d9bac9907efa3c5c343bc
Sergey Sklyar <sklyar1091@gmail.com>
2024-09-22 22:55:57 +0300

    imports delete

--- src/test/java/faang/school/postservice/service/LikeServiceTest.java
+++ src/test/java/faang/school/postservice/service/LikeServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mapstruct.control.MappingControl;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
