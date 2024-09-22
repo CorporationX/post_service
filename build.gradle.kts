6ffb719f35bd7e3e06aec6e249b879bb4e5f4afd
grisatkac <grisatkac@mail.ru>
2024-09-22 23:33:02 +0300

    add spring retry dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+    implementation("org.springframework.retry:spring-retry:2.0.5")
 
     /**
      * Test containers
