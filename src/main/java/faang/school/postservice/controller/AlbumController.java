f277094b140ae2ac93cd1819b0a53eeef86f1a95
Rashit Gareyev <gareyevrs@gmail.com>
2024-09-23 18:46:11 +0400

    feature BJS2-27232 ResponseEntity and ExceptionHandler deleted

--- src/main/java/faang/school/postservice/controller/AlbumController.java
+++ src/main/java/faang/school/postservice/controller/AlbumController.java
@@ -9,8 +9,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,115 +32,114 @@ public class AlbumController {
 
     @PostMapping()
     @Operation(summary = "Create an album", description = "Create a new empty album")
-    public ResponseEntity<AlbumDto> create(
+    public AlbumDto create(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Valid @RequestBody AlbumDto albumDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(albumService.create(albumDto));
+        return albumService.create(albumDto);
     }
 
     @PostMapping("/{albumId}/posts/{postId}")
     @Operation(summary = "Add a post", description = "Add a post to the album")
-    public ResponseEntity<String> addPostToAlbum(
+    public String addPostToAlbum(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "ID of the album", required = true)
             @PathVariable Long albumId,
             @Parameter(description = "ID of the post to add to the album", required = true)
             @PathVariable Long postId) {
-        return ResponseEntity.ok(albumService.addPostToAlbum(albumId, postId));
+        return albumService.addPostToAlbum(albumId, postId);
     }
 
     @DeleteMapping("/{albumId}/posts/{postId}")
     @Operation(summary = "Delete a post", description = "Delete a post from the album")
-    public ResponseEntity<AlbumDto> deletePostFromAlbum(
+    public AlbumDto deletePostFromAlbum(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "ID of the album", required = true)
             @PathVariable Long albumId,
             @Parameter(description = "ID of the post to delete from the album", required = true)
             @PathVariable Long postId) {
-        return ResponseEntity.ok(albumService.deletePostFromAlbum(albumId, postId));
+        return albumService.deletePostFromAlbum(albumId, postId);
     }
 
     @PostMapping("/{albumId}/favorites")
     @Operation(summary = "Add an album to favorites", description = "Add an album to favorites")
-    public ResponseEntity<String> addAlbumToFavorites(
+    public String addAlbumToFavorites(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "ID of the album to add to favorites", required = true)
             @PathVariable Long albumId) {
-        return ResponseEntity.ok(albumService.addAlbumToFavorites(albumId));
+        return albumService.addAlbumToFavorites(albumId);
     }
 
     @DeleteMapping("/{albumId}/favorites")
     @Operation(summary = "Delete album from favorites", description = "Delete album from favorites")
-    public ResponseEntity<String> deleteAlbumFromFavorites(
+    public String deleteAlbumFromFavorites(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "ID of the album to delete", required = true)
             @PathVariable Long albumId) {
-        return ResponseEntity.ok(albumService.deleteAlbumFromFavorites(albumId));
+        return albumService.deleteAlbumFromFavorites(albumId);
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Get an album by ID", description = "Retrieve an album by its ID.")
-    public ResponseEntity<AlbumDto> getAlbumById(
+    public AlbumDto getAlbumById(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "ID of the album", required = true)
             @PathVariable Long id) {
-        return ResponseEntity.ok(albumService.getAlbumById(id));
+        return albumService.getAlbumById(id);
     }
 
     @PostMapping("/user/filter")
     @Operation(summary = "Get user's albums by filter", description = "Retrieve user's albums by filter")
-    public ResponseEntity<List<AlbumDto>> getUserAlbums(
+    public List<AlbumDto> getUserAlbums(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "Filter for user's albums", required = true)
             @RequestBody AlbumFilterDto albumFilterDto) {
-        return ResponseEntity.ok(albumService.getUserAlbums(albumFilterDto));
+        return albumService.getUserAlbums(albumFilterDto);
     }
 
     @PostMapping("/filter")
     @Operation(summary = "Get all users albums by filter", description = "Retrieve all user's albums by filter")
-    public ResponseEntity<List<AlbumDto>> getAllUsersAlbums(
+    public List<AlbumDto> getAllUsersAlbums(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "Filter for all users albums", required = true)
             @RequestBody AlbumFilterDto albumFilterDto) {
-        return ResponseEntity.ok(albumService.getAllUsersAlbums(albumFilterDto));
+        return albumService.getAllUsersAlbums(albumFilterDto);
     }
 
     @PostMapping("/user/favorite/filter")
     @Operation(summary = "Get user's favorite albums by filter", description = "Retrieve user's favorite albums by filter")
-    public ResponseEntity<List<AlbumDto>> getUserFavoriteAlbums(
+    public List<AlbumDto> getUserFavoriteAlbums(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @Parameter(description = "Filter for user's favorite albums", required = true)
             @RequestBody AlbumFilterDto albumFilterDto) {
-        return ResponseEntity.ok(albumService.getUserFavoriteAlbums(albumFilterDto));
+        return albumService.getUserFavoriteAlbums(albumFilterDto);
     }
 
     @PutMapping("/user/{albumId}")
     @Operation(summary = "Update user's album", description = "Update an existing user's album")
-    public ResponseEntity<AlbumDto> update(
+    public AlbumDto update(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @PathVariable @NotNull Long albumId,
             @Valid @RequestBody AlbumDto albumDto) {
-        return ResponseEntity.ok(albumService.update(albumId, albumDto));
+        return albumService.update(albumId, albumDto);
     }
 
     @DeleteMapping("/user/{albumId}")
     @Operation(summary = "Delete user's album", description = "Delete an existing user's album")
-    public ResponseEntity<Void> delete(
+    public void delete(
             @Parameter(description = "ID of the user", required = true)
             @RequestHeader("x-user-id") String userId,
             @PathVariable @NotNull Long albumId) {
         albumService.delete(albumId);
-        return ResponseEntity.ok().build();
     }
 }
 
--- src/test/java/faang/school/postservice/AlbumControllerTest.java
+++ src/test/java/faang/school/postservice/AlbumControllerTest.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
 import java.util.List;
@@ -43,49 +41,38 @@ public class AlbumControllerTest {
     public void testCreateAlbum() {
         when(albumService.create(any(AlbumDto.class))).thenReturn(albumDto);
 
-        ResponseEntity<AlbumDto> response = albumController.create("1", albumDto);
+        AlbumDto response = albumController.create("1", albumDto);
 
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertEquals("Test Album", response.getBody().getTitle());
-        assertEquals("Test Description", response.getBody().getDescription());
+        assertEquals("Test Album", response.getTitle());
+        assertEquals("Test Description", response.getDescription());
     }
 
     @Test
     public void testGetAlbumById() {
         when(albumService.getAlbumById(1L)).thenReturn(albumDto);
 
-        ResponseEntity<AlbumDto> response = albumController.getAlbumById("1", 1L);
+        AlbumDto response = albumController.getAlbumById("1", 1L);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Test Album", response.getBody().getTitle());
-        assertEquals("Test Description", response.getBody().getDescription());
+        assertEquals("Test Album", response.getTitle());
+        assertEquals("Test Description", response.getDescription());
     }
 
     @Test
     public void testAddAlbumToFavorites() {
         when(albumService.addAlbumToFavorites(1L)).thenReturn("Album with id = 1 added to favorites");
 
-        ResponseEntity<String> response = albumController.addAlbumToFavorites("1", 1L);
+        String response = albumController.addAlbumToFavorites("1", 1L);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Album with id = 1 added to favorites", response.getBody());
+        assertEquals("Album with id = 1 added to favorites", response);
     }
 
     @Test
     public void testDeleteAlbumFromFavorites() {
         when(albumService.deleteAlbumFromFavorites(1L)).thenReturn("Album with id = 1 deleted from favorites");
 
-        ResponseEntity<String> response = albumController.deleteAlbumFromFavorites("1", 1L);
+        String response = albumController.deleteAlbumFromFavorites("1", 1L);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Album with id = 1 deleted from favorites", response.getBody());
-    }
-
-    @Test
-    public void testDeleteAlbum() {
-        ResponseEntity<Void> response = albumController.delete("1", 1L);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Album with id = 1 deleted from favorites", response);
     }
 
     @Test
@@ -96,12 +83,11 @@ public class AlbumControllerTest {
         AlbumFilterDto filterDto = new AlbumFilterDto();
         filterDto.setTitle("Test");
 
-        ResponseEntity<List<AlbumDto>> response = albumController.getUserAlbums("1", filterDto);
+        List<AlbumDto> response = albumController.getUserAlbums("1", filterDto);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(1, response.getBody().size());
-        assertEquals("Test Album", response.getBody().get(0).getTitle());
-        assertEquals("Test Description", response.getBody().get(0).getDescription());
+        assertEquals(1, response.size());
+        assertEquals("Test Album", response.get(0).getTitle());
+        assertEquals("Test Description", response.get(0).getDescription());
     }
 
     @Test
@@ -112,21 +98,19 @@ public class AlbumControllerTest {
 
         when(albumService.update(anyLong(), any(AlbumDto.class))).thenReturn(updatedDto);
 
-        ResponseEntity<AlbumDto> response = albumController.update("1", 1L, updatedDto);
+        AlbumDto response = albumController.update("1", 1L, updatedDto);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Updated Title", response.getBody().getTitle());
-        assertEquals("Updated Description", response.getBody().getDescription());
+        assertEquals("Updated Title", response.getTitle());
+        assertEquals("Updated Description", response.getDescription());
     }
 
     @Test
     public void testDeletePostFromAlbum() {
         when(albumService.deletePostFromAlbum(anyLong(), anyLong())).thenReturn(albumDto);
 
-        ResponseEntity<AlbumDto> response = albumController.deletePostFromAlbum("1", 1L, 1L);
+        AlbumDto response = albumController.deletePostFromAlbum("1", 1L, 1L);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Test Album", response.getBody().getTitle());
-        assertEquals("Test Description", response.getBody().getDescription());
+        assertEquals("Test Album", response.getTitle());
+        assertEquals("Test Description", response.getDescription());
     }
 }
