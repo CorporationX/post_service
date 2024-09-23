d829f7aa81fd4a81a25cd118fc3961ff47c45583
ifef228 <ifef228@gmail.com>
2024-09-23 08:43:48 +0300

    убрал комменты

--- src/main/java/faang/school/postservice/config/swagger/SwaggerConfig.java
+++ src/main/java/faang/school/postservice/config/swagger/SwaggerConfig.java
@@ -5,11 +5,6 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * url: http://localhost:8081/v3/api-docs
- * http://localhost:8080/api-docs.yaml
- * http://localhost:8081/swagger-ui/index.html
- */
 @Configuration
 public class SwaggerConfig {
     @Bean
