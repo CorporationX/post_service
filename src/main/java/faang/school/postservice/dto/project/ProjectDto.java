cc13eb99dd81520e511afedd17b90fd9a62f3f49
Okabe <mathew.mackonoha@gmail.com>
2024-09-24 09:36:19 +0300

    Валидация в сервисе постов - тут все готово, сущностей для работы мало.

--- src/main/java/faang/school/postservice/dto/project/ProjectDto.java
+++ src/main/java/faang/school/postservice/dto/project/ProjectDto.java
@@ -1,9 +1,13 @@
 package faang.school.postservice.dto.project;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ProjectDto {
+    @Min(value = 1, message = "ID must be a positive number")
     private long id;
+    @NotBlank(message = "Title should not be blank")
     private String title;
 }
