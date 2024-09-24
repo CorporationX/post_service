f6719ca775df6d9467abd8397e481897ecdedbda
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-09-24 22:12:57 +0500

    BJS2-28413 - Автора поста
    + Кешировал автора поста

--- src/main/java/faang/school/postservice/cache/repository/UserCacheRepository.java
+++ src/main/java/faang/school/postservice/cache/repository/UserCacheRepository.java
@@ -1,9 +1,9 @@
 package faang.school.postservice.cache.repository;
 
-import faang.school.postservice.cache.entity.PostCache;
+import faang.school.postservice.cache.entity.UserCache;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostCacheRepository extends JpaRepository<PostCache, Long> {
+public interface UserCacheRepository extends JpaRepository<UserCache, Long> {
 }
