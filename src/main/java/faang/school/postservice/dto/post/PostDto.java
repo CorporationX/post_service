be184adce91eda2fb220d63897e73fd09501e698
grisatkac <grisatkac@mail.ru>
2024-09-25 04:33:57 +0300

    add constructors in the dto's

--- src/main/java/faang/school/postservice/dto/post/PostDto.java
+++ src/main/java/faang/school/postservice/dto/post/PostDto.java
@@ -1,12 +1,16 @@
 package faang.school.postservice.dto.post;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDto {
 
     private Long id;
--- src/main/java/faang/school/postservice/dto/user/UserDto.java
+++ src/main/java/faang/school/postservice/dto/user/UserDto.java
@@ -1,10 +1,14 @@
 package faang.school.postservice.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
     private Long id;
     private String username;
