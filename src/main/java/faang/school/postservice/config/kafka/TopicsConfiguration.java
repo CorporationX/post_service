ec9f9fea8b0c98e04535f691be0d9680a762587d
grisatkac <grisatkac@mail.ru>
2024-09-25 04:32:23 +0300

    add kafka topics configuration

---
+++ src/main/java/faang/school/postservice/config/kafka/TopicsConfiguration.java
@@ -0,0 +1,31 @@
+package faang.school.postservice.config.kafka;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TopicsConfiguration {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.topic.post-like}")
+    private String postLikeTopic;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1() {
+        return new NewTopic(postLikeTopic, 1, (short) 1);
+    }
+}
