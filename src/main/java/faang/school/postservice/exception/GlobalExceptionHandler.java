4147ce2039cd3784e985780ce706a1ca8801c7d1
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-09-25 15:13:20 +0500

    Added Exception handlers

--- src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package faang.school.postservice.exception;
 
 import faang.school.postservice.dto.error.ErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleEntityNotFoundException(EntityNotFoundException exception) {
+        log.error("Entity not found exception occurred: {}",exception.getMessage());
         return ErrorResponse.builder()
                 .code(HttpStatus.NOT_FOUND.value())
                 .message(exception.getMessage())
@@ -26,20 +30,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        Map<String, String> errors = new HashMap<>();
-
-        exception.getBindingResult().getAllErrors().forEach(error -> {
-            var fieldName = ((FieldError) error).getField();
-            var errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-
-        StringBuilder sb = new StringBuilder();
-        errors.forEach((key, value) -> sb.append("%s; ".formatted(value)));
-        return ErrorResponse.builder()
-                .code(HttpStatus.BAD_REQUEST.value())
-                .message(sb.toString())
-                .build();
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("Method argument not valid exception occurred: {}", exception.getMessage());
+        return exception.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "")
+                ));
     }
 }
