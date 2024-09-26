00f617527888fbe4857b62e901016a008a8dcc0a
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-09-26 04:16:56 +0300

    BJS2-26850 Система комментариев

---
+++ src/main/java/faang/school/postservice/controller/CommentController.java
@@ -0,0 +1,60 @@
+package faang.school.postservice.controller;
+
+import faang.school.postservice.dto.comment.CommentDto;
+import faang.school.postservice.dto.comment.CommentUpdateDto;
+import faang.school.postservice.mapper.CommentMapper;
+import faang.school.postservice.mapper.CommentUpdateMapper;
+import faang.school.postservice.model.Comment;
+import faang.school.postservice.service.CommentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+public class CommentController {
+    private final CommentService commentService;
+    private final CommentMapper commentMapper;
+    private final CommentUpdateMapper commentUpdateMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentDto createComment(@Valid @RequestBody CommentDto commentResponseDto) {
+        Comment commentFromDto = commentMapper.toEntity(commentResponseDto);
+        Comment createdComment = commentService.createComment(commentFromDto);
+        return commentMapper.toDto(createdComment);
+    }
+
+    @PutMapping
+    public CommentUpdateDto updateComment(@Valid @RequestBody CommentUpdateDto commentUpdateDto) {
+        Comment commentFromUpdateDto = commentUpdateMapper.toEntity(commentUpdateDto);
+        Comment updatedComment = commentService.updateComment(commentFromUpdateDto);
+        return commentUpdateMapper.toDto(updatedComment);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public void delete(@PathVariable long commentId) {
+        commentService.deleteComment(commentId);
+    }
+
+    @GetMapping("/post/{postId}")
+    public List<CommentDto> findAllComments(@PathVariable long postId) {
+        return commentService.findAllComments(postId)
+                .stream()
+                .map(commentMapper::toDto)
+                .toList();
+    }
+}
---
+++ src/main/java/faang/school/postservice/service/CommentServiceHandler.java
@@ -0,0 +1,42 @@
+package faang.school.postservice.service;
+
+import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.exception.DataValidationException;
+import faang.school.postservice.model.Comment;
+import faang.school.postservice.repository.CommentRepository;
+import faang.school.postservice.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentServiceHandler {
+    private final UserServiceClient userServiceClient;
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    public void userExistsByIdValidation(long authorId) {
+        if (userServiceClient.getUser(authorId) == null) {
+            throw new DataValidationException("Author with ID: " + authorId + " not found.");
+        }
+    }
+
+    public void commentExistsByIdValidation(long commentId) {
+        if (!commentRepository.existsById(commentId)) {
+            throw new DataValidationException("Comment with ID: " + commentId + " not found.");
+        }
+    }
+
+    public void postExistsByIdValidation(long postId) {
+        if (!postRepository.existsById(postId)) {
+            throw new DataValidationException("Post with ID: " + postId + " not found.");
+        }
+    }
+
+    public void editCommentByAuthorValidation(UserDto user, Comment updatedComment) {
+        if (!user.getId().equals(updatedComment.getAuthorId())) {
+            throw new DataValidationException("Only Author can edit comment");
+        }
+    }
+}
