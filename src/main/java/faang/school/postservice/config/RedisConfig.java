437bd8075447fd5a5133bd56cf3d4b49c3ac39c1
klinkov <klinkov.1997@list.ru>
2024-09-27 18:33:57 +0300

    Убрал дублирование RedisConfig. Теперь запускается без ошибок.

--- src/main/java/faang/school/postservice/config/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/RedisConfig.java
@@ -1,15 +1,19 @@
 package faang.school.postservice.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
+    @Value("${spring.data.redis.channels.like_event_channel}")
+    private String likeEventChannel;
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
@@ -25,4 +29,8 @@ public class RedisConfig {
         return template;
     }
 
+    @Bean
+    public ChannelTopic likeEventTopic() {
+        return new ChannelTopic(likeEventChannel);
+    }
 }
