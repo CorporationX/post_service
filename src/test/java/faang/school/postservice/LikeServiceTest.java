1d5362897e06bdba9869eb5f24cce147d356d7ef
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-09-28 21:44:42 +0300

    сделал тесты

---
+++ src/test/java/faang/school/postservice/LikeServiceTest.java
@@ -0,0 +1,60 @@
+package faang.school.postservice;
+
+import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.model.Like;
+import faang.school.postservice.repository.LikeRepository;
+import faang.school.postservice.service.like.LikeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class LikeServiceTest {
+
+    @InjectMocks
+    private LikeServiceImpl likeService;
+    @Mock
+    private LikeRepository likeRepository;
+    @Mock
+    private UserServiceClient userServiceClient;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    private static final Like like1 = Like.builder()
+            .id(1L)
+            .userId(1L)
+            .build();
+    private static final List<Like> likes = List.of(like1);
+    private static final UserDto user1 = new UserDto(1L, "name", "mail");
+
+    @Test
+    public void testGetUsersByPostId() {
+        when(likeRepository.findByPostId(1L)).thenReturn(likes);
+
+        likeService.getUsersByPostId(1L);
+
+        verify(likeRepository).findByPostId(1L);
+        verify(userServiceClient).getUsersByIds(List.of(1L));
+    }
+
+    @Test
+    public void testGetUsersByCommentId() {
+        when(likeRepository.findByCommentId(1L)).thenReturn(likes);
+
+        likeService.getUsersByCommentId(1L);
+
+        verify(likeRepository).findByCommentId(1L);
+        verify(userServiceClient).getUsersByIds(List.of(1L));
+    }
+
+}
