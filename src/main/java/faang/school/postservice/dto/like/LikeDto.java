d9e6c1ea5b88da04fe8cf3a2fe56df026571a727
Ivan <spvanym@gmail.com>
2024-09-29 18:36:59 +0300

    Система лайков правки

--- src/main/java/faang/school/postservice/dto/like/LikeDto.java
+++ src/main/java/faang/school/postservice/dto/like/LikeDto.java
@@ -1,7 +1,6 @@
 package faang.school.postservice.dto.like;
 
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
