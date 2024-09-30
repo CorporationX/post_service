c6d3b2d2e02a6aeaffef0218ac6ac622daf8c985
Okabe <mathew.mackonoha@gmail.com>
2024-09-30 13:46:55 +0300

    refactor of main (LikeControllerTest)

--- src/test/java/faang/school/postservice/controller/LikeControllerTest.java
+++ src/test/java/faang/school/postservice/controller/LikeControllerTest.java
@@ -1,6 +1,8 @@
 package faang.school.postservice.controller;
 
+import faang.school.postservice.dto.like.LikeDto;
 import faang.school.postservice.service.LikeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,13 +10,24 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @ExtendWith(MockitoExtension.class)
 class LikeControllerTest {
-    @Mock
-    private LikeServiceImpl likeService;
     @InjectMocks
     private LikeController likeController;
-
+    @Mock
+    private LikeServiceImpl likeService;
+    private long postId;
+    private LikeDto likeDto;
+    private long commentId;
+    @BeforeEach
+    void setUp() {
+        postId = 3L;
+        commentId = 3L;
+        likeDto = new LikeDto(1L, 2L);
+    }
     @Test
     void getUsersLikedPost_whenOk() {
         long id = 10L;
@@ -32,4 +45,40 @@ class LikeControllerTest {
         Mockito.verify(likeService, Mockito.times(1))
                 .getUsersLikedComm(id);
     }
+
+    @Test
+    void TestAddLikeToPost() {
+        likeController.addLikeToPost(likeDto, postId);
+
+        verify(likeService, times(1)).addLikeToPost(likeDto, postId);
+    }
+
+    @Test
+    void TestDeleteLikeFromPost() {
+        likeController.deleteLikeFromPost(likeDto, postId);
+
+        verify(likeService, times(1)).deleteLikeFromPost(likeDto, postId);
+    }
+
+    @Test
+    void TestAddLikeToComment() {
+        likeController.addLikeToComment(likeDto, commentId);
+
+        verify(likeService, times(1)).addLikeToComment(likeDto, commentId);
+    }
+
+    @Test
+    void deleteLikeFromComment() {
+        likeController.deleteLikeFromComment(likeDto, commentId);
+
+        verify(likeService, times(1)).deleteLikeFromComment(likeDto, commentId);
+    }
+
+    @Test
+    void findLikesOfPublishedPost() {
+        likeController.findLikesOfPublishedPost(postId);
+
+        verify(likeService, times(1)).findLikesOfPublishedPost(postId);
+    }
 }
+
