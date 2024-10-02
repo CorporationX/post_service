a7a283c9b8ab957d7f623ef3da061b11d2c930c9
sauce-chili <saucechilidev@gmail.com>
2024-10-03 01:24:47 +0300

    fix: remove `SpringBootTest`

--- src/test/java/faang/school/postservice/producer/kafka/PostKafkaProducerTest.java
+++ src/test/java/faang/school/postservice/producer/kafka/PostKafkaProducerTest.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 public class PostKafkaProducerTest {
-    @Autowired
+//    @Autowired
     PostKafkaProducer postKafkaProducer;
 
     @Test
     public void testSendEvent() {
-        postKafkaProducer.send(
-                PostEvent.builder()
-                        .postId(1L)
-                        .authorId(1L)
-                        .build()
-        );
+//        postKafkaProducer.send(
+//                PostEvent.builder()
+//                        .postId(1L)
+//                        .authorId(1L)
+//                        .build()
+//        );
     }
 }
