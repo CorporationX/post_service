b435f1239df43aa5039c2eb06ac460603f8f5ae1
Robert Oguzov <robertoguzov2@gmail.com>
2024-10-03 16:21:08 +0300

    Заменил CrudRepository на JpaRepository

--- src/main/java/faang/school/postservice/repository/PostRepository.java
+++ src/main/java/faang/school/postservice/repository/PostRepository.java
@@ -1,16 +1,16 @@
 package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByAuthorId(long authorId);
 
