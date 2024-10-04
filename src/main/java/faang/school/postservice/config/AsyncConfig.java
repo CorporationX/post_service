3b0196c9dbb87bdbb98d857cce2707028984a3fd
Ilia Chudov <chudow-iluha@mail.ru>
2024-10-05 00:15:45 +0300

    BJS2-27077 [Experienced] Проверка постов на оскорбительный контент
    Небольшие изменения

--- src/main/java/faang/school/postservice/config/AsyncConfig.java
+++ src/main/java/faang/school/postservice/config/AsyncConfig.java
@@ -3,10 +3,12 @@ package faang.school.postservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
+@EnableAsync
 @Configuration
 public class AsyncConfig {
     @Value("${async.executor.core-pool-size}")
--- src/main/java/faang/school/postservice/model/Post.java
+++ src/main/java/faang/school/postservice/model/Post.java
@@ -5,6 +5,8 @@ import faang.school.postservice.model.album.Album;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
