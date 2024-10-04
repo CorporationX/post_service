036632bfe9733ecb8549255acd793bfccc95a05c
vestmen <alex.nemtsev95@gmail.com>
2024-10-04 20:42:14 +0300

    BJS2-27040 add fail test

---
+++ src/test/java/faang/school/postservice/CITest.java
@@ -0,0 +1,11 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CITest {
+    @Test
+    void test() {
+        Assertions.fail();
+    }
+}
