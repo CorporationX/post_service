9bcd039ea4b88a272ef6c89d9f44feaa2e35c2fe
ifef228 <ifef228@gmail.com>
2024-10-07 13:39:21 +0300

    конфиг

---
+++ src/main/java/faang/school/postservice/config/RedisPubSubConfig.java
@@ -0,0 +1,16 @@
+package faang.school.postservice.config;
+
+import faang.school.postservice.topic.CommentEventTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisPubSubConfig {
+    @Value("${spring.data.redis.topics.comment_event_topic}")
+    private String topic;
+    @Bean
+    CommentEventTopic commentEventTopic() {
+        return new CommentEventTopic(topic);
+    }
+}
