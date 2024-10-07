e2bc64531e4e20ccebc0eacd3846f29456257cbe
ifef228 <ifef228@gmail.com>
2024-10-07 15:39:12 +0300

    перенастроил конфигурацию

--- src/main/java/faang/school/postservice/config/RedisPubSubConfig.java
+++ src/main/java/faang/school/postservice/config/RedisPubSubConfig.java
@@ -1,9 +1,12 @@
 package faang.school.postservice.config;
 
+import faang.school.postservice.dto.comment.CommentEvent;
 import faang.school.postservice.topic.CommentEventTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisPubSubConfig {
@@ -12,5 +15,17 @@ public class RedisPubSubConfig {
     @Bean
     CommentEventTopic commentEventTopic() {
         return new CommentEventTopic(topic);
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
     }
 }
