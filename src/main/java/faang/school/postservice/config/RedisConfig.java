fe5694628553dd79f812727e9485872dfd20874c
Robert Oguzov <robertoguzov2@gmail.com>
2024-10-07 09:23:51 +0300

    Внес правки

--- src/main/java/faang/school/postservice/config/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/RedisConfig.java
@@ -1,8 +1,6 @@
 package faang.school.postservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
