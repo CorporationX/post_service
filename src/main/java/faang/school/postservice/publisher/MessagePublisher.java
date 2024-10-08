00ad97a611bb6c1aa0951b3b2b06490f429d5f07
Okabe <mathew.mackonoha@gmail.com>
2024-10-08 16:00:21 +0300

    Готова публикация Ивента лайка.

--- src/main/java/faang/school/postservice/publisher/MessagePublisher.java
+++ src/main/java/faang/school/postservice/publisher/MessagePublisher.java
@@ -1,8 +1,5 @@
 package faang.school.postservice.publisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import faang.school.postservice.model.event.LikeEvent;
-
 public interface MessagePublisher {
     public void publish(String message);
 }
