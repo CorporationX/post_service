84c75884ab252fde2d6e9414432d8a37ead37b85
SharkB38 <val.wolf.ne@gmail.com>
2024-10-10 11:25:39 +0900

    deleted resourcesIds from postDto

--- src/main/java/faang/school/postservice/mapper/post/PostMapper.java
+++ src/main/java/faang/school/postservice/mapper/post/PostMapper.java
@@ -4,13 +4,11 @@ import faang.school.postservice.dto.post.PostDto;
 import faang.school.postservice.dto.post.request.PostCreationRequest;
 import faang.school.postservice.model.Like;
 import faang.school.postservice.model.Post;
-import faang.school.postservice.model.Resource;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -19,7 +17,6 @@ public interface PostMapper {
     Post toPostFromCreationRequest(PostCreationRequest postCreationRequest);
 
     @Mapping(source = "likes", target = "likes", qualifiedByName = "getLikesCount")
-    @Mapping(source = "resources", target = "resources", qualifiedByName = "getResourcesKey")
     PostDto toPostDto(Post post);
 
     List<PostDto> toPostDtoList(List<Post> posts);
@@ -27,15 +24,5 @@ public interface PostMapper {
     @Named("getLikesCount")
     default int getLikesCount(List<Like> likes) {
         return likes.size();
-    }
-
-    @Named("getResourcesKey")
-    default List<String> getResourcesKey(List<Resource> resources) {
-        if (resources == null) {
-            return new ArrayList<>();
-        }
-        return resources.stream()
-                .map(Resource::getKey)
-                .toList();
     }
 }
