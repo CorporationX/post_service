be3cb6a6d151499907a946152d33f15a579ff05a
ifef228 <ifef228@gmail.com>
2024-10-10 20:05:07 +0300

    поправил

--- src/main/java/faang/school/postservice/config/RedisPubSubConfig.java
+++ src/main/java/faang/school/postservice/config/RedisPubSubConfig.java
@@ -1,31 +1,23 @@
 package faang.school.postservice.config;
 
-import faang.school.postservice.dto.comment.CommentEvent;
 import faang.school.postservice.topic.CommentEventTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisPubSubConfig {
     @Value("${spring.data.redis.topics.comment_event_topic}")
     private String topic;
+
     @Bean
     CommentEventTopic commentEventTopic() {
         return new CommentEventTopic(topic);
     }
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
     RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
+        return new RedisTemplate<>();
     }
 }
--- src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
@@ -2,7 +2,6 @@ package faang.school.postservice.publisher;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import faang.school.postservice.topic.CommentEventTopic;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +14,10 @@ import org.springframework.stereotype.Component;
 public class CommentEventPublisher implements MessagePublisher {
     private final RedisTemplate<String, String> redisTemplate;
     private final CommentEventTopic topic;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void publish(Object message) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
         try {
             String json = objectMapper.writeValueAsString(message);
             redisTemplate.convertAndSend(topic.getTopic(), json);
