b74cad6bd31cef56b36b83179f8ecabe0fd45212
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-10-11 14:54:11 +0500

    Refactoring: Deleted Sl4j

--- src/main/java/faang/school/postservice/publisher/AbstractEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/AbstractEventPublisher.java
@@ -1,11 +1,9 @@
 package faang.school.postservice.publisher;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 
-@Slf4j
 @RequiredArgsConstructor
 public abstract class AbstractEventPublisher<T> {
     private final RedisTemplate<String, Object> redisTemplate;
