1fdbec8c44539dbab4cd8381a431a37422e47c5e
PLSGOTOASS <koteykaarisha@mail.ru>
2024-10-14 06:33:03 +0300

    выполнил задание без тестов

---
+++ src/main/java/faang/school/postservice/dto/redis/CommentEvent.java
@@ -0,0 +1,16 @@
+package faang.school.postservice.dto.redis;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentEvent {
+    private long commentAuthorId;
+    private long postAuthorId;
+    private long postId;
+    private long commentId;
+    private String comment;
+}
