fd235b4739908cc87c7c7afee38529ccfda9b61b
Artem Voevoda <voevodacode@gmail.com>
2024-10-15 21:04:01 +0300

    typo

--- src/test/java/faang/school/postservice/service/impl/AsyncPostPublishServiceImplTest.java
+++ src/test/java/faang/school/postservice/service/impl/AsyncPostPublishServiceImplTest.java
@@ -2,6 +2,7 @@ package faang.school.postservice.service.impl;
 
 import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
+import faang.school.postservice.service.AsyncPostPublishService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
