145310eea79f3a84cd9c02a6f7362e2668993764
azhibek_a <azhibek.assylkhan@gmail.com>
2024-10-16 19:41:50 +0500

    naming channels

--- src/main/java/faang/school/postservice/scheduler/AuthorBanner.java
+++ src/main/java/faang/school/postservice/scheduler/AuthorBanner.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@EnableRetry
 public class AuthorBanner {
 
     private final PostService postService;
