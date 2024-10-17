6ab2bcad2a480805fc2ceb48ba5aed599e1f81d0
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-10-17 18:58:32 +0700

    доработка

--- src/main/java/faang/school/postservice/service/post/impl/PostServiceImpl.java
+++ src/main/java/faang/school/postservice/service/post/impl/PostServiceImpl.java
@@ -26,7 +26,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.apache.commons.collections4.ListUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
