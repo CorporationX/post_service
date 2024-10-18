fc7c620a4377fcd964681a8337c4bd6c320e4f57
Artem Voevoda <voevodacode@gmail.com>
2024-10-18 17:39:52 +0300

    typo

--- src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
@@ -8,10 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+// need AbstractEventPublisher<T>
 @Component
 @RequiredArgsConstructor
 @Slf4j
-// todo: need AbstractEventPublisher<T>
 public class LikeEventPublisher implements MessagePublisher {
 
     private final RedisTemplate<String, String> redisTemplate;
