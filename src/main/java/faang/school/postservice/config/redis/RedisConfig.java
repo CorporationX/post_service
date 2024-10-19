f49014577058125d225635cc76a7cb19e022c2ba
PLSGOTOASS <koteykaarisha@mail.ru>
2024-10-19 18:12:34 +0300

    поправил

--- src/main/java/faang/school/postservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfig.java
@@ -2,10 +2,7 @@ package faang.school.postservice.config.redis;
 
 import faang.school.postservice.publisher.LikeEventPublisher;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
