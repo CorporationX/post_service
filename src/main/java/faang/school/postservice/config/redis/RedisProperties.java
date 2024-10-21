74ec3381be6b7a752f862cd63e712e78c0ae41fa
Владислав Воронов <iPlesk90@yandex.ru>
2024-10-21 12:01:54 +0300

    Внес правки

--- src/main/java/faang/school/postservice/config/redis/RedisProperties.java
+++ src/main/java/faang/school/postservice/config/redis/RedisProperties.java
@@ -1,7 +1,9 @@
 package faang.school.postservice.config.redis;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.redis.listener.ChannelTopic;
 
 import java.util.Map;
 
@@ -11,8 +13,14 @@ public class RedisProperties {
     private int port;
     private String host;
     private Map<String, String> channels;
+    private ChannelTopic commentChannel;
 
     public String getChannel(String channelKey) {
         return channels.get(channelKey);
+    }
+
+    @PostConstruct
+    public void init() {
+        this.commentChannel = new ChannelTopic(getChannel("comment-channel"));
     }
 }
--- src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
@@ -3,12 +3,10 @@ package faang.school.postservice.publisher;
 
 import faang.school.postservice.config.redis.RedisProperties;
 import faang.school.postservice.dto.comment.CommentEvent;
-import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -20,15 +18,8 @@ public class CommentEventPublisher implements MessagePublisher<CommentEvent> {
     private final RedisTemplate<String, Object> redisTemplate;
     private final RedisProperties redisProperties;
 
-    private ChannelTopic topic;
-
-    @PostConstruct
-    public void init() {
-        this.topic = new ChannelTopic(redisProperties.getChannel("comment-channel"));
-    }
-
     @Override
     public void publish(CommentEvent message) {
-        redisTemplate.convertAndSend(topic.getTopic(), message);
+        redisTemplate.convertAndSend(redisProperties.getCommentChannel().getTopic(), message);
     }
 }
