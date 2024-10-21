a4fdb8f432bc1f3003b175fc879a6adda98afe8a
Okabe <mathew.mackonoha@gmail.com>
2024-10-21 17:01:43 +0300

    Готово

--- src/main/java/faang/school/postservice/config/redis/RedisConfiguration.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfiguration.java
@@ -1,14 +1,11 @@
 package faang.school.postservice.config.redis;
 
-import faang.school.postservice.listener.LikeEventListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -31,18 +28,6 @@ public class RedisConfiguration {
         return template;
     }
 
-    @Bean
-    MessageListenerAdapter likeEventListenerAdapter(LikeEventListener likeEventListener) {
-        return new MessageListenerAdapter(likeEventListener);
-    }
-
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(MessageListenerAdapter likeEventListenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(jedisConnectionFactory());
-        container.addMessageListener(likeEventListenerAdapter, likeEventTopic());
-        return container;
-    }
 
     @Bean
     ChannelTopic likeEventTopic() {
