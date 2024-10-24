1e645ddd3e4fced0331a59a8680d207181068846
Nikita Korolev <nk25.dev@gmail.com>
2024-10-24 19:29:48 +0300

    Changes requested by reviewer

--- src/main/java/faang/school/postservice/redis/publisher/BanAuthorPublisher.java
+++ src/main/java/faang/school/postservice/redis/publisher/BanAuthorPublisher.java
@@ -1,7 +1,5 @@
 package faang.school.postservice.redis.publisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.config.redis.RedisProperties;
 import faang.school.postservice.redis.publisher.dto.AuthorBanDto;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +14,12 @@ import java.util.Objects;
 public class BanAuthorPublisher implements MessagePublisher {
     private final StringRedisTemplate redisTemplate;
     private final RedisProperties properties;
-    private final ObjectMapper mapper;
 
     @Override
     public void publish(AuthorBanDto dto) {
         if (Objects.isNull(dto) || Objects.isNull(dto.userId())) {
             throw new IllegalStateException("Can't publish author for ban action: message cannot be null or empty");
         }
-        try {
-            String data = mapper.writeValueAsString(dto);
-            redisTemplate.convertAndSend(properties.getUserBanChannelName(), data);
-        } catch(JsonProcessingException e) {
-            throw new IllegalStateException("Faced issues during serialization of AuthorBanDto");
-        }
+        redisTemplate.convertAndSend(properties.getUserBanChannelName(), dto);
     }
 }
