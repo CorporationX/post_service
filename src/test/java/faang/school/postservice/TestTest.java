6576213b8f54c4b0dbff77516783d7b9bf869174
AlekseyB86 <avbelov86@mail.ru>
2024-11-14 19:05:49 +0300

    создал test

---
+++ src/test/java/faang/school/postservice/TestTest.java
@@ -0,0 +1,16 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(MockitoExtension.class)
+public class TestTest {
+    @Test
+    void test(){
+        fail();
+    }
+
+}
