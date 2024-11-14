7dc90f96f777986ab2e34391046550a5676fdfb3
AlekseyB86 <avbelov86@mail.ru>
2024-11-14 20:43:55 +0300

    test fail

---
+++ src/test/java/faang/school/postservice/TestTest.java
@@ -0,0 +1,11 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+    @Test
+    void testFail(){
+        Assertions.fail("++++++++++++++hfjshdfkjdhfjsfjfj++++++++");
+    }
+}
