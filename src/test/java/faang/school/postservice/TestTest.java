bc3d6e3346affd5aed7e0cad380bbbe697d9285c
AlekseyB86 <avbelov86@mail.ru>
2024-11-14 19:36:18 +0300

    создал тест

---
+++ src/test/java/faang/school/postservice/TestTest.java
@@ -0,0 +1,12 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+    @Test
+    void testFail() {
+        Assertions.fail("+++++++++FDSFKSDHJ SDJFHSJERUERU498544RE FWERFHEWIF FGSJKFGHSJKFDJF++++++++");
+    }
+}
