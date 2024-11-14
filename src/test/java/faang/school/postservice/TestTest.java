8969230e4899fd5f13314c72547db2156bef3f41
AlekseyB86 <avbelov86@mail.ru>
2024-11-14 19:23:04 +0300

    test fail

--- src/test/java/faang/school/postservice/TestTest.java
+++ src/test/java/faang/school/postservice/TestTest.java
@@ -1,16 +1,13 @@
 package faang.school.postservice;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-@ExtendWith(MockitoExtension.class)
 public class TestTest {
     @Test
-    void test(){
-        fail();
+    void testFail(){
+        fail("This is fail test!!! ё-хо-хо и бутылка рома ");
     }
 
 }
