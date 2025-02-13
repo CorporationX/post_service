eacd3f9cadf5e08d78f4786ea15d26986f9c5a70
DmitryQueen <276246@student.pwr.edu.pl>
2025-02-13 18:16:30 +0100

    changes serialization value to String and kafka config partially moved to class

--- src/main/java/faang/school/postservice/config/KafkaConfiguration.java
+++ src/main/java/faang/school/postservice/config/KafkaConfiguration.java
@@ -1,18 +1,33 @@
 package faang.school.postservice.config;
 
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
 
-    @Value("${kafka.topic.ban_user}")
-    private String banUserTopic;
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServer;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
 
     @Bean
-    public NewTopic newTopic() {
-        return new NewTopic(banUserTopic, 1, (short) 1);
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        return new DefaultKafkaProducerFactory<>(configProps);
     }
 }
