35392f149949f8a5e7eb64e32ab523e51d6c06a6
Vadim Naletov <naletov96@gmail.com>
2025-03-23 17:49:44 +0300

    minor updates

--- src/main/java/faang/school/postservice/controller/PostController.java
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -1,93 +1,83 @@
 package faang.school.postservice.controller;
 
 import faang.school.postservice.dto.PostDto;
+import faang.school.postservice.exception.DataValidationException;
 import faang.school.postservice.service.PostService;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/posts")
+@Validated
 public class PostController {
 
     private final PostService postService;
 
-    @PostMapping("/posts")
+    @PostMapping
     public PostDto createDraft(@RequestBody PostDto postDto) {
-        if (isInvalidToCreate(postDto)) {
-            throw new IllegalArgumentException("Data is not enough to create a new post");
-        }
+        isInvalidToCreate(postDto);
         return postService.createDraft(postDto);
     }
 
-    @PutMapping("/posts/{postId}/publish")
-    public PostDto publishPost(@PathVariable Long postId) {
-        validateId(postId);
+    @PutMapping("/{postId}/publish")
+    public PostDto publishPost(@PathVariable @Positive Long postId) {
         return postService.publishPost(postId);
     }
 
-    @PutMapping("/posts/{postId}")
-    public PostDto updatePost(@PathVariable Long postId, @RequestBody PostDto postDto) {
-        validateId(postId);
-        if (isInvalidToCreate(postDto)) {
-            throw new IllegalArgumentException("Data is not enough to update the post");
-        }
+    @PutMapping("/{postId}")
+    public PostDto updatePost(@PathVariable @Positive Long postId, @RequestBody PostDto postDto) {
+        isInvalidToCreate(postDto);
         return postService.updatePost(postId, postDto);
     }
 
-    @DeleteMapping("/posts/{postId}")
-    public PostDto softDelete(@PathVariable Long postId) {
-        validateId(postId);
+    @DeleteMapping("/{postId}")
+    public PostDto softDelete(@PathVariable @Positive Long postId) {
         return postService.softDelete(postId);
     }
 
-    @GetMapping("/posts/{postId}")
-    public PostDto getPostById(@PathVariable Long postId) {
-        validateId(postId);
+    @GetMapping("/{postId}")
+    public PostDto getPostById(@PathVariable @Positive Long postId) {
         return postService.getPostById(postId);
     }
 
     @GetMapping("/users/{authorId}/drafts")
-    public List<PostDto> getAllDraftsByAuthorId(@PathVariable Long authorId) {
-        validateId(authorId);
+    public List<PostDto> getAllDraftsByAuthorId(@PathVariable @Positive Long authorId) {
         return postService.getAllDraftsByAuthorId(authorId);
     }
 
     @GetMapping("/projects/{projectId}/drafts")
-    public List<PostDto> getAllDraftsByProjectId(@PathVariable Long projectId) {
-        validateId(projectId);
+    public List<PostDto> getAllDraftsByProjectId(@PathVariable @Positive Long projectId) {
         return postService.getAllDraftsByProjectId(projectId);
     }
 
     @GetMapping("/users/{authorId}/published")
-    public List<PostDto> getAllPublishedPostsByAuthorId(@PathVariable Long authorId) {
-        validateId(authorId);
+    public List<PostDto> getAllPublishedPostsByAuthorId(@PathVariable @Positive Long authorId) {
         return postService.getAllPublishedPostsByAuthorId(authorId);
     }
 
     @GetMapping("/projects/{projectId}/published")
-    public List<PostDto> getAllPublishedPostsByProjectId(@PathVariable Long projectId) {
-        validateId(projectId);
+    public List<PostDto> getAllPublishedPostsByProjectId(@PathVariable @Positive Long projectId) {
         return postService.getAllPublishedPostsByProjectId(projectId);
     }
 
-    private boolean isInvalidToCreate(PostDto postDto) {
+    private void isInvalidToCreate(PostDto postDto) {
         if (postDto.getContent() == null || postDto.getContent().isBlank()) {
-            return true;
+            throw new IllegalArgumentException("Data is not enough to update the post");
         }
-        return (postDto.getAuthorId() != null) == (postDto.getProjectId() != null);
-    }
-
-    private static void validateId(Long id) {
-        if (id <= 0) {
-            throw new IllegalArgumentException("Not positive id");
+        if ((postDto.getAuthorId() != null) == (postDto.getProjectId() != null)) {
+            throw new DataValidationException("Can be only one author");
         }
     }
 }
