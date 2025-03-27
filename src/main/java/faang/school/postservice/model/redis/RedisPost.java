a8c04b86b8ee8e3fb2a7f8408532b8883ada5492
Stanislav <stasikbygaev@gmail.com>
2025-03-27 17:02:44 +0500

    Правки

--- src/main/java/faang/school/postservice/model/redis/RedisPost.java
+++ src/main/java/faang/school/postservice/model/redis/RedisPost.java
@@ -1,7 +1,7 @@
 package faang.school.postservice.model.redis;
 
-import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
