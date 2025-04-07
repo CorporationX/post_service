c7336bf5dccef6bdd1955690c129136baa26a3c3
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-04-07 17:51:57 +0300

    Handler removed to exceptions

--- src/main/java/faang/school/postservice/exception/handler/ErrorResponse.java
+++ src/main/java/faang/school/postservice/exception/handler/ErrorResponse.java
@@ -1,4 +1,4 @@
-package faang.school.postservice.controller.handler;
+package faang.school.postservice.exception.handler;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
