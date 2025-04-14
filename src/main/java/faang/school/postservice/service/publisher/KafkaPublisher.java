fa205cb8d17bdd18bc17ae44cfaa891f0fabe008
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-04-14 14:53:18 +0300

    сделал тесты

--- src/main/java/faang/school/postservice/service/publisher/KafkaPublisher.java
+++ src/main/java/faang/school/postservice/service/publisher/KafkaPublisher.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class KafkaPublisher {
+    public static final String FAILED_SERIALIZING_OBJECT = "Failed to serialize object";
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper;
 
@@ -20,8 +22,8 @@ public class KafkaPublisher {
             kafkaTemplate.send(topic, message);
         } catch (JsonProcessingException e) {
             log.error("Serialization error for object: {}", object, e);
-            throw new RuntimeException("Failed to serialize object", e);
+            throw new RuntimeException(FAILED_SERIALIZING_OBJECT, e);
         }
-        log.info("Sent object: {}, to topic: {}", object.getClass(), topic);
+        log.info("Sent object: {}, to topic: {}", object.getClass().getName(), topic);
     }
 }
