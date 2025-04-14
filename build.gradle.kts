115ff23dd5c5d214023ae54484d0b7e894d0d365
Max Goloviznin <roxet59rus@mail.ru>
2025-04-14 17:33:29 +0500

    (BJS2-65467) exclude integration tests on CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,6 +103,9 @@ tasks.jacocoTestCoverageVerification {
 
 tasks.test {
     useJUnitPlatform()
+    if (System.getenv("CI") == "true") {
+        exclude("**/*IT*", "**/*IntegrationTest*")
+    }
     finalizedBy(tasks.jacocoTestReport)
 }
 
