300e69660ddd6cdd1d82a620d191e1c743c504ba
Ilyas Azhdar <azhdar902@gmail.com>
2025-04-19 12:24:47 +0500

    changed kafkatopic config

--- src/main/java/faang/school/postservice/config/kafka/KafkaTopic.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaTopic.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.config.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,8 +9,11 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopic {
 
+    @Value("${spring.data.kafka.topic.comment}")
+    private String commentTopic;
+
     @Bean
     public NewTopic comment() {
-        return TopicBuilder.name("comment").partitions(2).build();
+        return TopicBuilder.name(commentTopic).partitions(2).build();
     }
 }
--- src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
@@ -1,8 +1,8 @@
 package faang.school.postservice.publisher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.postservice.config.kafka.KafkaTopic;
 import faang.school.postservice.dto.kafkaevents.CommentEvent;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommentEventPublisher extends AbstractEventPublisher implements KafkaEventPublisher<CommentEvent> {
 
-    @Value("${spring.data.kafka.topic.comment}")
-    private String topic;
+    private final KafkaTopic kafkaTopic;
 
-    public CommentEventPublisher(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+    public CommentEventPublisher(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper, KafkaTopic kafkaTopic) {
         super(kafkaTemplate, objectMapper);
+        this.kafkaTopic = kafkaTopic;
     }
 
     @Override
     public void publish(CommentEvent event) {
-        sendMessage(event, topic);
+        sendMessage(event, kafkaTopic.comment().name());
     }
 }
