31ef9c4b8f7027d07aa1db5f2c7dddc53bc90aaa
Max Goloviznin <roxet59rus@mail.ru>
2025-04-21 00:38:43 +0500

    Revert "(BJS2-65255) test"
    This reverts commit 1eef75d5841f4ca3fa5287321fb6d25cca008b04.

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,6 +96,8 @@ tasks.jacocoTestReport {
 tasks.jacocoTestCoverageVerification {
     violationRules {
         rule {
+            enabled = false
+
             limit {
                 counter = "CLASS"
                 value = "COVEREDRATIO"
