c017b78a88b3b535d1310e094baacdc642072146
RamilNazmiev192 <nazmiev633@gmail.com>
2025-04-23 11:30:24 +0300

    add: добавление теста для проверки публикаций

--- src/test/java/faang/school/postservice/controller/comment/CommentControllerTest.java
+++ src/test/java/faang/school/postservice/controller/comment/CommentControllerTest.java
@@ -1,6 +1,8 @@
 package faang.school.postservice.controller.comment;
 
 import faang.school.postservice.dto.comment.CommentDto;
+import faang.school.postservice.dto.event.CommentEvent;
+import faang.school.postservice.publisher.CommentEventPublisher;
 import faang.school.postservice.service.comment.CommentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,84 +13,91 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-    @ExtendWith(MockitoExtension.class)
-    public class CommentControllerTest {
+@ExtendWith(MockitoExtension.class)
+public class CommentControllerTest {
 
-        @Mock
-        private CommentService commentService;
+    @Mock
+    private CommentService commentService;
 
-        @InjectMocks
-        private CommentController commentController;
+    @Mock
+    private CommentEventPublisher commentEventPublisher;
 
-        private CommentDto commentDto;
-        private final Long postId = 1L;
-        private final Long commentId = 1L;
+    @InjectMocks
+    private CommentController commentController;
 
-        @BeforeEach
-        void setUp() {
-            commentDto = CommentDto.builder()
-                    .id(commentId)
-                    .content("Test comment")
-                    .authorId(1L)
-                    .postId(1L)
-                    .build();
-        }
+    private CommentDto commentDto;
+    private final Long postId = 1L;
+    private final Long commentId = 1L;
 
-        @Test
-        void testCreateComment() {
-            when(commentService.createComment(postId, commentDto)).thenReturn(commentDto);
-
-            ResponseEntity<CommentDto> response = commentController.createComment(postId, commentDto);
-
-            assertNotNull(response);
-            assertEquals(200, response.getStatusCode().value());
-            assertEquals(commentDto, response.getBody());
-            verify(commentService, times(1)).createComment(postId, commentDto);
-        }
-
-        @Test
-        void testUpdateComment() {
-            when(commentService.updateComment(commentId, commentDto)).thenReturn(commentDto);
-
-            ResponseEntity<CommentDto> response = commentController.updateComment(commentId, commentDto);
-
-            assertNotNull(response);
-            assertEquals(200, response.getStatusCode().value());
-            assertEquals(commentDto, response.getBody());
-            verify(commentService, times(1)).updateComment(commentId, commentDto);
-        }
-
-        @Test
-        void testGetCommentPostId() {
-            List<CommentDto> comments = List.of(commentDto);
-            when(commentService.getAllComments(postId)).thenReturn(comments);
-
-            ResponseEntity<List<CommentDto>> response = commentController.getCommentPostId(postId);
-
-            assertNotNull(response);
-            assertEquals(200, response.getStatusCode().value());
-            assertEquals(comments, response.getBody());
-            verify(commentService, times(1)).getAllComments(postId);
-        }
-
-        @Test
-        void testDeleteComment() {
-            doNothing().when(commentService).deleteComment(commentId);
-
-            ResponseEntity<Map<String, String>> response = commentController.deleteComment(commentId);
-
-            assertNotNull(response);
-            assertEquals(HttpStatus.OK, response.getStatusCode());
-
-            Map<String, String> expected = Map.of("message", "Comment with ID " + commentId + " deleted");
-            assertEquals(expected, response.getBody());
-
-            verify(commentService, times(1)).deleteComment(commentId);
-        }
+    @BeforeEach
+    void setUp() {
+        commentDto = CommentDto.builder()
+                .id(commentId)
+                .content("Test comment")
+                .authorId(1L)
+                .postId(1L)
+                .build();
     }
+
+    @Test
+    void testCreateComment() {
+        when(commentService.createComment(postId, commentDto)).thenReturn(commentDto);
+        doNothing().when(commentEventPublisher).publish(any(CommentEvent.class));
+
+
+        ResponseEntity<CommentDto> response = commentController.createComment(postId, commentDto);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(commentDto, response.getBody());
+        verify(commentService, times(1)).createComment(postId, commentDto);
+        verify(commentEventPublisher, times(1)).publish(any(CommentEvent.class));
+    }
+
+    @Test
+    void testUpdateComment() {
+        when(commentService.updateComment(commentId, commentDto)).thenReturn(commentDto);
+
+        ResponseEntity<CommentDto> response = commentController.updateComment(commentId, commentDto);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(commentDto, response.getBody());
+        verify(commentService, times(1)).updateComment(commentId, commentDto);
+    }
+
+    @Test
+    void testGetCommentPostId() {
+        List<CommentDto> comments = List.of(commentDto);
+        when(commentService.getAllComments(postId)).thenReturn(comments);
+
+        ResponseEntity<List<CommentDto>> response = commentController.getCommentPostId(postId);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(comments, response.getBody());
+        verify(commentService, times(1)).getAllComments(postId);
+    }
+
+    @Test
+    void testDeleteComment() {
+        doNothing().when(commentService).deleteComment(commentId);
+
+        ResponseEntity<Map<String, String>> response = commentController.deleteComment(commentId);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        Map<String, String> expected = Map.of("message", "Comment with ID " + commentId + " deleted");
+        assertEquals(expected, response.getBody());
+
+        verify(commentService, times(1)).deleteComment(commentId);
+    }
+}
