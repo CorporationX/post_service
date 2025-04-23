d33fd721025d678350089e85a872785eb295fe97
Артемий <xkmitx@gmail.com>
2025-04-23 23:47:57 +0300

    hotfix

--- src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/LikeEventPublisher.java
@@ -1,18 +1,30 @@
 package faang.school.postservice.publisher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.dto.like.LikeEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LikeEventPublisher {
     private final RedisTemplate<String, String> redisTemplate;
     private final ChannelTopic channelTopic;
+    private final ObjectMapper objectMapper;
 
     public void publish(LikeEvent event) {
-        redisTemplate.convertAndSend(channelTopic.getTopic(), event);
+        try {
+            String message = objectMapper.writeValueAsString(event);
+            redisTemplate.convertAndSend(channelTopic.getTopic(), message);
+            log.info("m: {}, t: {}", message, channelTopic.getTopic());
+        } catch (JsonProcessingException e) {
+            log.error("Failed to serialize LikeEvent", e);
+            throw new RuntimeException(e);
+        }
     }
 }
