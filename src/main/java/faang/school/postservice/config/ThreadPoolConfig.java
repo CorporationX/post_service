e2f11b296d9f1c94f34014b4df6ff914f985f490
SergeyErmoshenko <ermoshenko.work@gmail.com>
2025-04-23 14:39:10 +0300

    фикс конфига

--- src/main/java/faang/school/postservice/config/ThreadPoolConfig.java
+++ src/main/java/faang/school/postservice/config/ThreadPoolConfig.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Executors;
 
 @Configuration
 public class ThreadPoolConfig {
-    @Value("${threadpool.size}")
+    @Value("${app.thread-pool-size}")
     private int threadPoolSize;
 
     @Bean
--- src/main/java/faang/school/postservice/scheduler/ScheduledPostPublisher.java
+++ src/main/java/faang/school/postservice/scheduler/ScheduledPostPublisher.java
@@ -2,7 +2,6 @@ package faang.school.postservice.scheduler;
 
 import faang.school.postservice.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +9,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ScheduledPostPublisher {
     private final PostService postService;
-    @Value("${scheduler.publish-cron}")
-    private String publishCron;
 
-    @Scheduled(cron = "${scheduler.publish-cron}")
+    @Scheduled(cron = "${posts.publishing.cron}")
     public void publicScheduledPostJob() {
         postService.publishScheduledPosts();
     }
