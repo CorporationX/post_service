217a35f9b8911cb02262c9b42bc49b8e5459fe5c
Kot-Mars <61290390+Kot-Mars@users.noreply.github.com>
2025-05-01 21:33:04 +0300

    Анализ комментариев (#1791)
    Анализ комментариев

--- src/main/java/faang/school/postservice/service/comment/CommentServiceImpl.java
+++ src/main/java/faang/school/postservice/service/comment/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package faang.school.postservice.service.comment;
 import faang.school.postservice.client.CommentAnalyzer;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.contants.ErrorMessage;
+import faang.school.postservice.dto.comment.CommentEvent;
 import faang.school.postservice.dto.comment.CommentRequestDto;
 import faang.school.postservice.dto.comment.CommentResponseDto;
 import faang.school.postservice.dto.comment.CommentUpdateDto;
@@ -16,6 +17,7 @@ import faang.school.postservice.mapper.CommentRequestMapper;
 import faang.school.postservice.mapper.CommentResponseMapper;
 import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Post;
+import faang.school.postservice.publisher.CommentEventPublisher;
 import faang.school.postservice.repository.CommentRepository;
 import faang.school.postservice.repository.PostRepository;
 import faang.school.postservice.service.kafka.publisher.KafkaPublisher;
@@ -103,6 +105,7 @@ public class CommentServiceImpl implements CommentService {
     private final CommentRequestMapper commentRequestMapper;
     private final CommentResponseMapper commentResponseMapper;
     private final UserServiceClient userServiceClient;
+    private final CommentEventPublisher commentEventPublisher;
     private ExecutorService executor;
 
     @PostConstruct
@@ -171,6 +174,8 @@ public class CommentServiceImpl implements CommentService {
         comment.setAuthorId(commentRequestDto.getAuthorId());
         commentRepository.save(comment);
         log.info(INFO_CREATE_COMMENT, comment.getId(), commentRequestDto.getAuthorId(), commentRequestDto.getPostId());
+        commentEventPublisher.publish(new CommentEvent(commentRequestDto.getPostId(), commentRequestDto.getAuthorId(),
+                comment.getId(), LocalDateTime.now()));
     }
 
     public void updateComment(Long id, CommentUpdateDto commentUpdateDto) {
