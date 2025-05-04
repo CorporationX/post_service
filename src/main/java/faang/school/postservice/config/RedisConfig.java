e747ba3239d8e1c3673d37d5f6e21114c51b575b
Артемий <xkmitx@gmail.com>
2025-05-04 17:18:31 +0300

    BJS2-66438: Fix conflict

--- src/main/java/faang/school/postservice/config/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/RedisConfig.java
@@ -11,7 +11,6 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
