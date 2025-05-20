b4d8823cdd1218b5f1b273aa758eda1dc4ba8524
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-05-20 16:29:01 +0300

    BJS2-77199 check pull request for CI Pipeline

--- src/test/java/faang/school/postservice/PostServiceAppTest.java
+++ src/test/java/faang/school/postservice/PostServiceAppTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostServiceAppTest {
+
     @Test
     public void testFail() {
-        fail();
-//        assertTrue(true);
+//        fail();
+        assertTrue(true);
     }
 }
