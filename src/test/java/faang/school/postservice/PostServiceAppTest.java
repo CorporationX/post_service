799072bd29b50a743275a4c8d88135ae5065f71f
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-05-20 15:09:31 +0300

    BJS2-77199 Post Service CI Pipeline + jacoco

---
+++ src/test/java/faang/school/postservice/PostServiceAppTest.java
@@ -0,0 +1,13 @@
+package faang.school.postservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceAppTest {
+
+    @Test
+    public void testApplication() {
+        assertTrue(true);
+    }
+}
