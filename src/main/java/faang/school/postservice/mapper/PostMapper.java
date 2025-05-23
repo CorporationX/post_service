81a041b404376b9f14c36e307f266d442eaf0a47
Buga Dan <danbil.942@gmail.com>
2025-05-23 16:03:14 +0300

    Add Comment, Like, and Post functionalities
    This commit introduces the initial implementation for managing comments, likes, and posts.
    Includes:
    - Data models (DTOs) for Comment, Like, and Post.
    - Interfaces and implementations for services (CommentService, LikeService, PostService).
    - Mappers for converting between DTOs and entities.
    - Controllers (LikeController) for exposing functionalities via API.
    - Initial unit tests for services and controllers.

---
+++ src/main/java/faang/school/postservice/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package faang.school.postservice.mapper;
+
+import faang.school.postservice.dto.like.PostDto;
+import faang.school.postservice.model.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PostMapper {
+    Post toEntity(PostDto postDto);
+
+    @Mapping(target = "likeCount",
+            expression = "java(post.getLikes() != null ? post.getLikes().size() : 0)")
+    PostDto toDto(Post post);
+}
