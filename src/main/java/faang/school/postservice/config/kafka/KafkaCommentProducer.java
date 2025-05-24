a51ef9548f1952cf6e71c1bc45d69adbe5ad0712
Артемий <xkmitx@gmail.com>
2025-05-24 19:21:06 +0300

    BJS2-66673: create KafkaCommentProducer

---
+++ src/main/java/faang/school/postservice/config/kafka/KafkaCommentProducer.java
@@ -0,0 +1,24 @@
+package faang.school.postservice.config.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.postservice.dto.comment.CommentEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaCommentProducer extends AbstractKafkaProducer {
+    @Value("${spring.kafka.producer.topics.news-feed-comment}")
+    private String newsFeedCommentTopic;
+
+    public KafkaCommentProducer(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        super(kafkaTemplate, objectMapper);
+    }
+
+    public void send(CommentEvent comment) {
+        send(newsFeedCommentTopic, comment);
+        log.info("News feed comment sent to topic: " + newsFeedCommentTopic);
+    }
+}
--- src/main/java/faang/school/postservice/service/comment/CommentServiceImpl.java
+++ src/main/java/faang/school/postservice/service/comment/CommentServiceImpl.java
@@ -2,6 +2,7 @@ package faang.school.postservice.service.comment;
 
 import faang.school.postservice.client.CommentAnalyzer;
 import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.config.kafka.KafkaCommentProducer;
 import faang.school.postservice.config.kafka.NotificationKafkaProducer;
 import faang.school.postservice.contants.ErrorMessage;
 import faang.school.postservice.dto.comment.CommentEvent;
@@ -111,6 +112,7 @@ public class CommentServiceImpl implements CommentService {
     private final UserServiceClient userServiceClient;
     private final CommentEventPublisher commentEventPublisher;
     private final NotificationKafkaProducer notificationKafkaProducer;
+    private final KafkaCommentProducer kafkaCommentProducer;
     private ExecutorService executor;
 
     @PostConstruct
@@ -183,8 +185,10 @@ public class CommentServiceImpl implements CommentService {
                 commentRequestDto.getAuthorId(),
                 comment.getId(),
                 LocalDateTime.now()));
-        commentEventPublisher.publish(new CommentEvent(commentRequestDto.getPostId(), commentRequestDto.getAuthorId(),
-                comment.getId(), LocalDateTime.now()));
+        CommentEvent commentEvent = new CommentEvent(commentRequestDto.getPostId(), commentRequestDto.getAuthorId(),
+                comment.getId(), LocalDateTime.now());
+        commentEventPublisher.publish(commentEvent);
+        kafkaCommentProducer.send(commentEvent);
         log.info(INFO_CREATE_COMMENT, comment.getId(), commentRequestDto.getAuthorId(), commentRequestDto.getPostId());
     }
 
