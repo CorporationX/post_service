b987923dad7231790616a75fd8c906a7cafa62d2
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-05-24 22:10:38 +0300

    Исправил PostRedisRepository

--- src/main/java/faang/school/postservice/repository/ad/PostRedisRepository.java
+++ src/main/java/faang/school/postservice/repository/ad/PostRedisRepository.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.repository.ad;
 
 import faang.school.postservice.dto.post.PostResponseDto;
+import faang.school.postservice.utils.JsonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,16 +13,17 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class PostRedisRepository {
     private static final String KEY_PREFIX = "post:";
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final JsonUtils jsonUtils;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Value("${spring.data.redis.post.post-ttl-hours}")
     private int postTtl;
 
     public void savePost(Long id, PostResponseDto post) {
-        redisTemplate.opsForValue().set(KEY_PREFIX + id, post, Duration.ofHours(postTtl));
+        redisTemplate.opsForValue().set(KEY_PREFIX + id, jsonUtils.toJson(post), Duration.ofHours(postTtl));
     }
 
     public PostResponseDto getPost(Long id) {
-        return (PostResponseDto) redisTemplate.opsForValue().get(KEY_PREFIX + id);
+        return jsonUtils.fromJson(redisTemplate.opsForValue().get(KEY_PREFIX + id), PostResponseDto.class);
     }
 }
