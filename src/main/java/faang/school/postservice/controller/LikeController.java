dca943b979b209109217ca98229ba3a2f0efdc09
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-05-26 23:35:49 +0300

    refactor

--- src/main/java/faang/school/postservice/controller/LikeController.java
+++ src/main/java/faang/school/postservice/controller/LikeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
