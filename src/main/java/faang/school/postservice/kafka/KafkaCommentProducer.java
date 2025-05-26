8bd5249d5e9d3e8bdaa4e499daa951160f0f9040
Kot-Mars <stemcraft65@gmail.com>
2025-05-26 17:38:41 +0300

    BJS2-66673: Перенес Kafka Перенес kafka из конфига в отдельную папку

--- src/main/java/faang/school/postservice/kafka/KafkaCommentProducer.java
+++ src/main/java/faang/school/postservice/kafka/KafkaCommentProducer.java
@@ -1,6 +1,7 @@
-package faang.school.postservice.config.kafka;
+package faang.school.postservice.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.postservice.config.kafka.AbstractKafkaProducer;
 import faang.school.postservice.dto.comment.CommentEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
