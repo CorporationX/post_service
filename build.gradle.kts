c6e16b6298aeb792d5705e31be47be8a73138f6e
balahanton <balahanton@gmail.com>
2025-05-26 10:08:13 +0300

    1

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
      * Database
      */
     implementation("org.liquibase:liquibase-core")
+    implementation("org.springframework.kafka:spring-kafka:4.0.0-M2")
     implementation("redis.clients:jedis:4.3.2")
     runtimeOnly("org.postgresql:postgresql")
 
@@ -36,6 +37,7 @@ dependencies {
      * Utils & Logging
      */
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("org.redisson:redisson:3.47.0")
     implementation("org.slf4j:slf4j-api:2.0.5")
     implementation("ch.qos.logback:logback-classic:1.4.6")
     implementation("org.projectlombok:lombok:1.18.26")
