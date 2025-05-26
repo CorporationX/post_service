bb1de3a6d98192e644514c50f577a0e0ed92498b
Danila Bogatyrev <d.l.bogatyrev@gmail.com>
2025-05-26 21:55:10 +0300

    add error handler feign

--- src/main/java/faang/school/postservice/client/FeignConfig.java
+++ src/main/java/faang/school/postservice/client/FeignConfig.java
@@ -1,13 +1,24 @@
 package faang.school.postservice.client;
 
+import faang.school.postservice.client.handler.FeignErrorHandler;
 import faang.school.postservice.config.context.UserContext;
+import faang.school.postservice.mapper.ErrorResponseMapper;
+import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class FeignConfig {
     @Bean
     public FeignUserInterceptor feignUserInterceptor(UserContext userContext) {
         return new FeignUserInterceptor(userContext);
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder(List<FeignErrorHandler> handlers,
+                                     ErrorResponseMapper errorResponseMapper) {
+        return new FeignClientErrorDecoder(handlers, errorResponseMapper);
     }
 }
