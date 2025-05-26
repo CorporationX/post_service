ffd79659f0a2cdbb2c7ed615fc79ea309f52e9ca
Artem Doronin <doronintema2018@yandex.ru>
2025-05-26 17:35:54 +0500

    commit 2

--- src/main/java/faang/school/postservice/publisher/KafkaLikeProducer.java
+++ src/main/java/faang/school/postservice/publisher/KafkaLikeProducer.java
@@ -1,14 +1,17 @@
 package faang.school.postservice.publisher;
 
 import faang.school.postservice.dto.event.LikeEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class KafkaLikeProducer implements KafkaEventPublisher<LikeEvent> {
 
     private KafkaTemplate<String,LikeEvent> kafkaTemplate;
+
     @Value("${spring.data.kafka.topic.likes.name}")
     private String topic;
 
