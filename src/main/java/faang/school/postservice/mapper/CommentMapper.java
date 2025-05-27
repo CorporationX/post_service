fa4189b8160c2fe8f04d4e7679f8b0d14f772d83
Tanya <hummingbird_t@mail.ru>
2025-05-27 11:07:26 +0300

    Comment commit

---
+++ src/main/java/faang/school/postservice/mapper/CommentMapper.java
@@ -0,0 +1,25 @@
+package faang.school.postservice.mapper;
+
+import faang.school.postservice.dto.comment.CommentForCreationDto;
+import faang.school.postservice.dto.comment.CommentForUpdateDto;
+import faang.school.postservice.dto.comment.CommentOutputDto;
+import faang.school.postservice.model.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CommentMapper {
+
+    Comment toEntity (CommentForCreationDto dto);
+
+    @Mapping(target = "postId", source = "post.id")
+    CommentOutputDto toDto (Comment comment);
+
+    List<CommentOutputDto> toListDto (List<Comment> comments);
+
+    Comment updateEntityFromDto(CommentForUpdateDto dto, @MappingTarget Comment entity);
+}
---
+++ src/main/java/faang/school/postservice/service/CommentService.java
@@ -0,0 +1,20 @@
+package faang.school.postservice.service;
+
+import faang.school.postservice.dto.comment.CommentForCreationDto;
+import faang.school.postservice.dto.comment.CommentForUpdateDto;
+import faang.school.postservice.dto.comment.CommentOutputDto;
+
+import java.util.List;
+
+public interface CommentService {
+
+    public CommentOutputDto create(CommentForCreationDto commentDto);
+
+    public CommentOutputDto update(CommentForUpdateDto commentDto);
+
+    public CommentOutputDto findById(long commentId);
+
+    public void deleteById(long commentId);
+
+    List<CommentOutputDto> findByPostId(long postId);
+}
