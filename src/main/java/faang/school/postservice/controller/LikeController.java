48d49524ed53dd59aab18842415420e99e77324a
Artem Doronin <doronintema2018@yandex.ru>
2025-05-28 22:36:40 +0500

    commit 1

--- src/main/java/faang/school/postservice/controller/LikeController.java
+++ src/main/java/faang/school/postservice/controller/LikeController.java
@@ -3,6 +3,7 @@ package faang.school.postservice.controller;
 import faang.school.postservice.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,10 @@ public class LikeController {
     @DeleteMapping("/comments/{commentId}")
     public void removeLikeAtComment(@PathVariable Long commentId) {
         likeService.removeLikeAtComment(commentId);
+    }
+
+    @GetMapping
+    public void listLikes() {
+        likeService.like();
     }
 }
