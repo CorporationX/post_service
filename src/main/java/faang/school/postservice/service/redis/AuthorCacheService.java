154a2abd6bb99e65b6d86c51372e70cc555d6583
Kot-Mars <stemcraft65@gmail.com>
2025-05-29 17:46:34 +0300

    BJS2-66669: fixCode

--- src/main/java/faang/school/postservice/service/redis/AuthorCacheService.java
+++ src/main/java/faang/school/postservice/service/redis/AuthorCacheService.java
@@ -1,21 +1,38 @@
 package faang.school.postservice.service.redis;
 
+import faang.school.postservice.exception.RedisOperationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
+import static faang.school.postservice.contants.ErrorMessage.CACHE_AUTHOR_ERROR;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthorCacheService {
-    private static final String AUTHOR_CACHE_KEY = "newsfeed:authors";
-
     private final RedisTemplate<String, String> redisTemplate;
 
+    @Value("${spring.data.redis.key-prefix.author-cache}")
+    private String authorCacheKey;
+
     public void cacheAuthor(String userId, Duration ttl) {
-        redisTemplate.opsForSet().add(AUTHOR_CACHE_KEY, userId);
-        redisTemplate.expire(AUTHOR_CACHE_KEY, ttl);
+        String key = authorCacheKey + ":" + userId;
+        try {
+            Boolean added = redisTemplate.opsForValue().setIfAbsent(key, "cached", ttl);
+            if (Boolean.TRUE.equals(added)) {
+                log.info("Author {} cached with TTL {}", userId, ttl);
+            } else {
+                log.info("Author {} already cached", userId);
+            }
+        } catch (RuntimeException e) {
+            log.error("{}: userId={}, reason={}", CACHE_AUTHOR_ERROR, userId, e.getMessage(), e);
+            throw new RedisOperationException(CACHE_AUTHOR_ERROR);
+        }
     }
 
 }
