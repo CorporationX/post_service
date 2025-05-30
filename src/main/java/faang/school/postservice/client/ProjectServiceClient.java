6f2bd65b4927369d6bfe48a1b6411e4d7a0639ea
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-05-30 18:16:57 +0300

    BJS2-78244 refactor

--- src/main/java/faang/school/postservice/client/ProjectServiceClient.java
+++ src/main/java/faang/school/postservice/client/ProjectServiceClient.java
@@ -1,7 +1,6 @@
 package faang.school.postservice.client;
 
 import faang.school.postservice.dto.project.ProjectClientResponseDto;
-import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +14,8 @@ import java.util.List;
         configuration = FeignConfig.class)
 public interface ProjectServiceClient {
     @GetMapping("/{projectId}")
-    @Valid ProjectClientResponseDto getProject(@PathVariable long projectId);
+    ProjectClientResponseDto getProject(@PathVariable long projectId);
 
     @GetMapping()
-    @Valid List<ProjectClientResponseDto> getProjectsByIds(@RequestParam List<Long> ids);
+    List<ProjectClientResponseDto> getProjectsByIds(@RequestParam List<Long> ids);
 }
