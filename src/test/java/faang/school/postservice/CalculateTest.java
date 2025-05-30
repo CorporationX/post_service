f7c8b147b6980dad48c3abef21d6241a219a91a0
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-05-30 07:31:48 +0300

    test6 checkstyle++

--- src/test/java/faang/school/postservice/CalculateTest.java
+++ src/test/java/faang/school/postservice/CalculateTest.java
@@ -6,9 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculateTest {
     Calculate calculate = new Calculate();
+
     @Test
-    void addTest(){
-        assertEquals(6,calculate.add(2,3));
+    void addTest() {
+        assertEquals(5, calculate.add(2, 3));
     }
 
 }
