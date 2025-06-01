93d7892e9e158db3c5b5b09e8ff5cf53ee7597b8
Marsel <mars1212@inbox.ru>
2025-06-01 20:10:46 +0300

    BJS2-66684-kafka-listener

--- src/main/java/faang/school/postservice/consumer/KafkaLikeConsumer.java
+++ src/main/java/faang/school/postservice/consumer/KafkaLikeConsumer.java
@@ -6,10 +6,13 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class KafkaLikeConsumer {
+    private static final Long INCREMENT_VALUE = 1L;
     private final RedisTemplate<String, String> redisTemplate;
 
     @KafkaListener(topics = "${spring.kafka.topic.like.name}")
@@ -17,20 +20,20 @@ public class KafkaLikeConsumer {
         String postKey = "post:" + postId;
         String likesKey = "likes:" + postId;
 
-        try {
-            if (Boolean.TRUE.equals(redisTemplate.hasKey(postKey))) {
-                boolean isNewLike = redisTemplate.opsForSet().add(likesKey, String.valueOf(userId)) == 1L;
+        if (Boolean.FALSE.equals(redisTemplate.hasKey(postKey))) {
+            log.warn("Post {} not found in Redis, skipping like from user {}", postId, userId);
+            return;
+        }
 
-                if (isNewLike) {
-                    redisTemplate.opsForHash().increment(postKey, "likeCount", 1L);
-                    log.info("New like added for post {} by user {}", postId, userId);
-                }
-                else {
-                    log.info("Like already exists for post {} by user {}", postId, userId);
-                }
-            }
-            else {
-                log.warn("Post {} not found in Redis, skipping like from user {}", postId, userId);
+        try {
+            boolean isNewLike = Objects.equals(redisTemplate.opsForSet()
+                    .add(likesKey, String.valueOf(userId)), INCREMENT_VALUE);
+
+            if (isNewLike) {
+                redisTemplate.opsForHash().increment(postKey, "likeCount", INCREMENT_VALUE);
+                log.info("New like added for post {} by user {}", postId, userId);
+            } else {
+                log.info("Like already exists for post {} by user {}", postId, userId);
             }
         } catch (Exception e) {
             log.error("Error processing like.", e);
