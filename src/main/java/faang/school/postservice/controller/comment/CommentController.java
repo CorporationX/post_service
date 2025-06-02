f395adff8c93f416d0e2d8f1e67dfb4a61b388ad
Fagan Gafulov <gafulov@yandex.ru>
2025-06-02 17:01:37 +0300

    BJS2-78038 вернул Dto, поправил контроллер

--- src/main/java/faang/school/postservice/controller/comment/CommentController.java
+++ src/main/java/faang/school/postservice/controller/comment/CommentController.java
@@ -1,7 +1,10 @@
 package faang.school.postservice.controller.comment;
 
+import faang.school.postservice.dto.comment.CommentCreateDto;
 import faang.school.postservice.dto.comment.CommentDtoResponse;
+import faang.school.postservice.dto.comment.CommentUpdateDto;
 import faang.school.postservice.service.comment.CommentServiceFacade;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,8 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,15 +25,19 @@ public class CommentController {
     private final CommentServiceFacade commentServiceF;
 
     @PostMapping("/post/{postId}")
-    public ResponseEntity<CommentDtoResponse> createComment(@PathVariable long postId,
-                                                            @RequestParam String content) {
+    public ResponseEntity<CommentDtoResponse> createComment(@Valid @RequestBody CommentCreateDto commentDto) {
+        long postId = commentDto.getPostId();
+        String content = commentDto.getContent();
+
         CommentDtoResponse commentDtoResponse = commentServiceF.createComment(postId, content);
         return ResponseEntity.ok(commentDtoResponse);
     }
 
     @PatchMapping("/{commentId}")
-    public ResponseEntity<CommentDtoResponse> updateComment(@PathVariable long commentId,
-                                                            @RequestParam String newContent) {
+    public ResponseEntity<CommentDtoResponse> updateComment(@Valid @RequestBody CommentUpdateDto commentDto) {
+        long commentId = commentDto.getCommentId();
+        String newContent = commentDto.getNewContent();
+
         CommentDtoResponse commentDtoResponse = commentServiceF.updateComment(commentId, newContent);
         return ResponseEntity.ok(commentDtoResponse);
     }
