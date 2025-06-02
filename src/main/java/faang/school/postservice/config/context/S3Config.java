79084fc039cd7322c3210d2d6ac489960b7f104a
Fagan Gafulov <gafulov@yandex.ru>
2025-06-02 16:44:36 +0300

    BJS2-78063 настройка конфига S3Client

---
+++ src/main/java/faang/school/postservice/config/context/S3Config.java
@@ -0,0 +1,47 @@
+package faang.school.postservice.config.context;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Slf4j
+@Configuration
+public class S3Config {
+
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Value("${minio.region}")
+    private String region;
+
+    @Value("${minio.path-style-access}")
+    private boolean pathStyleAccess;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(region))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(pathStyleAccess)
+                        .build())
+                .build();
+    }
+
+}
