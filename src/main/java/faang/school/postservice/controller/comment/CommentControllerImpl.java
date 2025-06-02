389f9f0e03b51d2ca69f35ef2b536f64f7311612
Fagan Gafulov <gafulov@yandex.ru>
2025-06-02 03:56:20 +0300

    BJS2-78042 решение конфликтов

--- src/main/java/faang/school/postservice/controller/comment/CommentControllerImpl.java
+++ src/main/java/faang/school/postservice/controller/comment/CommentControllerImpl.java
@@ -1,14 +1,12 @@
 package faang.school.postservice.controller.comment;
 
-import faang.school.postservice.dto.comment.CommentCreateDto;
 import faang.school.postservice.dto.comment.CommentDtoResponse;
-import faang.school.postservice.dto.comment.CommentUpdateDto;
 import faang.school.postservice.service.comment.CommentServiceFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,14 +18,14 @@ public class CommentControllerImpl implements CommentController {
     private final CommentServiceFacade commentServiceF;
 
     @Override
-    public ResponseEntity<CommentDtoResponse> createComment(@RequestBody CommentCreateDto commentDto) {
-        CommentDtoResponse commentDtoResponse = commentServiceF.createComment(commentDto);
+    public ResponseEntity<CommentDtoResponse> createComment(@PathVariable long postId, @RequestParam String content) {
+        CommentDtoResponse commentDtoResponse = commentServiceF.createComment(postId, content);
         return ResponseEntity.ok(commentDtoResponse);
     }
 
     @Override
-    public ResponseEntity<CommentDtoResponse> updateComment(@RequestBody CommentUpdateDto commentDto) {
-        CommentDtoResponse commentDtoResponse = commentServiceF.updateComment(commentDto);
+    public ResponseEntity<CommentDtoResponse> updateComment(@PathVariable long commentId, @RequestParam String newContent) {
+        CommentDtoResponse commentDtoResponse = commentServiceF.updateComment(commentId, newContent);
         return ResponseEntity.ok(commentDtoResponse);
     }
 
