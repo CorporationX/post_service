c20eec23ec8c89dd2c6b5f465e1600bdda18a653
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-06-03 10:15:18 +0300

    BJS2-77197 правка замечаний

--- src/main/java/faang/school/postservice/config/CommonConfig.java
+++ src/main/java/faang/school/postservice/config/CommonConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PostServiceConfig {
+public class CommonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
