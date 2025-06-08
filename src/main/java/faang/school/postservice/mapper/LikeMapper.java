61b46a29696229cbb995d4a5542333df7065f101
Danila Bogatyrev <d.l.bogatyrev@gmail.com>
2025-06-08 18:15:49 +0300

    BJS2-77829: failed build

--- src/main/java/faang/school/postservice/mapper/LikeMapper.java
+++ src/main/java/faang/school/postservice/mapper/LikeMapper.java
@@ -19,7 +19,7 @@ public interface LikeMapper {
 
     @Mapping(target = "commentId", source = "comment.id") 
     @Mapping(target = "postId", source = "post.id")
-    LikeDto toDto(Like like);
+    LikeDto toDto(Lik1e like);
 
     @Mapping(target = "commentId", source = "comment.id") 
     @Mapping(target = "postId", source = "post.id")
