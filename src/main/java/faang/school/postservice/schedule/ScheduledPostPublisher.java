e7f03dbb1aaa996a605566852bd727cfc410bb8c
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-06-08 15:59:34 +0200

    Ci pipeLine

--- src/main/java/faang/school/postservice/schedule/ScheduledPostPublisher.java
+++ src/main/java/faang/school/postservice/schedule/ScheduledPostPublisher.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.schedule;
 
-import faang.school.postservice.service.PostService;
+
+import faang.school.postservice.service.post.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
