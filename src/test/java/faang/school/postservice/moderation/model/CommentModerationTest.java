52fc282890e8ce9b8d59cfcc2677960eeba20ea8
Keriod <nikolainovopashin98@gmail.com>
2025-06-11 20:51:54 +0500

    feature/BJS2-78094. Проверка комментариев на оскорбительный контент -Проверка коменнтариев на оскорбительный контет и удаление соответствующих комментариев -Тесты на CommentModeration

--- src/test/java/faang/school/postservice/moderation/model/CommentModerationTest.java
+++ src/test/java/faang/school/postservice/moderation/model/CommentModerationTest.java
@@ -16,8 +16,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
