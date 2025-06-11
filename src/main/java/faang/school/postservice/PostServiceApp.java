2373122b1258a8f070d4e02cb2e9b5bcb7c90135
Tanya <hummingbird_t@mail.ru>
2025-06-11 09:40:04 +0300

    after rebasing

--- src/main/java/faang/school/postservice/PostServiceApp.java
+++ src/main/java/faang/school/postservice/PostServiceApp.java
@@ -13,7 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableRetry
 @EnableFeignClients(basePackages = "faang.school.postservice.client")
-@EnableRetry
 public class PostServiceApp {
     public static void main(String[] args) {
         new SpringApplicationBuilder(PostServiceApp.class)
