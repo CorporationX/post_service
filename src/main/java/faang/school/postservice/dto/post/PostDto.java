ddf90f19d398f15ae296f8dd15b230fd0f69bb4a
SAM <SULEYMANOV.USA@GMAIL.COM>
2025-06-11 09:36:10 -0700

    Улучшил метод пакетной обработки постов для большей читаемости кода.

--- src/main/java/faang/school/postservice/dto/post/PostDto.java
+++ src/main/java/faang/school/postservice/dto/post/PostDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 public record PostDto(
         Long id,
         @NotBlank(message = "Содержимое поста не может быть пустым")
