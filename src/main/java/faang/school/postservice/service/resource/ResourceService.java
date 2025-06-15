f0b5d157e9d927c08be15d26988a66be7ed822bd
Ramil Burgart <ramilburgart7006@gmail.com>
2025-06-15 18:08:51 +0300

    added logic for adding a picture to a post
    the logic written for S3Client(generateKey, delete, download Image), ResourceService(upload, delete, download),
    ResourceController(PostMapping, DeleteMapping, GetMapping). added tests for ResourceService.
    
    ValidationResource added image size compression

--- src/main/java/faang/school/postservice/service/resource/ResourceService.java
+++ src/main/java/faang/school/postservice/service/resource/ResourceService.java
@@ -1,5 +1,6 @@
 package faang.school.postservice.service.resource;
 
+import faang.school.postservice.dto.s3.S3Dto;
 import faang.school.postservice.model.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,4 +8,6 @@ public interface ResourceService {
     Resource addBuildForPost(long postId, MultipartFile file);
 
     void deleteImageByPostId(long postId, long resourceId);
+
+    public S3Dto downloadImage(long postId, long resourceId);
 }
--- src/main/java/faang/school/postservice/service/s3/S3Service.java
+++ src/main/java/faang/school/postservice/service/s3/S3Service.java
@@ -1,16 +1,25 @@
 package faang.school.postservice.service.s3;
 
-import faang.school.postservice.validation.ValidationResource;
+import faang.school.postservice.dto.s3.S3Dto;
+import faang.school.postservice.validation.resource.ValidationResource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
@@ -21,6 +30,7 @@ import java.util.Objects;
 @Slf4j
 public class S3Service {
     private final S3Client s3Client;
+    private final ValidationResource validationResource;
     @Value("${services.s3.bucket}")
     private String bucketName;
 
@@ -28,15 +38,17 @@ public class S3Service {
         String key = String.format("%s%s", System.currentTimeMillis(), image.getOriginalFilename());
 
         try (InputStream inputStream = image.getInputStream()) {
-            ValidationResource.correctedBuild(image);
+            BufferedImage bufferedImage = validationResource.correctedBuild(image);
+            MultipartFile multipartFile = validationResource.bufferedImageToMultipartFile(bufferedImage,
+                    "jpeg", image.getOriginalFilename());
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucketName)
                     .key(key)
-                    .metadata(Map.of("filename", Objects.requireNonNull(image.getOriginalFilename())))
-                    .contentLength(image.getSize())
-                    .contentType(image.getContentType())
+                    .metadata(Map.of("filename", Objects.requireNonNull(multipartFile.getOriginalFilename())))
+                    .contentLength(multipartFile.getSize())
+                    .contentType(multipartFile.getContentType())
                     .build();
-            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, image.getSize()));
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, multipartFile.getSize()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -50,4 +62,23 @@ public class S3Service {
                 .build());
     }
 
+    public S3Dto downloadImage(String key) {
+        ResponseInputStream<GetObjectResponse> responseObject = s3Client.getObject(
+                GetObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .build());
+        Resource streamResource = new InputStreamResource(responseObject);
+        HeadObjectResponse metadata = s3Client.headObject(HeadObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build());
+
+        return S3Dto.builder()
+                .name(metadata.metadata().get("filename"))
+                .contentType(metadata.contentType())
+                .contentLength(metadata.contentLength())
+                .resource(streamResource)
+                .build();
+    }
 }
