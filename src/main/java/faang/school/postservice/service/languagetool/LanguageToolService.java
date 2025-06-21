1f2b42251b1987c7ff222eac22d7dd0a19122b7d
Fagan Gafulov <gafulov@yandex.ru>
2025-06-21 18:25:34 +0300

    BJS2-78085 пофиксил код, добавил retry

--- src/main/java/faang/school/postservice/service/languagetool/LanguageToolService.java
+++ src/main/java/faang/school/postservice/service/languagetool/LanguageToolService.java
@@ -4,21 +4,24 @@ import faang.school.postservice.dto.languagetool.LanguageToolResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class LanguageToolService {
     private final RestTemplate restTemplate;
-    private final Executor customExecutor;
+    private final ThreadPoolTaskExecutor executor;
 
     @Value("${app.correction.url}")
     private String url;
@@ -26,6 +29,11 @@ public class LanguageToolService {
     @Value("${app.correction.language}")
     private String language;
 
+    @Retryable(
+            retryFor = {RestClientResponseException.class, RestClientException.class},
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     public CompletableFuture<LanguageToolResponse> checkText(String originalText) {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.add("text", originalText);
@@ -35,7 +43,7 @@ public class LanguageToolService {
         return CompletableFuture
                 .supplyAsync(
                         () -> restTemplate.postForObject(url, formData, LanguageToolResponse.class),
-                        customExecutor)
+                        executor)
                 .exceptionally(e -> {
                     log.error("External API request failed", e);
                     throw new RestClientException("Failed to check text with LanguageTool");
