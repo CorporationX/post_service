3df414283ddc0546bfe5ac52d0f08b093505494a
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-06-24 12:47:13 +0300

    BJS2-77048 Анализ лайков producer

---
+++ src/main/java/faang/school/postservice/config/KafkaProducerConfig.java
@@ -0,0 +1,60 @@
+package faang.school.postservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaProducerConfig {
+
+    @Value("${kafka.host}")
+    private String bootstrapAddress;
+
+    @Value("${kafka.topic-name}")
+    private String topicName;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configs.put(AdminClientConfig.RETRIES_CONFIG, 5);
+        configs.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);
+
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return new NewTopic(topicName, 2, (short) 1);
+    }
+}
---
+++ src/main/java/faang/school/postservice/dto/likesystem/LikeEventDto.java
@@ -0,0 +1,17 @@
+package faang.school.postservice.dto.likesystem;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LikeEventDto {
+        private long authorId;
+        private long receiverId;
+        private LocalDateTime receivedAt;
+}
+
