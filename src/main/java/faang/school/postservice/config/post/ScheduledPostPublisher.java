2bc043029be82d2f805e815b2e5b953ed1048ba9
Tamirlan Dexengazy <dextomi02@gmail.com>
2025-06-25 19:21:37 +0500

    Убрал ненужную аннотацию @Slf4j

--- src/main/java/faang/school/postservice/config/post/ScheduledPostPublisher.java
+++ src/main/java/faang/school/postservice/config/post/ScheduledPostPublisher.java
@@ -2,11 +2,9 @@ package faang.school.postservice.config.post;
 
 import faang.school.postservice.service.post.PostService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @RequiredArgsConstructor
 @Component
 public class ScheduledPostPublisher {
