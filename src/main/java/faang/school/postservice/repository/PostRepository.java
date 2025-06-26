6a91b5b3fc7c869619b88f622ace0b60dcf29e7a
Tamirlan Dexengazy <dextomi02@gmail.com>
2025-06-27 00:19:22 +0500

    Поменял запрос на нативный и в нем же сделал блокировку, убрав аннотацию

--- src/main/java/faang/school/postservice/repository/PostRepository.java
+++ src/main/java/faang/school/postservice/repository/PostRepository.java
@@ -1,9 +1,7 @@
 package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Post;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -20,8 +18,14 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("SELECT p FROM Post p LEFT JOIN FETCH p.likes WHERE p.authorId = :authorId")
     List<Post> findByAuthorIdWithLikes(long authorId);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT p FROM Post p WHERE p.published = false AND p.deleted = false AND p.scheduledAt <= CURRENT_TIMESTAMP")
+    @Query(nativeQuery = true, value = """
+            SELECT * FROM post p
+            WHERE p.published = FALSE
+            AND p.deleted = FALSE
+            AND p.scheduled_at <= CURRENT_TIMESTAMP
+            FOR UPDATE SKIP LOCKED
+            LIMIT 1000
+            """)
     List<Post> findReadyToPublish();
 
     @Query(nativeQuery = true, value = """
