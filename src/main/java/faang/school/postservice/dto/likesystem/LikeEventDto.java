beea1305bd019abc4ccee95485cdc9868153e3cb
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-06-26 20:33:35 +0300

    переименовал поле в LikeEventDto

--- src/main/java/faang/school/postservice/dto/likesystem/LikeEventDto.java
+++ src/main/java/faang/school/postservice/dto/likesystem/LikeEventDto.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LikeEventDto {
-    private long authorId;
+    private long actorId;
     private long receiverId;
     private LocalDateTime receivedAt;
 }
