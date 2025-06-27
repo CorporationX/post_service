7ad5fcb128bf2e9a2657cb386033b3957d1896a0
saroyangor <dr.saroyan98@gmail.com>
2025-06-27 13:02:36 +0300

    fix comments

--- src/main/java/faang/school/postservice/config/kafka/KafkaProducerConfig.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import faang.school.postservice.dto.comment.CommentSendEvent;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -17,11 +18,13 @@ import java.util.Map;
 @Configuration
 @RequiredArgsConstructor
 public class KafkaProducerConfig {
+    @Value("${spring.data.kafka.bootstrap-servers}")
+    private String URL;
 
     @Bean
     public ProducerFactory<String, CommentSendEvent> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, URL);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
