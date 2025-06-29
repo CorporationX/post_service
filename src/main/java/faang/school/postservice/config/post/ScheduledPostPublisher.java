425bf368258ed815b21539ece0e7e79a5cefba13
dextomi <dextomi02@gmail.com>
2025-06-29 21:20:48 +0500

    Пофиксил транзакции и Async

--- src/main/java/faang/school/postservice/config/post/ScheduledPostPublisher.java
+++ src/main/java/faang/school/postservice/config/post/ScheduledPostPublisher.java
@@ -2,6 +2,7 @@ package faang.school.postservice.config.post;
 
 import faang.school.postservice.service.post.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 public class ScheduledPostPublisher {
     private final PostService postService;
 
+    @Async("scheduledPostsExecutor")
     @Scheduled(cron = "${scheduled-post-publisher.comments.cron}")
     public void publishScheduledPosts() {
         postService.publishScheduledPosts();
--- src/main/java/faang/school/postservice/service/post/PostBatchPublisher.java
+++ src/main/java/faang/school/postservice/service/post/PostBatchPublisher.java
@@ -4,9 +4,7 @@ import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,8 +16,6 @@ public class PostBatchPublisher {
 
     private final PostRepository postRepository;
 
-    @Async("scheduledPostsExecutor")
-    @Transactional
     public void publishPosts(List<Post> posts) {
         LocalDateTime publishTime = LocalDateTime.now();
 
