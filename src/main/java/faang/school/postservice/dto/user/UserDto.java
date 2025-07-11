70fbfd15eaaedb458836735004afffd73c5b9b92
tanufa 25 <tanufa25@gmail.com>
2025-07-11 23:37:08 +0300

    BJS2-78152 Kafka Event for Comment Creation

--- src/main/java/faang/school/postservice/dto/user/UserDto.java
+++ src/main/java/faang/school/postservice/dto/user/UserDto.java
@@ -1,8 +1,23 @@
 package faang.school.postservice.dto.user;
 
-public record UserDto(
-    Long id,
-    String username,
-    String email
-) {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.Locale;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class UserDto {
+    private long id;
+    private String username;
+    private String email;
+    private String phone;
+    private PreferredContact preference;
+    private Locale locale;
+
+    public enum PreferredContact {
+        EMAIL, PHONE, TELEGRAM
+    }
 }
