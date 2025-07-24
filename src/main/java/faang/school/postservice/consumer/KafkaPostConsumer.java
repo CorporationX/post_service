b0a49bf1b177b3b781ec7c0ff754d5d8c4f6a29b
pirojok_yt <tamik34@mail.ru>
2025-07-24 21:11:46 +0500

    delete post

--- src/main/java/faang/school/postservice/consumer/KafkaPostConsumer.java
+++ src/main/java/faang/school/postservice/consumer/KafkaPostConsumer.java
@@ -1,9 +1,7 @@
 package faang.school.postservice.consumer;
 
 import faang.school.postservice.dto.post.PostEventDto;
-import faang.school.postservice.service.PostService;
 import faang.school.postservice.utils.Feed;
-import faang.school.postservice.utils.PostCache;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,14 +12,9 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class KafkaPostConsumer {
     private final Feed feed;
-    private final PostService service;
-    private final PostCache postCache;
 
     @KafkaListener(topics = "${kafka.topics.posts.create-post}", groupId = "${kafka.group}")
     public void consume(PostEventDto event) {
-        event.followeesIds().forEach(id -> {
-            feed.save(id, event.postId());
-            postCache.save(event);
-        });
+        event.followeesIds().forEach(id -> feed.save(id, event.postId()));
     }
 }
