14a6327ca1480917f819e3bbe2520ddbae027586
pirojok_yt <tamik34@mail.ru>
2025-07-24 15:56:36 +0500

    fixes

--- src/main/java/faang/school/postservice/config/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 import java.util.concurrent.ConcurrentLinkedDeque;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
