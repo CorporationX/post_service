5fec7ab88f323ec917d3e25f293c23224fb9f0ea
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-07-25 10:18:27 +0300

    Сохранение поста в Redis после публикации и Эндпоинт получения фида пользователя

--- src/test/java/faang/school/postservice/controller/PostControllerTest.java
+++ src/test/java/faang/school/postservice/controller/PostControllerTest.java
@@ -39,7 +39,7 @@ public class PostControllerTest {
     @InjectMocks
     private PostController postController;
 
-   /* @Test
+   /*@Test
     public void testCreateDraftPost() {
         PostRequestDto request = createRequest("Post", 2L, null);
         PostResponseDto postDto = createPostResponseDto(request);
