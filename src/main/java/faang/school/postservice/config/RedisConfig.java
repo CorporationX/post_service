a08349da69b78018b715c3df0f4a81e196bb604a
pirojok_yt <tamik34@mail.ru>
2025-07-28 16:07:01 +0500

    fixes

--- src/main/java/faang/school/postservice/config/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/RedisConfig.java
@@ -1,12 +1,10 @@
 package faang.school.postservice.config;
 
-import faang.school.postservice.model.Post;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 import java.util.concurrent.ConcurrentLinkedDeque;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
