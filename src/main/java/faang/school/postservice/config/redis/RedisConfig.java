aa36b2b009e896ad0a33fc55821569b58e283933
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-07-31 20:25:56 +0200

    BJS2-77624: Слушатель Kafka-эвентов для нужно еще доработать zset и конфиги и тесты

--- src/main/java/faang/school/postservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfig.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
