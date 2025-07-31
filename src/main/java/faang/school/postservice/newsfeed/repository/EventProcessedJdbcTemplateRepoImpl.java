04e79a00dc9863e4343e27dca58e3deebbdbed15
Aleksandr <alexandr.sidorkin@gmail.com>
2025-07-31 17:43:30 +0200

    85179 - old event ids scheduled clean up added

--- src/main/java/faang/school/postservice/newsfeed/repository/EventProcessedJdbcTemplateRepoImpl.java
+++ src/main/java/faang/school/postservice/newsfeed/repository/EventProcessedJdbcTemplateRepoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Repository
@@ -17,5 +18,12 @@ public class EventProcessedJdbcTemplateRepoImpl implements EventProcessedReposit
     public int registerEventId(UUID uuid) {
         String sql = "INSERT INTO processed_events_uuids (uuid) VALUES (uuid) ON CONFLICT DO NOTHING";
         return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+    @Override
+    @Transactional
+    public void cleanUpOldRecords(int days) {
+        LocalDateTime cutoff = LocalDateTime.now().minusDays(days);
+        jdbcTemplate.update("DELETE FROM processed_events_uuids WHERE created_at < ?", cutoff);
     }
 }
--- src/main/java/faang/school/postservice/newsfeed/repository/EventProcessedRepository.java
+++ src/main/java/faang/school/postservice/newsfeed/repository/EventProcessedRepository.java
@@ -4,4 +4,5 @@ import java.util.UUID;
 
 public interface EventProcessedRepository {
     int registerEventId(UUID uuid);
+    void cleanUpOldRecords(int days);
 }
