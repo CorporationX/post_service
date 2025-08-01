ec864125504839b92954c0f421454831c2daeaa9
Vladislav Kozlov <littlerock2877@mail.ru>
2025-08-01 23:47:45 +0300

    add: caching

---
+++ src/main/java/faang/school/postservice/config/properties/CacheConfigProperties.java
@@ -0,0 +1,15 @@
+package faang.school.postservice.config.properties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@Getter
+@ConfigurationProperties(prefix = "cache.config")
+@RequiredArgsConstructor
+public class CacheConfigProperties {
+    private final Duration defaultTtl;
+    private final Duration usersTtl;
+}
