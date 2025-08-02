dec7df3d0141dd3e6a4b099aa56e913b94148eba
Ramil Burgart <ramilburgart7006@gmail.com>
2025-08-02 09:42:45 +0300

    added annotation

--- src/main/java/faang/school/postservice/aspect/LikeCreationEventAspect.java
+++ src/main/java/faang/school/postservice/aspect/LikeCreationEventAspect.java
@@ -3,19 +3,16 @@ package faang.school.postservice.aspect;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.notification.LikeCreateNotificationEvent;
 import faang.school.postservice.dto.user.UserDto;
-import faang.school.postservice.model.Like;
 import faang.school.postservice.model.Post;
 import faang.school.postservice.publisher.LikeCreateEventPublisher;
-import faang.school.postservice.service.like.LikeService;
 import faang.school.postservice.service.post.PostService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 @Aspect
 public class LikeCreationEventAspect {
 
@@ -23,6 +20,8 @@ public class LikeCreationEventAspect {
     private final UserServiceClient userServiceClient;
     private final PostService postService;
 
+    @AfterReturning(value = "@annotation(faang.school.postservice.annotation.LikeCreationEventKafka)",
+            returning = "post")
     public void publishLikeCreationEvent(Post post) {
         UserDto userDto = userServiceClient.getUser(post.getAuthorId());
         Post postPublish = postService.getPostById(post.getId());
