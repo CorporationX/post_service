7713ce61ef82a202af229fc06c5aee2b378218dc
firma78 <rap215@icloud.com>
2025-08-03 23:28:46 +0300

    Лситнер like

--- src/main/java/faang/school/postservice/messaging/KafkaLikeConsumer.java
+++ src/main/java/faang/school/postservice/messaging/KafkaLikeConsumer.java
@@ -1,11 +1,10 @@
 package faang.school.postservice.messaging;
 
 import faang.school.postservice.mapper.JsonMapper;
-import faang.school.postservice.model.redis.CachedComment;
+import faang.school.postservice.model.redis.CachedLike;
 import faang.school.postservice.repository.redis.RedisPostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
@@ -14,19 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class KafkaCommentConsumer {
+public class KafkaLikeConsumer {
     private final JsonMapper mapper;
     private final RedisPostRepository redisPostRepository;
 
 
-    @KafkaListener(topics = "${spring.data.kafka.topic.comments}")
+    @KafkaListener(topics = "${spring.data.kafka.topic.likes}")
     @Transactional(transactionManager = "kafkaTransactionManager")
     public void listen(String json, Acknowledgment acknowledgment) {
         try {
-            CachedComment event = mapper.mapToObject(json, CachedComment.class);
-            redisPostRepository.addComment(event.getPostId(), event);
+            CachedLike event = mapper.mapToObject(json, CachedLike.class);
+            redisPostRepository.addLike(event.getPostId(), event);
             acknowledgment.acknowledge();
-            log.info("Comment with id: {} for post with id: {} was cached", event.getId(), event.getPostId());
+            log.info("Like with id: {} for post with id: {} was cached", event.getId(), event.getPostId());
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
