a9c41794be6b35dbf37be624aef7396f6791b7f9
v888.rus <v888.rus@gmail.com>
2025-08-09 20:12:13 +0000

    WIP(7). News Feed. Part 1. FULLY WORKINGgit add .!

--- src/main/java/faang/school/postservice/publisher/post/kafka/KafkaProducer.java
+++ src/main/java/faang/school/postservice/publisher/post/kafka/KafkaProducer.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class KafkaProducer {
-    private final KafkaTemplate<String, KafkaPostMessage> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendMessage(KafkaPostMessage kafkaPostMessage) {
         kafkaTemplate.send("post-create-event", kafkaPostMessage.getId().toString(), kafkaPostMessage);
--- src/main/java/faang/school/postservice/service/feed/FeedService.java
+++ src/main/java/faang/school/postservice/service/feed/FeedService.java
@@ -1,16 +1,24 @@
 package faang.school.postservice.service.feed;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import faang.school.postservice.client.UserServiceClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FeedService {
     private final UserServiceClient userServiceClient;
 
     public void heat() {
+        int totalSubscriptions = userServiceClient.getAllSubscriptions();
+        List<Integer> authors = userServiceClient.getAuthorsOrderdBySubscribers();
 
+        log.info("Total lines to process {}.", totalSubscriptions);
+        log.info("List of authors by subscribers {}.", authors);
     }
 }
