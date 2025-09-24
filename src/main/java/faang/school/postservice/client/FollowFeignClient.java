5cf65c1476e456a625408ad855a53a79faa1f8d5
ddddddasha <daria.zhelobkovich@gmail.com>
2025-09-24 14:52:59 +0300

    fix: FollowFeignClient

--- src/main/java/faang/school/postservice/client/FollowFeignClient.java
+++ src/main/java/faang/school/postservice/client/FollowFeignClient.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", contextId = "followFeignClient",  url = "${user-service.host}:${user-service.port}")
+@FeignClient(name = "user-service", contextId = "followFeignClient", url = "${user-service.host}:${user-service.port}")
 public interface FollowFeignClient {
+
     @GetMapping("/api/v1/follows/{authorId}/followers/ids")
     FollowersPage getFollowerIds(
             @PathVariable long authorId,
@@ -15,7 +16,8 @@ public interface FollowFeignClient {
             @RequestParam(defaultValue = "1000") int limit);
 
     @GetMapping("/api/v1/follows/{userId}/following/ids")
-    FollowersPage getFollowingIds(@PathVariable long userId,
-                           @RequestParam(required = false) String cursor,
-                           @RequestParam(defaultValue = "1000") int limit);
+    FollowersPage getFollowingIds(
+            @PathVariable long userId,
+            @RequestParam(required = false) String cursor,
+            @RequestParam(defaultValue = "1000") int limit);
 }
