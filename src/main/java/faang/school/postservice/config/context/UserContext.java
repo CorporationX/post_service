27b46972c068e7e96967a0511b254a8d13f3c1a4
Setched <kir32kir@gmail.com>
2025-09-27 22:55:13 +0300

    Рабочая версия

--- src/main/java/faang/school/postservice/config/context/UserContext.java
+++ src/main/java/faang/school/postservice/config/context/UserContext.java
@@ -12,13 +12,8 @@ public class UserContext {
     }
 
     public long getUserId() {
-        Long userId = userIdHolder.get();
-        //if (userId == null) {
-        //throw new IllegalArgumentException(
-        //"User ID is missing. Please make sure 'x-user-id' header is included in the request.");
-        //}
-        //return 1;
-        return 1;
+        Long userId = 1L;
+        return userId;
     }
 
     public void clear() {
