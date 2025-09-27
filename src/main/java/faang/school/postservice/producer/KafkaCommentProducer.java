2ee1066cefdddcc5eb1227386c431aebaa8c57f9
Setched <kir32kir@gmail.com>
2025-09-27 04:35:54 +0300

    News-feed-BJS2-84401 -поправил замечания

--- src/main/java/faang/school/postservice/producer/KafkaCommentProducer.java
+++ src/main/java/faang/school/postservice/producer/KafkaCommentProducer.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,12 +19,21 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KafkaCommentProducer {
 
+    private final RetryTemplate retryTemplate;
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @Value("${spring.kafka.topics.comments}")
     private String commentTopics;
 
-    public void sendComment(CommentCreatedEventAvro comment) {
-        kafkaTemplate.send(commentTopics, comment);
+    public void sendCommentWithRetry(CommentCreatedEventAvro comment) {
+        try {
+            retryTemplate.execute(context -> {
+                kafkaTemplate.send(commentTopics, comment);
+                return null;
+            });
+        } catch (Exception e) {
+            log.error("Комментарий не получилось отправить после нескольких попыток");
+        }
+
     }
 }
