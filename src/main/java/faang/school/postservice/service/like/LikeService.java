8b70765bc9ce3528233a1620a1e283bcc850e7c1
Lily <lilly.liho@gmail.com>
2025-10-17 17:17:51 +0300

    add like service

---
+++ src/main/java/faang/school/postservice/service/like/LikeService.java
@@ -0,0 +1,13 @@
+package faang.school.postservice.service.like;
+
+import faang.school.postservice.dto.like.LikeDto;
+
+public interface LikeService {
+    LikeDto likePost(long postId, long userId);
+
+    void unlikePost(long postId, long userId);
+
+    LikeDto likeComment(long commentId, long userId);
+
+    void unlikeComment(long commentId, long userId);
+}
