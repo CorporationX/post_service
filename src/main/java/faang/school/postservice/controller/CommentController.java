f9a69400bbf884285031be10fa91bc8ac63fddf1
Elya Khamzina <ekkhamzi@mail.ru>
2025-10-20 15:39:04 +0300

    validated убрала

--- src/main/java/faang/school/postservice/controller/CommentController.java
+++ src/main/java/faang/school/postservice/controller/CommentController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +24,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts/{postId}/comments")
-@Validated
 public class CommentController {
 
     private final CommentService commentService;
