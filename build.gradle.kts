dfae0606e705570567e8bbe7cf2ce06bcff368eb
Lily <lilly.liho@gmail.com>
2025-10-20 16:24:28 +0300

    add swagger to post service

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,11 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     /**
+     * Documentation
+     */
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+    /**
      * Database
      */
     implementation("org.liquibase:liquibase-core")
