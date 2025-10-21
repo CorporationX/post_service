8ebf6b0281316bb40f677c2ca498275c1122d4ba
EdelweissPC <shherbakov99ilya@mail.ru>
2025-10-21 17:42:52 +0300

    BJS2-96077 add pipeline

--- src/main/java/faang/school/postservice/PostServiceApp.java
+++ src/main/java/faang/school/postservice/PostServiceApp.java
@@ -14,5 +14,7 @@ public class PostServiceApp {
         new SpringApplicationBuilder(PostServiceApp.class)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
+        if (4 == 5)
+            System.out.println(4);
     }
 }
