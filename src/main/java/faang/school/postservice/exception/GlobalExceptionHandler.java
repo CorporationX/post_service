8c5edb4a74c170c73619794d8ceedb98b2aa2c76
AndreySN7 <andrey__n@mail.ru>
2025-10-24 19:05:30 +0300

    Правки по кросс-ревью

--- src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/exception/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -16,41 +15,41 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleResourceNotFound(EntityNotFoundException e) {
         log.error("Resource not found", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(DataValidationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleResourceBadRequest(DataValidationException e) {
         log.error("Incorrect data has been entered", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<String> handleResourceBadRequest(ForbiddenException e) {
         log.error("Forbidden action", e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.error("Validation failed", ex);
+        Map<String, String> errors = extractValidationErrors(ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    private Map<String, String> extractValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> {
             String fieldName = error.getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        log.error("Validation failed", ex);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        return errors;
     }
 
     @ExceptionHandler(FeignException.NotFound.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleFeignNotFoundException(FeignException.NotFound e) {
         log.error("Remote resource not found", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
@@ -58,7 +57,6 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(FeignException.class)
-    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public ResponseEntity<String> handleFeignException(FeignException e) {
         log.error("Service unavailable", e);
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
@@ -66,7 +64,6 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(FeignException.ServiceUnavailable.class)
-    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public ResponseEntity<String> handleFeignServiceUnavailable(FeignException.ServiceUnavailable e) {
         log.error("Service is currently unavailable", e);
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
