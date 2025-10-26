f1e3f3f828526885e4f041192363a492af9d9dcf
Elya Khamzina <ekkhamzi@mail.ru>
2025-10-27 00:17:29 +0300

    PR

--- src/main/java/faang/school/postservice/service/image/S3ClientConfig.java
+++ src/main/java/faang/school/postservice/service/image/S3ClientConfig.java
@@ -12,7 +12,7 @@ import software.amazon.awssdk.services.s3.S3Configuration;
 import java.net.URI;
 
 @Configuration
-public class S3ClientConfig { //для PR
+public class S3ClientConfig {
 
     @Value("${services.s3.endpoint}")
     private String endpoint;
