34cbcc638c91569849c5413056858856218f8467
Mikhail Easy <mghievrie@bk.ru>
2025-11-02 16:39:25 +0500

    BJS2-96755 refactor problem

--- src/main/java/faang/school/postservice/model/ad/Ad.java
+++ src/main/java/faang/school/postservice/model/ad/Ad.java
@@ -11,14 +11,18 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 @Table(name = "post_ad")
