4f5b916e7c4424dde8bcf4c1576ff96e05ce7fed
Sofiia Сhebotareva <Sofiia.chebotareva@gmail.com>
2025-11-03 16:53:25 +0100

    fix after review

--- src/main/java/faang/school/postservice/client/WebClientConfig.java
+++ src/main/java/faang/school/postservice/client/WebClientConfig.java
@@ -1,16 +1,19 @@
 package faang.school.postservice.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+    @Value("${textgears.base-url}")
+    private String baseUrl;
 
     @Bean
     public WebClient textGearsWebClient() {
         return WebClient.builder()
-                .baseUrl("https://api.textgears.com")
+                .baseUrl(baseUrl)
                 .build();
     }
 }
