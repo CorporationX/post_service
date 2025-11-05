d07c8de726cc4480042dde71cb9a7006d8167b8c
derzhko <aderzhko@radiofid.ru>
2025-11-05 16:34:39 +0300

    initial commit

--- src/main/java/faang/school/postservice/dto/user/UserDto.java
+++ src/main/java/faang/school/postservice/dto/user/UserDto.java
@@ -1,8 +1,14 @@
 package faang.school.postservice.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record UserDto(
+    @NotNull(message = "id should not be null")
     Long id,
+    @NotBlank(message = "username should not be blank")
     String username,
+    @NotBlank(message = "email should not be blank")
     String email
 ) {
 }
