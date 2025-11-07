adc63be66d22234c4039b1b2d6e2ee20545f3857
Scherbi <shherbakov99ilya@mail.ru>
2025-11-07 22:26:44 +0300

    BJS2-96120 add config for banWords

---
+++ src/main/java/faang/school/postservice/config/moderator/ModerationDictionary.java
@@ -0,0 +1,50 @@
+package faang.school.postservice.config.moderator;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class ModerationDictionary {
+
+    private static final String KEY_FOR_BAN_WORDS = "ban_words";
+
+    private List<String> banWords;
+
+    @PostConstruct
+    public void loadBanWords() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ClassPathResource resource = new ClassPathResource("ban-word.json");
+        try {
+            Map<String, List<String>> data = objectMapper.readValue(resource.getInputStream(),
+                    new TypeReference<>() {
+                    });
+
+            List<String> words = data.get(KEY_FOR_BAN_WORDS);
+            this.banWords = doToLowerCase(words);
+        } catch (Exception e) {
+            log.error("Reading from file ban_word.json failed with an error.", e);
+            this.banWords = new ArrayList<>();
+            this.banWords.add("блять");
+        }
+    }
+
+    public List<String> getBanWords() {
+        return banWords;
+    }
+
+    public List<String> doToLowerCase(List<String> list) {
+        return list.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+    }
+}
