bd580fe60570bfceaa13a03b037f3f2c847e51e1
K0I2T <hiro94337@gmail.com>
2025-11-12 10:51:28 +0300

    BJS2-95867 post system without unit-tests

---
+++ src/main/java/faang/school/postservice/controller/PostController.java
@@ -0,0 +1,124 @@
+package faang.school.postservice.controller;
+
+import faang.school.postservice.config.context.UserContext;
+import faang.school.postservice.dto.post.PostDto;
+import faang.school.postservice.service.PostService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/v1/posts")
+public class PostController {
+    private final PostService postService;
+    private final UserContext userContext;
+
+    @PostMapping("/creating")
+    public void createPost(@RequestBody @Valid PostDto postDto) {
+        validationCreatorId(postDto);
+        postService.createPost(postDto);
+    }
+
+    @PutMapping("/publishing/{postId}")
+    public void publishPost(@PathVariable long postId) {
+        postService.publishPost(postId);
+    }
+
+    @PutMapping("/updating")
+    public void updatePost(@RequestBody @Valid PostDto postDto) {
+        postService.updatePost(postDto);
+    }
+
+    @PutMapping("/deleted/{postId}")
+    public void deletePost(@PathVariable long postId) {
+        postService.deletePost(postId);
+    }
+
+    @GetMapping("/{postId}")
+    public PostDto getPost(@PathVariable long postId) {
+        return postService.getPost(postId);
+    }
+
+    @GetMapping("/drafts/author")
+    public List<PostDto> getPostDraftsByAuthorId() {
+        validationAuthorId();
+        return postService.getPostDraftsByAuthorId(userContext.getUserId());
+    }
+
+    @GetMapping("/drafts/project")
+    public List<PostDto> getPostDraftsByProjectId() {
+        validationProjectId();
+        return postService.getPostDraftsByProjectId(userContext.getProjectId());
+    }
+
+    @GetMapping("/published/author")
+    public List<PostDto> getPostPublishedByAuthorId() {
+        validationAuthorId();
+        return postService.getPostPublishedByAuthorId(userContext.getUserId());
+    }
+
+    @GetMapping("/published/project")
+    public List<PostDto> getPostPublishedByProjectId() {
+        validationProjectId();
+        return postService.getPostPublishedByProjectId(userContext.getProjectId());
+    }
+
+    private void validationCreatorId(PostDto post) {
+        Long xUserId = userContext.getUserId();
+        Long xProjectId = userContext.getProjectId();
+
+        if (xUserId == null && xProjectId == null) {
+            throw new NullPointerException("ID of the author of the post should not be null");
+        }
+        if (xUserId != null && xProjectId != null) {
+            throw new IllegalArgumentException("Сan be only one author");
+        }
+
+        if (xUserId != null) {
+            post.setAuthorId(xUserId);
+        } else {
+            post.setProjectId(xProjectId);
+        }
+    }
+
+    private void validationAuthorId() {
+        Long xUserId = userContext.getUserId();
+        Long xProjectId = userContext.getProjectId();
+
+        if (xUserId == null && xProjectId == null) {
+            throw new NullPointerException("ID of the author of the post should not be null");
+        }
+        if (xUserId != null && xProjectId != null) {
+            throw new IllegalArgumentException("Сan be only one author");
+        }
+        if (xProjectId != null) {
+            throw new IllegalArgumentException("Viewing is prohibited");
+        }
+    }
+
+    private void validationProjectId() {
+        Long xUserId = userContext.getUserId();
+        Long xProjectId = userContext.getProjectId();
+
+        if (xUserId == null && xProjectId == null) {
+            throw new NullPointerException("ID of the author of the post should not be null");
+        }
+        if (xUserId != null && xProjectId != null) {
+            throw new IllegalArgumentException("Сan be only one author");
+        }
+        if (xUserId != null) {
+            throw new IllegalArgumentException("Viewing is prohibited");
+        }
+    }
+}
--- src/main/java/faang/school/postservice/repository/PostRepository.java
+++ src/main/java/faang/school/postservice/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Post;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
@@ -22,4 +24,11 @@ public interface PostRepository extends CrudRepository<Post, Long> {
             "= false AND p.scheduledAt <= CURRENT_TIMESTAMP")
     List<Post> findReadyToPublish();
 
+    @Modifying
+    @Query("UPDATE Post p SET p.published = :published, p.publishedAt = :publishedAt WHERE p.id = :postId")
+    void updateIsPublished(long postId, boolean published, LocalDateTime publishedAt);
+
+    @Modifying
+    @Query("UPDATE Post p SET p.deleted = :deleted WHERE p.id = :postId")
+    void updateIsDeleted(long postId, boolean deleted);
 }
