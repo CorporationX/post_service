0a363a51e9e636b0ba6f720dda57dab242df4e47
AndreySN7 <andrey__n@mail.ru>
2025-11-16 21:56:50 +0300

    fix

---
+++ src/main/java/faang/school/postservice/config/kafka/KafkaConfig.java
@@ -0,0 +1,36 @@
+package faang.school.postservice.config.kafka;
+
+import faang.school.postservice.dto.event.CommentEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ProducerFactory<Long, CommentEvent> commentEventProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<Long, CommentEvent> commentEventKafkaTemplate() {
+        return new KafkaTemplate<>(commentEventProducerFactory());
+    }
+}
---
+++ src/main/java/faang/school/postservice/publisher/EventPublisher.java
@@ -0,0 +1,22 @@
+package faang.school.postservice.publisher;
+
+import faang.school.postservice.dto.event.CommentEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class EventPublisher {
+
+    private static final String COMMENT_TOPIC = "comment_topic";
+
+    private final KafkaTemplate<Long, CommentEvent> commentEventKafkaTemplate;
+
+    public void publish(CommentEvent event) {
+        commentEventKafkaTemplate.send(COMMENT_TOPIC, event.getAuthorId(), event);
+        log.info("✅ Publishing CommentEvent: {}", event);
+    }
+}
