cf4d5182ea2f7dc1e38f5ee90e8565eb32b4d599
Deathroels <loocyep@gmail.com>
2025-11-17 23:55:28 +0300

    BJS2-97260: -add PostEventAspect

--- src/main/java/faang/school/postservice/dto/post/PostEventAspect.java
+++ src/main/java/faang/school/postservice/dto/post/PostEventAspect.java
@@ -2,85 +2,96 @@ package faang.school.postservice.dto.post;
 
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.config.context.UserContext;
+import faang.school.postservice.dto.common.PageResponse;
 import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.kafka.publisher.PostViewEventPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class PostEventAspect {
-    private final ApplicationEventPublisher eventPublisher;
+    private final PostViewEventPublisher eventPublisher;
     private final UserContext userContext;
     private final UserServiceClient userServiceClient;
+    private final TaskExecutor taskExecutor;
 
-    @Around("@annotation(PublishPostEvent)")
-    public Object publishPostEvent(ProceedingJoinPoint joinPoint) throws  Throwable {
-        Long postId = extractPostId(joinPoint);
-
+    @After("@annotation(publishPostEvent)")
+    public void publishPostEvent(ProceedingJoinPoint joinPoint, PublishPostEvent publishPostEvent) throws  Throwable {
         Object result = joinPoint.proceed();
 
         if (result != null) {
-            publishEvent(postId, result);
-        }
-
-        return result;
-    }
-
-    private Long extractPostId(ProceedingJoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        String[] parameterNames = signature.getParameterNames();
-
-        for (int i = 0; i < parameterNames.length; i++) {
-            if (("postId".equals(parameterNames[i]) || "id".equals(parameterNames[i]))
-                    && args[i] instanceof Long) {
-                return (Long) args[i];
+            if (publishPostEvent.async()) {
+                taskExecutor.execute(()-> publishEvents(publishPostEvent.eventClass(), result));
+            } else {
+                publishEvents(publishPostEvent.eventClass(), result);
             }
         }
-
-        for (Object arg : args) {
-            if (arg instanceof Long) {
-                return (Long) arg;
-            }
-        }
-
-        throw new IllegalArgumentException("Post ID not found in method arguments");
     }
 
-    private void publishEvent(Long postId, Object methodResult) {
+    private void publishEvents(Class<?> eventClass, Object methodResult) {
         try {
             long userId = userContext.getUserId();
             UserDto user = userServiceClient.getUser(userId);
 
-            Long authorId = extractAuthorId(methodResult);
-
-            PostViewEvent event = new PostViewEvent(
-                    postId,
-                    authorId,
-                    userId,
-                    LocalDateTime.now()
-            );
-            eventPublisher.publishEvent(event);
+            if (eventClass == PostV2Dto.class) {
+                publishSingleEvent((PostV2Dto) methodResult, userId);
+            } else if (eventClass == PageResponse.class) {
+                publishCollectionEvents((PageResponse) methodResult, userId);
+            }
 
         } catch (Exception e) {
             log.error("Failed to publish PostViewEvent: {}", e.getMessage());
         }
     }
 
-    private Long extractAuthorId(Object methodResult) {
-        if (methodResult instanceof PostV2Dto) {
-            return ((PostV2Dto) methodResult).authorId();
+    private void publishSingleEvent(PostV2Dto postDto, long userId) {
+
+            PostViewEvent event = new PostViewEvent(
+                    postDto.id(),
+                    postDto.authorId(),
+                    userId,
+                    LocalDateTime.now()
+            );
+            eventPublisher.publish(event);
+
+    }
+
+    private void publishCollectionEvents(Object methodResult, long userId) {
+        if (methodResult instanceof PageResponse<?> pageResponse) {
+            pageResponse.content().forEach(item -> {
+                if (item instanceof PostV2Dto postDto) {
+                    PostViewEvent event = new PostViewEvent(
+                            postDto.id(),
+                            postDto.authorId(),
+                            userId,
+                            LocalDateTime.now()
+                    );
+                    eventPublisher.publish(event);
+                }
+            });
+        } else if (methodResult instanceof Collection<?> collection) {
+            collection.forEach(item -> {
+                if (item instanceof PostV2Dto postDto) {
+                    PostViewEvent event = new PostViewEvent(
+                            postDto.id(),
+                            postDto.authorId(),
+                            userId,
+                            LocalDateTime.now()
+                    );
+                    eventPublisher.publish(event);
+                }
+            });
         }
-        throw new IllegalStateException("Cannot extract authorId from method result");
     }
 }
