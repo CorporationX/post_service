7fcc76b942bf450449cf0d328f1402e8ab96a92c
v888.rus <v888.rus@gmail.com>
2025-11-20 15:30:46 +0000

    Interview with Vlad files

---
+++ src/main/java/faang/school/postservice/interview/Category.java
@@ -0,0 +1,9 @@
+package faang.school.postservice.interview;
+
+public class Category {
+    private String name;
+
+    public void printToConsole() {
+        System.out.println("Vals: " + name);
+    }
+}
