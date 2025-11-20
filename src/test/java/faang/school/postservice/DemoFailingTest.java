7aeada3dc939b9d8a095f0cf19c607cfdae4cf8f
a.dernovskiy <dernovski@icloud.com>
2025-11-20 11:28:53 +0700

    test branch for ci

---
+++ src/test/java/faang/school/postservice/DemoFailingTest.java
@@ -0,0 +1,13 @@
+package faang.school.postservice;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class DemoFailingTest {
+
+    @Test
+    void shouldFailForCiDemo() {
+        fail("demo");
+    }
+}
