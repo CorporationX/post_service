e1757c03019823386f049b9434344b10aa76ddcc
AndreySN7 <andrey__n@mail.ru>
2025-11-25 23:33:28 +0300

    правки ревью: fix

--- src/main/java/faang/school/postservice/config/kafka/KafkaConfig.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaConfig.java
@@ -1,8 +1,10 @@
 package faang.school.postservice.config.kafka;
 
 import faang.school.postservice.dto.event.CommentEvent;
+import faang.school.postservice.event.UserBanEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,16 +23,46 @@ public class KafkaConfig {
     private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<Long, CommentEvent> commentEventProducerFactory() {
+    public ProducerFactory<String, Object> stringObjectProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<Long, CommentEvent> commentEventKafkaTemplate() {
-        return new KafkaTemplate<>(commentEventProducerFactory());
+    public ProducerFactory<Long, CommentEvent> longCommentEventProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<Long, CommentEvent> longCommentEventKafkaTemplate() {
+        return new KafkaTemplate<>(longCommentEventProducerFactory());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> stringObjectKafkaTemplate() {
+        return new KafkaTemplate<>(stringObjectProducerFactory());
+    }
+    @Bean
+    public ProducerFactory<String, UserBanEvent> userBanEventProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, UserBanEvent> userBanEventKafkaTemplate() {
+        return new KafkaTemplate<>(userBanEventProducerFactory());
     }
 }
--- src/main/java/faang/school/postservice/validator/like/LikeValidator.java
+++ src/main/java/faang/school/postservice/validator/like/LikeValidator.java
@@ -1,13 +1,61 @@
 package faang.school.postservice.validator.like;
 
+import faang.school.postservice.exception.DataValidationException;
+import faang.school.postservice.exception.ResourceNotFoundException;
 import faang.school.postservice.model.Comment;
+import faang.school.postservice.model.Like;
 import faang.school.postservice.model.Post;
+import faang.school.postservice.repository.CommentRepository;
+import faang.school.postservice.repository.LikeRepository;
+import faang.school.postservice.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
-public interface LikeValidator {
+@RequiredArgsConstructor
+@Slf4j
+public class LikeValidator {
 
-    public Post validateLikeOnPost(long postId, long userId, boolean doSet);
+    private final LikeRepository likeRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
-    public Comment validateLikeOnComment(long commentId, long userId, boolean doSet);
+    public Post validateLikeOnPost(long postId, long userId, boolean doSet) {
+        Optional<Post> post = postRepository.findById(postId);
+        if (post.isEmpty()) {
+            log.error("Post doesn't exist");
+            throw new ResourceNotFoundException("Post doesn't exist");
+        }
+        Optional<Like> like = likeRepository.findByPostIdAndUserId(postId, userId);
+        if (like.isPresent() && doSet) {
+            log.error("User has already set like on this post");
+            throw new DataValidationException("User has already set a like on this post");
+        }
+        if (like.isEmpty() && !doSet) {
+            log.error("User hasn't set like on this post");
+            throw new DataValidationException("User hasn't set like on this post");
+        }
+        return post.get();
+    }
+
+    public Comment validateLikeOnComment(long commentId, long userId, boolean doSet) {
+        Optional<Comment> comment = commentRepository.findById(commentId);
+        if (comment.isEmpty()) {
+            log.error("Comment doesn't exist");
+            throw new ResourceNotFoundException("Comment doesn't exist");
+        }
+        Optional<Like> like = likeRepository.findByCommentIdAndUserId(commentId, userId);
+        if (like.isPresent() && doSet) {
+            log.error("User has already set a like on the comment");
+            throw new DataValidationException("User has already set a like on the comment");
+        }
+        if (like.isEmpty() && !doSet) {
+            log.error("User hasn't set a like on the comment");
+            throw new DataValidationException("User hasn't set a like on the comment");
+        }
+        return comment.get();
+    }
 }
