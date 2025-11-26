4514f98c149c60bce4759067fb1a585154b54aaf
Viks <viktor.naroznoi@gmail.com>
2025-11-26 12:22:30 +0200

    --BJS2-96897 - changes based on feedback

--- src/main/java/faang/school/postservice/messaging/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/messaging/CommentEventPublisher.java
@@ -1,12 +1,11 @@
 package faang.school.postservice.messaging;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.postservice.dto.event.CommentEventDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.yaml.snakeyaml.events.CommentEvent;
 
 @Slf4j
 @Component
@@ -14,14 +13,15 @@ import org.yaml.snakeyaml.events.CommentEvent;
 public class CommentEventPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
-    private final ObjectMapper objectMapper;
 
     @Value("${spring.data.redis.channels.name.comment_event}")
     private String channel;
 
-    public void publish(CommentEvent commentEvent) {
-        log.info("Publishing comment event notification");
-        redisTemplate.convertAndSend(channel, commentEvent);
+    public void publish(CommentEventDto commentEventDto) {
+        log.info("Publishing comment event notification, authorId: {}, receiverId: {}, commentId: {}, postId: {}",
+                commentEventDto.authorId(), commentEventDto.receivedId(),
+                commentEventDto.commentId(), commentEventDto.postId());
+        redisTemplate.convertAndSend(channel, commentEventDto);
         log.info("Comment event notification sent");
     }
 }
