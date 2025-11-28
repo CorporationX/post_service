d0ccddaece01c49817ce3172019c38e5a098a56c
ayazs <ayaz-22@yandex.ru>
2025-11-28 11:42:15 +0500

    редакция RedisConfiguration

--- src/main/java/faang/school/postservice/redis/message_broker/RedisConfiguration.java
+++ src/main/java/faang/school/postservice/redis/message_broker/RedisConfiguration.java
@@ -3,6 +3,7 @@ package faang.school.postservice.redis.message_broker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -36,9 +37,9 @@ public class RedisConfiguration {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory);
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
