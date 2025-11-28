a761db02c64185057cef991f30f51a182163b844
ayazs <ayaz-22@yandex.ru>
2025-11-28 10:11:18 +0500

    удален RedisConfig

--- src/main/java/faang/school/postservice/redis/message_broker/RedisConfiguration.java
+++ src/main/java/faang/school/postservice/redis/message_broker/RedisConfiguration.java
@@ -3,11 +3,11 @@ package faang.school.postservice.redis.message_broker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 
@@ -23,6 +23,9 @@ public class RedisConfiguration {
     @Value("${spring.data.redis.channels.like}")
     private String topicName;
 
+    @Value("${spring.data.redis.channels.comment}")
+    private String topicComment;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
@@ -33,16 +36,21 @@ public class RedisConfiguration {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
+        template.setConnectionFactory(jedisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 
     @Bean
     ChannelTopic likeTopic() {
         return new ChannelTopic(topicName);
+    }
+
+    @Bean
+    ChannelTopic topicComment() {
+        return new ChannelTopic(topicComment);
     }
 }
