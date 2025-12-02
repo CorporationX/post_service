ca23a7f0b29e501063c44b25b1f0819d337bda22
Viktor Narõžnõi <90530830+flesheatingcat@users.noreply.github.com>
2025-12-02 23:24:47 +0200

    --BJS2-96897 - Comment Event Publisher in post service with Redis config (#2165)
    * --BJS2-96897 - Comment Event Publisher in post service with Redis config
    
    * --BJS2-96897 - changes based on feedback
    
    * --BJS2-96897 - changes based on feedback

---
+++ src/main/java/faang/school/postservice/messaging/CommentEventPublisher.java
@@ -0,0 +1,31 @@
+package faang.school.postservice.messaging;
+
+import faang.school.postservice.dto.event.CommentEventDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CommentEventPublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Value("${spring.data.redis.channels.name.comment_event}")
+    private String channel;
+
+    public void publish(CommentEventDto commentEventDto) {
+        log.info("Publishing comment event notification, authorId: {}, receiverId: {}, commentId: {}, postId: {}",
+                commentEventDto.authorId(), commentEventDto.receivedId(),
+                commentEventDto.commentId(), commentEventDto.postId());
+        try {
+            redisTemplate.convertAndSend(channel, commentEventDto);
+        } catch (RuntimeException e) {
+            log.error("Conversion or sending message failed with error: ", e);
+        }
+        log.info("Comment event notification sent");
+    }
+}
