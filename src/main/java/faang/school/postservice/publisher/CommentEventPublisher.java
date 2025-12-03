c72aac0dfaf1e3f182892b95574fa4e186a2139e
ayazs <ayaz-22@yandex.ru>
2025-12-03 23:05:11 +0500

    96995 fixed after updating postservice

--- src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
+++ src/main/java/faang/school/postservice/publisher/CommentEventPublisher.java
@@ -1,8 +1,10 @@
 package faang.school.postservice.publisher;
 
 import faang.school.postservice.dto.comment.CommentEvent;
+import faang.school.postservice.dto.event.CommentEventDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
@@ -15,6 +17,9 @@ public class CommentEventPublisher implements MessagePublisher<CommentEvent> {
     private final RedisTemplate<String, Object> redisTemplate;
     private final ChannelTopic topicComment;
 
+    @Value("${spring.data.redis.channels.comment_event_channel.name}")
+    private String channel;
+
 
     @Override
     public void publishMessage(CommentEvent event) {
@@ -25,5 +30,17 @@ public class CommentEventPublisher implements MessagePublisher<CommentEvent> {
             log.error("Failed to publish event: {}", e.getMessage(), e);
         }
 
+    }
+
+    public void publish(CommentEventDto commentEventDto) {
+        log.info("Publishing comment event notification, authorId: {}, receiverId: {}, commentId: {}, postId: {}",
+                commentEventDto.authorId(), commentEventDto.receivedId(),
+                commentEventDto.commentId(), commentEventDto.postId());
+        try {
+            redisTemplate.convertAndSend(channel, commentEventDto);
+        } catch (RuntimeException e) {
+            log.error("Conversion or sending message failed with error: ", e);
+        }
+        log.info("Comment event notification sent");
     }
 }
