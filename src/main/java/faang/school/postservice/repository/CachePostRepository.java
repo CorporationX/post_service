fd2a2570be08cd9dd23e537b0c3f02e9861bd533
Rukk Taras <tarrukk@gmail.com>
2025-12-19 16:59:53 +0300

    Разработал систему получение из Kafka и обработки PostEvent.

--- src/main/java/faang/school/postservice/repository/CachePostRepository.java
+++ src/main/java/faang/school/postservice/repository/CachePostRepository.java
@@ -2,6 +2,8 @@ package faang.school.postservice.repository;
 
 import faang.school.postservice.dto.redis.RedisPostDto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CachePostRepository extends CrudRepository<RedisPostDto, Long> {
 }
