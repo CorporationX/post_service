2cc79cdb42aaa589668800618edb64bd28d8d5a7
Scherbi <shherbakov99ilya@mail.ru>
2025-12-20 15:35:03 +0300

    BJS2-97340 create save in redis

--- src/main/java/faang/school/postservice/PostServiceApp.java
+++ src/main/java/faang/school/postservice/PostServiceApp.java
@@ -3,11 +3,13 @@ package faang.school.postservice;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableScheduling
 @EnableRetry
 @EnableFeignClients(basePackages = "faang.school.postservice.client")
