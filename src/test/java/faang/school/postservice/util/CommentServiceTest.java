9725629f11c21cd927c2056da303cd4115a84435
Mikhail Easy <mghievrie@bk.ru>
2025-12-21 01:16:06 +0500

    BJS2-97238 tests

--- src/test/java/faang/school/postservice/util/CommentServiceTest.java
+++ src/test/java/faang/school/postservice/util/CommentServiceTest.java
@@ -7,6 +7,7 @@ import faang.school.postservice.dto.comment.UpdateCommentDto;
 import faang.school.postservice.exception.EntityNotFoundException;
 import faang.school.postservice.exception.ForbiddenException;
 import faang.school.postservice.mapper.CommentMapper;
+import faang.school.postservice.messages.kafka.producers.CommentPublish;
 import faang.school.postservice.messages.spring.publishers.SpringCommentPublisher;
 import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Post;
@@ -47,6 +48,8 @@ public class CommentServiceTest {
     SpringCommentPublisher commentPublisher;
     @Mock
     private PostRepository postRepository;
+    @Mock
+    private CommentPublish commentPublish;
     @InjectMocks
     CommentServiceImpl service;
 
