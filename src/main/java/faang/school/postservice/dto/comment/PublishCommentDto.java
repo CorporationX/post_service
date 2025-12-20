be52e35c507b325b74e3d024f5d77e53779b610c
Mikhail Easy <mghievrie@bk.ru>
2025-12-21 01:10:50 +0500

    BJS2-97238 minor fix

--- src/main/java/faang/school/postservice/dto/comment/PublishCommentDto.java
+++ src/main/java/faang/school/postservice/dto/comment/PublishCommentDto.java
@@ -2,9 +2,14 @@ package faang.school.postservice.dto.comment;
 
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public record PublishCommentDto(
         Long commentId,
-        Long authorId
+        Long authorId,
+        Long postId,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
 ) {
 }
