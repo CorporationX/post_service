bfafb5d60c664057164ccaf75ef4995e2df3b547
Elya Khamzina <ekkhamzi@mail.ru>
2025-12-21 20:27:31 +0300

    feature-BJS2-97872-kafka-event-listener

--- src/test/java/faang/school/postservice/util/service/FeedServiceImplTest.java
+++ src/test/java/faang/school/postservice/util/service/FeedServiceImplTest.java
@@ -21,7 +21,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
