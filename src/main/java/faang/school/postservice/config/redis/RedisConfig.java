2d0ab8c15f7481217699555230946663b936f7ee
Asta <charlyfazbear@gmail.com>
2025-12-21 14:00:41 +0300

    -saving the comment author to Redis after publishing

--- src/main/java/faang/school/postservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfig.java
@@ -1,26 +1,38 @@
-package faang.school.postservice.config;
+package faang.school.postservice.config.redis;
 
-import faang.school.postservice.dto.user.UserDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.mapping.RedisMappingContext;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, UserDto> authorRedisTemplate(
+    public RedisTemplate<String, Object> authorRedisTemplate(
             RedisConnectionFactory connectionFactory) {
 
-        RedisTemplate<String, UserDto> template = new RedisTemplate<>();
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return template;
+    }
+
+    @Bean
+    public RedisMappingContext redisMappingContext(
+            @Value("${cache.redis.author.ttl-seconds}") long ttlSeconds) {
+
+        RedisMappingContext context = new RedisMappingContext();
+        context.setTimeToLive(Duration.ofSeconds(ttlSeconds));
+        return context;
     }
 }
