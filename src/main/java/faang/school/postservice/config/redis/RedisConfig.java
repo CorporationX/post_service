fa1e4e58c114e156532e6c760f8c4cf242ebc84a
Asta <charlyfazbear@gmail.com>
2025-12-21 14:17:56 +0300

    -saving the comment author to Redis after publishing

--- src/main/java/faang/school/postservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/postservice/config/redis/RedisConfig.java
@@ -1,21 +1,17 @@
 package faang.school.postservice.config.redis;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.mapping.RedisMappingContext;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisTemplate<String, Object> authorRedisTemplate(
+    @Bean("RedisTemplate")
+    public RedisTemplate<String, Object> RedisTemplate(
             RedisConnectionFactory connectionFactory) {
 
         RedisTemplate<String, Object> template = new RedisTemplate<>();
