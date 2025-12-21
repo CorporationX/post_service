b86b073f6c4d45814358cb87c895dc579ef8f8d6
Asta <charlyfazbear@gmail.com>
2025-12-21 14:15:28 +0300

    -saving the comment author to Redis after publishing

--- src/main/java/faang/school/postservice/config/redis/AuthorCache.java
+++ src/main/java/faang/school/postservice/config/redis/AuthorCache.java
@@ -1,14 +1,19 @@
 package faang.school.postservice.config.redis;
 
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
+@Builder
 @RedisHash("author")
 public record AuthorCache (
         @Id
         Long id,
         String username,
         String email,
-        Boolean active
+        Boolean active,
+        @TimeToLive
+        Long ttl
 ){
 }
