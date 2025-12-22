421f7f68c51701be7e542a64d6105b6cf872728d
Viks <viktor.naroznoi@gmail.com>
2025-12-22 19:19:20 +0200

    --BJS2-97221 - Added a bit of logging

--- src/main/java/faang/school/postservice/messaging/KafkaPostViewProducer.java
+++ src/main/java/faang/school/postservice/messaging/KafkaPostViewProducer.java
@@ -2,12 +2,14 @@ package faang.school.postservice.messaging;
 
 import faang.school.postservice.dto.event.PostViewEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KafkaPostViewProducer {
@@ -24,6 +26,7 @@ public class KafkaPostViewProducer {
                 Instant.now()
         );
 
+        log.info("Sending post view event");
         kafkaTemplate.send(
                 topic,
                 postId.toString(),
