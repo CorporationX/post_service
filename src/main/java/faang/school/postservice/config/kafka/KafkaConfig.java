b66e5837763606ae041b1db151126db3ca03db1e
Asta <charlyfazbear@gmail.com>
2025-12-23 22:24:54 +0300

    -kafka consumer

--- src/main/java/faang/school/postservice/config/kafka/KafkaConfig.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.JsonSerializer;
