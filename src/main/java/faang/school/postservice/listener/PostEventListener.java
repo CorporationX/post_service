011e506ac506ee383bb496a81e14a6cf17451f08
Kuanysh <skn_88@mail.ru>
2025-12-25 13:25:51 +0500

    BJS2-97784 news feed: add user to redis

--- src/main/java/faang/school/postservice/listener/PostEventListener.java
+++ src/main/java/faang/school/postservice/listener/PostEventListener.java
@@ -1,11 +1,17 @@
 package faang.school.postservice.listener;
 
+import faang.school.postservice.client.UserServiceClient;
+import faang.school.postservice.dto.cache.UserCacheDto;
 import faang.school.postservice.dto.cache.PostCacheDto;
 import faang.school.postservice.dto.event.PostPublishEventDto;
+import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.exception.EntityNotFoundException;
 import faang.school.postservice.repository.cache.FeedCacheRepository;
+import faang.school.postservice.repository.cache.UserCacheRepository;
 import faang.school.postservice.repository.cache.PostCacheRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
@@ -18,6 +24,8 @@ import java.time.Instant;
 public class PostEventListener {
     private final FeedCacheRepository feedCacheRepository;
     private final PostCacheRepository postCacheRepository;
+    private final UserCacheRepository userCacheRepository;
+    private final UserServiceClient userServiceClient;
 
     @KafkaListener(
             topics = "${kafka.topic.post-event}",
@@ -25,6 +33,12 @@ public class PostEventListener {
     public void handlePostPublishEvent(PostPublishEventDto postPublishEventDto, Acknowledgment acknowledgment) {
         log.info("New post publish event: {}", postPublishEventDto);
         try {
+            UserDto userDto = getUserAuthorId(postPublishEventDto.authorId());
+            userCacheRepository.save(UserCacheDto.builder()
+                    .id(userDto.id())
+                    .name(userDto.username())
+                    .build());
+
             postCacheRepository.save(PostCacheDto.builder()
                     .id(postPublishEventDto.postId())
                     .authorId(postPublishEventDto.authorId())
@@ -45,5 +59,13 @@ public class PostEventListener {
             return;
         }
         acknowledgment.acknowledge();
+    }
+
+    private UserDto getUserAuthorId(long userId) {
+        ResponseEntity<UserDto> responseEntity = userServiceClient.getUser(userId);
+        if (responseEntity.getBody() == null) {
+            throw new EntityNotFoundException("User " + userId + " not found");
+        }
+        return responseEntity.getBody();
     }
 }
