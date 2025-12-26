f76fba09c4b1179a18d44162467138fc2583453d
Viks <viktor.naroznoi@gmail.com>
2025-12-26 14:01:23 +0200

    --BJS2-97231 - Initial commit

--- src/main/java/faang/school/postservice/repository/redis/RedisUserRepository.java
+++ src/main/java/faang/school/postservice/repository/redis/RedisUserRepository.java
@@ -1,10 +1,37 @@
 package faang.school.postservice.repository.redis;
 
 import faang.school.postservice.dto.user.CacheUserDto;
-import org.springframework.data.repository.CrudRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RedisUserRepository extends CrudRepository<CacheUserDto, Long> {
+import java.time.Duration;
+import java.util.Optional;
 
+@Repository
+@RequiredArgsConstructor
+public class RedisUserRepository {
+
+    private static final String KEY_PREFIX = "users:";
+
+    private final RedisTemplate<String, CacheUserDto> redisTemplate;
+
+    @Value("${spring.data.redis.user-repository.ttl-days}")
+    private Duration ttl;
+
+    public void save(CacheUserDto cacheUserDto) {
+        String key = KEY_PREFIX + cacheUserDto.getId();
+
+        redisTemplate.opsForValue().set(
+                key,
+                cacheUserDto,
+                ttl.toDays()
+        );
+    }
+
+    public Optional<CacheUserDto> findById(Long userId) {
+        String key = KEY_PREFIX + userId;
+        return Optional.ofNullable(redisTemplate.opsForValue().get(key));
+    }
 }
