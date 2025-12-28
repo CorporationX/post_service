e3e49ed15da58c0ef941a712df91706481b3bdd9
Kuanysh <skn_88@mail.ru>
2025-12-28 05:55:13 +0500

    BJS2-97790 news feed: fix conflicts

--- src/main/java/faang/school/postservice/listener/PostEventListener.java
+++ src/main/java/faang/school/postservice/listener/PostEventListener.java
@@ -3,14 +3,12 @@ package faang.school.postservice.listener;
 import faang.school.postservice.dto.cache.PostCacheDto;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.cache.UserCacheDto;
-import faang.school.postservice.dto.cache.PostCacheDto;
 import faang.school.postservice.dto.event.PostPublishEventDto;
 import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.exception.EntityNotFoundException;
 import faang.school.postservice.repository.cache.FeedCacheRepository;
 import faang.school.postservice.repository.cache.PostCacheRepository;
 import faang.school.postservice.repository.cache.UserCacheRepository;
-import faang.school.postservice.repository.cache.PostCacheRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +26,6 @@ public class PostEventListener {
     private final PostCacheRepository postCacheRepository;
     private final UserCacheRepository userCacheRepository;
     private final UserServiceClient userServiceClient;
-    private final PostCacheRepository postCacheRepository;
 
     @KafkaListener(
             topics = "${kafka.topic.post-event}",
