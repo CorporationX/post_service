d01e374888be32a4b9845585c95eaa9123c9476d
Viktor Narõžnõi <90530830+flesheatingcat@users.noreply.github.com>
2025-12-28 19:12:46 +0200

    --BJS2-97225 - Initial commit (#2194)
    * --BJS2-97221 - Initial commit
    
    * --BJS2-97221 - Corrections
    
    * --BJS2-97225 - Corrections based on feedback

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     /**
      * Utils & Logging
      */
+    implementation("org.springframework.kafka:spring-kafka")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.2")
     implementation("org.slf4j:slf4j-api:2.0.5")
