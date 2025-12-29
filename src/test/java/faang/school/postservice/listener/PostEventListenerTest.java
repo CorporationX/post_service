1e132d7e9cc728886c9d8c82e39c7f519636717e
Kuanysh <skn_88@mail.ru>
2025-12-29 22:33:28 +0500

    BJS2-97790 news feed: fix conflicts

--- src/test/java/faang/school/postservice/listener/PostEventListenerTest.java
+++ src/test/java/faang/school/postservice/listener/PostEventListenerTest.java
@@ -25,6 +25,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
@@ -56,9 +57,6 @@ public class PostEventListenerTest {
 
     @Mock
     private UserCacheRepository userCacheRepository;
-
-    @Mock
-    private PostCacheRepository postCacheRepository;
 
     @Mock
     private Acknowledgment acknowledgment;
@@ -102,13 +100,6 @@ public class PostEventListenerTest {
             .authorId(incorrectAuthorId)
             .postId(postId)
             .subscriberIds(subscribers)
-            .build();
-
-    PostCacheDto postCacheDto = PostCacheDto.builder()
-            .id(postId)
-            .authorId(authorId)
-            .content("content")
-            .createdAt(Instant.now())
             .build();
 
     @Test
@@ -170,6 +161,8 @@ public class PostEventListenerTest {
 
     @Test
     public void testFailWhilePostRedisExceptionReturned() {
+        when(userServiceClient.getUser(anyLong()))
+                .thenReturn(ResponseEntity.ok(userDto));
         doThrow(new RuntimeException("Can't add a value in Post Redis"))
                 .when(postCacheRepository)
                 .save(any(PostCacheDto.class));
