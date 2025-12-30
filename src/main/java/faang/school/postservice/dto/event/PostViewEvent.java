416a917292264a0321c7cbe0fd2eef8d79b22977
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-12-30 13:08:43 +0100

    consume post-view events with atomic Redis updates

---
+++ src/main/java/faang/school/postservice/dto/event/PostViewEvent.java
@@ -0,0 +1,20 @@
+package faang.school.postservice.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostViewEvent implements Serializable {
+    private Long postId;
+    private Long userId;
+    private LocalDateTime viewedAt;
+}
+
