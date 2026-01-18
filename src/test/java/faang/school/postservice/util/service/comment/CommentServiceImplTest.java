783e6eb10969626650132176c08b2551aa312fa4
Andre` <acmisatyuk@gmail.com>
2026-01-18 19:14:59 +0300

    BJS2-97837 News Feed: BJS2-97843 Feed Endpoint: build fix

--- src/test/java/faang/school/postservice/util/service/comment/CommentServiceImplTest.java
+++ src/test/java/faang/school/postservice/util/service/comment/CommentServiceImplTest.java
@@ -28,9 +28,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
