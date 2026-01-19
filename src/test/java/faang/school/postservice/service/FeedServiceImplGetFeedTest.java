bb29b3853c84f11b0a43e5cbd5fef86c8c3b9e40
Andre` <acmisatyuk@gmail.com>
2026-01-19 18:52:57 +0300

    BJS2-97837 News Feed: BJS2-97843 Feed Endpoint: OutboxPattern added for postEvents processing

--- src/test/java/faang/school/postservice/service/FeedServiceImplGetFeedTest.java
+++ src/test/java/faang/school/postservice/service/FeedServiceImplGetFeedTest.java
@@ -28,9 +28,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FeedServiceImplGetFeedTest {
